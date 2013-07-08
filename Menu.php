2fec1ddfd0687b4336817d721a7c04c36b6ea9ad
François Jacquet <user@example.com>
2013-07-08 13:50:55 -0500

    Bugfix Remove modules with no programs
    Bug when a module with no programs available appears in the menu

--- Menu.php
+++ Menu.php
@@ -20,6 +20,9 @@ if(empty($_ROSARIO['Menu']))
 
 	foreach($menu as $modcat=>$profiles)
 	{
+		//modif Francois: bugfix remove modules with no programs
+		$no_programs_in_module = true;
+		
 		$programs = $profiles[$profile];
 		foreach($programs as $program=>$title)
 		{
@@ -27,11 +30,16 @@ if(empty($_ROSARIO['Menu']))
 			{
 //				if($_ROSARIO['AllowUse'][$program] && ($profile!='admin' || !$exceptions[$modcat][$program] || AllowEdit($program)))
 				if(!empty($_ROSARIO['AllowUse'][$program]) && ($profile!='admin' || empty($exceptions[$modcat][$program]) || AllowEdit($program)))
+				{
 					$_ROSARIO['Menu'][$modcat][$program] = $title;
+					$no_programs_in_module = false;
+				}
 			}
 			else
 				$_ROSARIO['Menu'][$modcat][$program] = $title;
 		}
+		if ($no_programs_in_module)
+			unset($_ROSARIO['Menu'][$modcat]);
 	}
 
 //modif Francois: enable password change for students
