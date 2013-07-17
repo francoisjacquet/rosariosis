78f46e65debc1707985ccb8e586064fd0177c744
François Jacquet <user@example.com>
2013-07-17 14:15:30 -0500

    Replace ? with & in modname (5)
    Allow PHP scripts in misc/ one by one in place of the whole foldergfix

--- Modules.php
+++ Modules.php
@@ -2,20 +2,29 @@
 error_reporting(E_ALL ^ E_NOTICE);
 //error_reporting(E_ERROR);
 include 'Warehouse.php';
+
+//array_rwalk($_REQUEST,'DBEscapeString');
+
 //modif Francois: add TinyMCE to the textarea (see modules/Students/Letters.php & modules/Grades/HonorRollSubject.php & modules/Grades/HonorRoll.php)
-if ((!$_REQUEST['modname']=='Students/Letters.php' || !isset($_REQUEST['letter_text'])) && (!$_REQUEST['modname']=='Grades/HonorRollSubject.php' || !isset($_REQUEST['honor_roll_text'])) && (!$_REQUEST['modname']=='Grades/HonorRoll.php' || !isset($_REQUEST['honor_roll_text'])))
+/*if (($_REQUEST['modname']=='Students/Letters.php' && isset($_REQUEST['letter_text'])) || (($_REQUEST['modname']=='Grades/HonorRollSubject.php' || $_REQUEST['modname']=='Grades/HonorRoll.php') && isset($_REQUEST['honor_roll_text'])))
 {
+	$REQUEST_letter_text = $_REQUEST['letter_text'];
+	$REQUEST_honor_roll_text = $_REQUEST['honor_roll_text'];
+}*/
+if ((!$_REQUEST['modname']=='Students/Letters.php' || !isset($_REQUEST['letter_text'])) && (!$_REQUEST['modname']=='Grades/HonorRollSubject.php' || !isset($_REQUEST['honor_roll_text'])) && (!$_REQUEST['modname']=='Grades/HonorRoll.php' || !isset($_REQUEST['honor_roll_text'])))
 	if(!get_magic_quotes_gpc())
 		array_rwalk($_REQUEST,'addslashes');
-	array_rwalk($_REQUEST,'strip_tags');
-}
+
+array_rwalk($_REQUEST,'strip_tags');
 
 if(!isset($_REQUEST['_ROSARIO_PDF']))
 {
 	Warehouse('header');
 
-	//if(strpos($_REQUEST['modname'],'misc/')===false && $_REQUEST['modname']!='Students/Student.php' && $_REQUEST['modname']!='School_Setup/Calendar.php' && $_REQUEST['modname']!='Scheduling/Schedule.php' && $_REQUEST['modname']!='Attendance/Percent.php' && $_REQUEST['modname']!='Attendance/Percent.php&list_by_day=true' && $_REQUEST['modname']!='Scheduling/MassRequests.php' && $_REQUEST['modname']!='Scheduling/MassSchedule.php' && $_REQUEST['modname']!='Student_Billing/Fees.php')
+	//modif Francois: security fix, cf http://www.securiteam.com/securitynews/6S02U1P6BI.html
+	//allow PHP scripts in misc/ one by one in place of the whole folder
 	//if(strpos($_REQUEST['modname'],'misc/')===false)
+	if (!in_array($_REQUEST['modname'], array('misc/ChooseRequest.php', 'misc/ChooseCourse.php', 'misc/ViewContact.php')))
 		echo '<script type="text/javascript">if(window == top  && (!window.opener || window.opener.location.href.substring(0,(window.opener.location.href.indexOf("&")!=-1?window.opener.location.href.indexOf("&"):window.opener.location.href.replace("#","").length))!=window.location.href.substring(0,(window.location.href.indexOf("&")!=-1?window.location.href.indexOf("&"):window.location.href.replace("#","").length)))) window.location.href = "index.php";</script>';
 	echo '</HEAD><BODY id="modulesBody">';
 	echo '<DIV id="Migoicons" style="visibility:hidden;position:absolute;z-index:1000;top:-100px"></DIV>';
@@ -41,7 +50,6 @@ if($_REQUEST['modname'])
 	else*/
 		$modname = $_REQUEST['modname'];
 
-//	if(!$_REQUEST['LO_save'] && !isset($_REQUEST['_ROSARIO_PDF']) && (strpos($modname,'misc/')===false || $modname=='misc/Registration.php' || $modname=='misc/Export.php' || $modname=='misc/Portal.php'))
 	if(empty($_REQUEST['LO_save']) && !isset($_REQUEST['_ROSARIO_PDF']) && (strpos($modname,'misc/')===false || $modname=='misc/Registration.php' || $modname=='misc/Export.php' || $modname=='misc/Portal.php'))
 		$_SESSION['_REQUEST_vars'] = $_REQUEST;
 
@@ -69,7 +77,7 @@ if($_REQUEST['modname'])
 	//modif Francois: security fix, cf http://www.securiteam.com/securitynews/6S02U1P6BI.html
 	//allow PHP scripts in misc/ one by one in place of the whole folder
 	//if(substr($_REQUEST['modname'],0,5)=='misc/')
-	if (!$allowed && in_array($_REQUEST['modname'], array('misc/ChooseCourse.php', 'misc/ChooseCourse.php', 'misc/Portal.php', 'misc/ViewContact.php')))
+	if (!$allowed && in_array($_REQUEST['modname'], array('misc/ChooseRequest.php', 'misc/ChooseCourse.php', 'misc/Portal.php', 'misc/ViewContact.php')))
 		$allowed = true;
 
 	if($allowed)
