e200d736bdc748322e6fc5321c0d0c98300dce42
François Jacquet <user@example.com>
2013-07-22 17:23:23 -0500

    Help PDF rework
    - CSS WPAdmin look'n'feel
    - '%s Handbook' translation

--- Help.php
+++ Help.php
@@ -171,25 +171,29 @@ if(mb_strpos($_SERVER['PHP_SELF'],'Help.php'))
 	}
 
 	$handle = PDFStart();
-	echo '<TABLE><TR><TD><IMG SRC="assets/themes/'.Preferences('THEME').'/logo.png"></TD><TD><h1>'.$title.' Handbook<BR />Version 1.0</h1></TD></TR></TABLE><HR>';
+	echo '<TABLE><TR><TD><IMG SRC="assets/themes/'.Preferences('THEME').'/logo.png" /></TD><TD><h1>'.sprintf(_('%s Handbook'),$title).'<BR />'.sprintf(_('version %s'),'1.0').'</h1></TD></TR></TABLE><HR>';
 
 	foreach($help as $program=>$value)
 	{
 		if(mb_strpos($program,'/'))
 		{
 			$modcat = str_replace('_',' ',mb_substr($program,0,mb_strpos($program,'/')));
-			if($modcat!=$old_modcat && $RosarioModules[str_replace(' ','_',$modcat)]) //if module activated
-				echo '<div style="page-break-after: always;"></div><TABLE><TR><TD><IMG SRC="assets/icons/'.str_replace(' ','_',$modcat).'.png" height="48"></TD><TD><h1>'.$modcat.'</h1></TD></TR></TABLE><HR>';
+			
+			if (!$RosarioModules[str_replace(' ','_',$modcat)]) //module not activated
+				break;
+		
+			if($modcat!=$old_modcat)
+				echo '<div style="page-break-after: always;"></div><TABLE><TR><TD><IMG SRC="assets/icons/'.str_replace(' ','_',$modcat).'.png" class="headerIcon" /></TD><TD><h2>'.$modcat.'</h2></TD></TR></TABLE><HR>';
 			$old_modcat = $modcat;
 		}
 		$_REQUEST['modname'] = $program;
-		echo '<h2>';
+		echo '<h3>';
 		if($program=='default')
-			echo Config('TITLE').' '.$title.' Handbook<BR />'.sprintf(_('version %s'),'1.0');
+			echo Config('TITLE').' - '.sprintf(_('%s Handbook'),$title).'<BR />'.sprintf(_('version %s'),'1.0');
 		else
 			echo (ProgramTitle() == 'RosarioSIS' ? str_replace(' ','_',$program) : ProgramTitle());
-		echo '</h2>';
-		echo '<TABLE style="border: solid 1px black; background-color:#DDD;" class="width-100p cellpadding-5"><TR><TD>';
+		echo '</h3>';
+		echo '<TABLE class="width-100p cellpadding-5"><TR><TD class="header2">';
 		if($student==true)
 			$value = str_replace('your child','yourself',str_replace('your child\'s','your',$value));
 		echo $value;
--- Help_es.php
+++ Help_es.php
@@ -168,30 +168,33 @@ if(mb_strpos($_SERVER['PHP_SELF'],'Help_es.php'))
 	}
 
 	$handle = PDFStart();
-	echo '<TABLE><TR><TD><IMG SRC="assets/themes/'.Preferences('THEME').'/logo.png"></TD><TD><h1>Manual del '.$title.'<BR />Versión 1.0</h1></TD></TR></TABLE><HR>';
+	echo '<TABLE><TR><TD><IMG SRC="assets/themes/'.Preferences('THEME').'/logo.png" /></TD><TD><h1>'.sprintf(_('%s Handbook'),$title).'<BR />'.sprintf(_('version %s'),'1.0').'</h1></TD></TR></TABLE><HR>';
 
 	foreach($help as $program=>$value)
 	{
 		if(mb_strpos($program,'/'))
 		{
 			$modcat = str_replace('_',' ',mb_substr($program,0,mb_strpos($program,'/')));
-			if($modcat!=$old_modcat && $RosarioModules[str_replace(' ','_',$modcat)]) //if module activated
-				echo '<div style="page-break-after: always;"></div><TABLE><TR><TD><IMG SRC="assets/icons/'.str_replace(' ','_',$modcat).'.png" height="48"></TD><TD><h1>'._($modcat).'</h1></TD></TR></TABLE><HR>';
+			
+			if (!$RosarioModules[str_replace(' ','_',$modcat)]) //module not activated
+				break;
+		
+			if($modcat!=$old_modcat)
+				echo '<div style="page-break-after: always;"></div><TABLE><TR><TD><IMG SRC="assets/icons/'.str_replace(' ','_',$modcat).'.png"  class="headerIcon" /></TD><TD><h2>'._($modcat).'</h2></TD></TR></TABLE><HR>';
 			$old_modcat = $modcat;
 		}
 		$_REQUEST['modname'] = $program;
 		echo '<h3>';
 		if($program=='default')
-			echo Config('TITLE').' Manual del '.$title.'<BR />Versión 1.0';
+			echo Config('TITLE').' - '.sprintf(_('%s Handbook'),$title).'<BR />'.sprintf(_('version %s'),'1.0');
 		else
 			echo (ProgramTitle() == 'RosarioSIS' ? str_replace(' ','_',$program) : ProgramTitle());
 		echo '</h3>';
-		echo '<TABLE style="border: solid 1px black; background-color:#DDD;" class="width-100p cellpadding-5"><TR><TD>';
+		echo '<TABLE class="width-100p cellpadding-5"><TR><TD class="header2">';
 		/*if($student==true)
 			$value = str_replace('your child','yourself',str_replace('your child\'s','your',$value));*/
 		echo $value;
 		echo '</TD></TR></TABLE><BR />';
-		$_REQUEST['modname'] = '';
 	}
 	PDFStop($handle);
 }
