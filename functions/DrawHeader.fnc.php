6b21617cfe77efabcec1ae4cd0bedca3547c5a31
François Jacquet <user@example.com>
2013-07-22 17:11:56 -0500

    CSS headerIcon

--- functions/DrawHeader.fnc.php
+++ functions/DrawHeader.fnc.php
@@ -3,25 +3,16 @@
 function DrawHeader($left='',$right='',$center='')
 {	global $_ROSARIO;
 
-	if(!isset($_ROSARIO['DrawHeader']))
-	{
-		$_ROSARIO['DrawHeader'] = '';
-		if($_ROSARIO['HeaderIcon'])
-			$left = '<IMG src="assets/icons/'.$_ROSARIO['HeaderIcon'].'" height="32" /> '.$left; //modif Francois: icones
-	}
-
 	echo '<TABLE class="width-100p cellspacing-0 cellpadding-0"><TR>';
 //modif Francois: CSS WPadmin
 	if($left)
-		echo '<TD '.$_ROSARIO['DrawHeader'].' style="text-align:left;">&nbsp;'.($_ROSARIO['DrawHeader'] == ''? '<H2>'.$left.'</H2>':$left).'</TD>';
+		echo '<TD '.$_ROSARIO['DrawHeader'].' style="text-align:left;">&nbsp;'.(!isset($_ROSARIO['DrawHeader'])? (!empty($_ROSARIO['HeaderIcon']) ? '<IMG src="assets/icons/'.$_ROSARIO['HeaderIcon'].'" class="headerIcon" /> '.'<H2>'.$left.'</H2>' : '<H2>'.$left.'</H2>'):$left).'</TD>';
 	if($center)
-		echo '<TD '.$_ROSARIO['DrawHeader'].' style="text-align:center">'.($_ROSARIO['DrawHeader'] == ''? '<H2>'.$center.'</H2>':$center).'</TD>';
+		echo '<TD '.$_ROSARIO['DrawHeader'].' style="text-align:center">'.(!isset($_ROSARIO['DrawHeader'])? '<H2>'.$center.'</H2>':$center).'</TD>';
 	if($right)
-		echo '<TD '.$_ROSARIO['DrawHeader'].' style="text-align:right">'.($_ROSARIO['DrawHeader'] == ''? '<H2>'.$right.'</H2>':$right).'</TD>';
+		echo '<TD '.$_ROSARIO['DrawHeader'].' style="text-align:right">'.(!isset($_ROSARIO['DrawHeader'])? '<H2>'.$right.'</H2>':$right).'</TD>';
 	echo '</TR></TABLE>';
 
-	if($_ROSARIO['DrawHeader'] == '') {
-		$_ROSARIO['DrawHeader'] = 'class="header2"';
-	}
+	$_ROSARIO['DrawHeader'] = 'class="header2"';
 }
 ?>
