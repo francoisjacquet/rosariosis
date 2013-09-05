08b87646137d3bc05c641e03a0719e20c16614b2
François Jacquet <user@example.com>
2013-09-05 16:36:02 -0500

    Stackable tables

--- modules/School_Setup/MarkingPeriods.php
+++ modules/School_Setup/MarkingPeriods.php
@@ -232,7 +232,7 @@ if(empty($_REQUEST['modfunc']))
 		echo '<FORM action="Modules.php?modname='.$_REQUEST['modname'].'&mp_term='.$_REQUEST['mp_term'].'&marking_period_id='.$_REQUEST['marking_period_id'].'&year_id='.$_REQUEST['year_id'].'&semester_id='.$_REQUEST['semester_id'].'&quarter_id='.$_REQUEST['quarter_id'].'" method="POST">';
 		DrawHeader($title,AllowEdit()?$delete_button.SubmitButton(_('Save')):'');
 		$header .= '<TABLE class="width-100p cellpadding-3">';
-		$header .= '<TR>';
+		$header .= '<TR class="st">';
 
 		$header .= '<TD>' . TextInput($RET['TITLE'],'tables['.$_REQUEST['marking_period_id'].'][TITLE]',(!$RET['TITLE']?'<span style="color:red">':'')._('Title').(!$RET['TITLE']?'</span>':''), 'required') . '</TD>';
 		$header .= '<TD>' . TextInput($RET['SHORT_NAME'],'tables['.$_REQUEST['marking_period_id'].'][SHORT_NAME]',(!$RET['SHORT_NAME']?'<span style="color:red">':'')._('Short Name').(!$RET['SHORT_NAME']?'</span>':''), 'required') . '</TD>';
@@ -240,7 +240,7 @@ if(empty($_REQUEST['modfunc']))
 		$header .= '<TD><TABLE class="width-100p cellpadding-0"><TR><TD>' . CheckboxInput($RET['DOES_GRADES'],'tables['.$_REQUEST['marking_period_id'].'][DOES_GRADES]',_('Graded'),$checked,$_REQUEST['marking_period_id']=='new','<IMG SRC="assets/check.png" height="15">','<IMG SRC="assets/x.png" height="15">') . '</TD>';
 		$header .= '<TD>' . CheckboxInput($RET['DOES_COMMENTS'],'tables['.$_REQUEST['marking_period_id'].'][DOES_COMMENTS]',_('Comments'),$checked,$_REQUEST['marking_period_id']=='new','<IMG SRC="assets/check.png" height="15">','<IMG SRC="assets/x.png" height="15">') . '</TD></TR></TABLE></TD>';
 		$header .= '</TR>';
-		$header .= '<TR>';
+		$header .= '<TR class="st">';
 
 		$header .= '<TD>' . DateInput($RET['START_DATE'],'tables['.$_REQUEST['marking_period_id'].'][START_DATE]',(!$RET['START_DATE']?'<span style="color:red">':'')._('Begins').(!$RET['START_DATE']?'</span>':'')) . '</TD>';
 		$header .= '<TD>' . DateInput($RET['END_DATE'],'tables['.$_REQUEST['marking_period_id'].'][END_DATE]',(!$RET['END_DATE']?'<span style="color:red">':'')._('Ends').(!$RET['END_DATE']?'</span>':'')) . '</TD>';
@@ -258,7 +258,7 @@ if(empty($_REQUEST['modfunc']))
 	// DISPLAY THE MENU
 	$LO_options = array('save'=>false,'search'=>false);
 
-	echo '<TABLE><TR>';
+	echo '<TABLE><TR class="st">';
 
 	// FY
 	$sql = "SELECT MARKING_PERIOD_ID,TITLE FROM SCHOOL_MARKING_PERIODS WHERE MP='FY' AND SCHOOL_ID='".UserSchool()."' AND SYEAR='".UserSyear()."' ORDER BY SORT_ORDER";
