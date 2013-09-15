f9f487afbb192a6c18b983208ffbae3b6c7bbe83
François Jacquet <user@example.com>
2013-09-15 15:22:54 -0500

    Bugfix: escape Course Title

--- modules/misc/ChooseRequest.php
+++ modules/misc/ChooseRequest.php
@@ -7,11 +7,11 @@ if(!$_REQUEST['course_id'])
 else
 {
 	$course_title = DBGet(DBQuery("SELECT TITLE FROM COURSES WHERE COURSE_ID='".$_REQUEST['course_id']."'"));
-	$course_title = $course_title[1]['TITLE'].'<INPUT type="hidden" name="request_course_id" value="'.$_REQUEST['course_id'].'">'; 
+	$course_title = str_replace(array("'",'"'),array('&#39;','&quot;'),$course_title[1]['TITLE']).'<INPUT type="hidden" name="request_course_id" value="'.$_REQUEST['course_id'].'">'; 
 
 //modif Francois: add <label> on checkbox
 	echo '<script type="text/javascript">opener.document.getElementById("request_div").innerHTML = "';
-	$toEscape = str_replace(array("'",'"'),array('&#39;','&quot;'),$course_title).'<BR /><label><INPUT type="checkbox" name="not_request_course" value="Y">'.str_replace(array("'",'"'),array('&#39;','&quot;'),_('Not Requested')).'</label>';
+	$toEscape = $course_title.'<BR /><label><INPUT type="checkbox" name="not_request_course" value="Y">'.str_replace(array("'",'"'),array('&#39;','&quot;'),_('Not Requested')).'</label>';
 	echo str_replace('"','\"',$toEscape);
 	echo '"; window.close();</script>';
 }
