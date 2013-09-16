688f63369520752a7e39c792fb57ef1ae5c3354e
François Jacquet <user@example.com>
2013-09-16 17:40:22 -0500

    Responsive ReportCardComments

--- modules/Grades/ReportCardComments.php
+++ modules/Grades/ReportCardComments.php
@@ -254,7 +254,7 @@ function makeColorInput($value,$name)
 	$colors = array('#330099','#3366FF','#003333','#FF3300','#660000','#666666','#333366','#336633','purple','teal','firebrick','tan');
 	foreach($colors as $color)
 	{
-		$color_select[$color] = array('<TABLE class="cellpadding-0 cellspacing-0"><TR><TD style="width:100%; background-color:'.$color.'">&nbsp;</TD></TR></TABLE>','<TABLE class="cellpadding-0 cellspacing-0"><TR><TD style="background-color:'.$color.'; width:30px">&nbsp;</TD></TR></TABLE>');
+		$color_select[$color] = array('<span style="background-color:'.$color.'">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>','<span style="background-color:'.$color.';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>');
 	}
 	return RadioInput($value,"values[$id][$name]",'',$color_select);
 }
