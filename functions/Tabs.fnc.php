04a4570fc98af69e804ef9a05852d9cebf442bd7
François Jacquet <user@example.com>
2013-10-11 15:53:37 -0500

    Use same Tabs style for PDF

--- functions/Tabs.fnc.php
+++ functions/Tabs.fnc.php
@@ -60,16 +60,10 @@ function DrawTab($title,$link='')
 
 //modif Francois: css WPadmin
 	if($link && !isset($_REQUEST['_ROSARIO_PDF']))
-	{
 		$block_table .= '<h3><A HREF="'.$link.'" class="BoxHeading" id="tab_link['.preg_replace('/[^a-zA-Z0-9]/','_',$link).']">'._($title).'</A></h3>';
-	}
 	else
-	{
-		if(!isset($_REQUEST['_ROSARIO_PDF']))
-			$block_table .= '<h3>'.$title.'</h3>';
-		else
-			$block_table .= '<span class="size-1" style="color:'.Preferences('HIGHLIGHT').'"><b>'.$title.'</b>&nbsp;</span>';
-	}
+		$block_table .= '<h3>'.$title.'</h3>';
+		
 	return $block_table;
 }
 
