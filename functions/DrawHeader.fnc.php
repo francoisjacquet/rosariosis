a4a8783c7aee8832ed274e86e95e622688635768
François Jacquet <user@example.com>
2013-10-30 15:27:56 -0500

    Bugfix reset H2 DrawHeader

--- functions/DrawHeader.fnc.php
+++ functions/DrawHeader.fnc.php
@@ -6,11 +6,11 @@ function DrawHeader($left='',$right='',$center='')
 	echo '<TABLE class="width-100p cellspacing-0 cellpadding-0"><TR>';
 //modif Francois: CSS WPadmin
 	if($left)
-		echo '<TD '.$_ROSARIO['DrawHeader'].' style="text-align:left;">&nbsp;'.(!isset($_ROSARIO['DrawHeader'])? (!empty($_ROSARIO['HeaderIcon']) ? '<H2>'.'<IMG src="assets/icons/'.$_ROSARIO['HeaderIcon'].'" class="headerIcon" /> '.$left.'</H2>' : '<H2>'.$left.'</H2>'):$left).'</TD>';
+		echo '<TD '.$_ROSARIO['DrawHeader'].' style="text-align:left;">&nbsp;'.(empty($_ROSARIO['DrawHeader'])? (!empty($_ROSARIO['HeaderIcon']) ? '<H2>'.'<IMG src="assets/icons/'.$_ROSARIO['HeaderIcon'].'" class="headerIcon" /> '.$left.'</H2>' : '<H2>'.$left.'</H2>'):$left).'</TD>';
 	if($center)
-		echo '<TD '.$_ROSARIO['DrawHeader'].' style="text-align:center">'.(!isset($_ROSARIO['DrawHeader'])? '<H2>'.$center.'</H2>':$center).'</TD>';
+		echo '<TD '.$_ROSARIO['DrawHeader'].' style="text-align:center">'.(empty($_ROSARIO['DrawHeader'])? '<H2>'.$center.'</H2>':$center).'</TD>';
 	if($right)
-		echo '<TD '.$_ROSARIO['DrawHeader'].' style="text-align:right">'.(!isset($_ROSARIO['DrawHeader'])? '<H2>'.$right.'</H2>':$right).'</TD>';
+		echo '<TD '.$_ROSARIO['DrawHeader'].' style="text-align:right">'.(empty($_ROSARIO['DrawHeader'])? '<H2>'.$right.'</H2>':$right).'</TD>';
 	echo '</TR></TABLE>';
 
 	$_ROSARIO['DrawHeader'] = 'class="header2"';
