6cb0ee17e97b35a2a37bcc511c4c15025e97dbea
François Jacquet <user@example.com>
2014-01-02 17:32:33 -0500

    2013 to 2014

--- index.php
+++ index.php
@@ -179,7 +179,7 @@ if(!$_SESSION['STAFF_ID'] && !$_SESSION['STUDENT_ID'] && $_REQUEST['modfunc']!='
 	<BR />&copy; 2004-2009 <A HREF="http://www.miller-group.net">The Miller Group, Inc</A>
 	<br />&copy; 2009 <a href="http://www.glenn-abbey.com">Glenn Abbey Software, Inc</a>
 	<br />&copy; 2009 <a href="http://www.centresis.org">Learners Circle, LLC</a>
-	<br />&copy; 2012-2013 <a href="http://www.rosariosis.org">François Jacquet</a>
+	<br />&copy; 2012-2014 <a href="http://www.rosariosis.org">François Jacquet</a>
 	</span>';
 	PopTable("footer");
 	echo '<BR /></BODY></HTML>';
