c2e83acc4ae3346e49f0a8b0be69705e27f7280e
François Jacquet <user@example.com>
2014-01-08 17:36:02 -0500

    Move SYEAR from config table to config.inc.php (2)

--- functions/Config.fnc.php
+++ functions/Config.fnc.php
@@ -7,7 +7,7 @@ function Config($item)
 	{
 		$QI=DBQuery("SELECT TITLE, CONFIG_VALUE FROM CONFIG WHERE ".(UserSchool() < 1 ? '' : "SCHOOL_ID='".UserSchool()."' OR")." SCHOOL_ID='0'");
 		$_ROSARIO['Config'] = DBGet($QI, array(), array('TITLE'));
-		$_ROSARIO['Config'][1]['SYEAR'] = $DefaultSyear;
+		$_ROSARIO['Config']['SYEAR'][1]['CONFIG_VALUE'] = $DefaultSyear;
 	}
 
 	return $_ROSARIO['Config'][$item][1]['CONFIG_VALUE'];
