4aaebca67fe0a3985b276ef272ed58d1eb90fbf0
François Jacquet <user@example.com>
2014-01-10 16:28:27 -0500

    Fix bug inputs with double quotes (4)

--- functions/Inputs.php
+++ functions/Inputs.php
@@ -44,12 +44,12 @@ function TextInput($value,$name,$title='',$options='',$div=true)
 			return '<INPUT type="text" name="'.$name.'" id="'.$name.'" '.($value || $value==='0'?'value="'.str_replace(array("'",'"'),array('&#39;','&quot;'),$value).'"':'').' '.$options.' />'.($title!=''?'<BR />'.(mb_strpos(mb_strtolower($title),'<span ')===false?'<span class="legend-gray">':'').'<label for="'.$name.'">'.$title.'</label>'.(mb_strpos(mb_strtolower($title),'<span ')===false?'</span>':'').'':'');
 		else
 		{
-			$return = '<DIV id="div'.$name.'"><div class="onclick" onclick=\'javascript:addHTML("';
+			$return = '<DIV id="div'.$name.'"><div class="onclick" onclick=\'javascript:addHTML(html'.str_replace(array('[',']'),'',$name);
 			
-			$toEscape = '<INPUT type="text" id="input'.$name.'" name="'.$name.'" '.($value||$value==='0'?'value="'.str_replace(array("'",'"'),'&#39;',$value).'"':'').' '.$options.' />'.($title!=''?'<BR />'.(mb_strpos(mb_strtolower($title),'<span ')===false?'<span class="legend-gray">':'').'<label for="input'.$name.'">'.str_replace("'",'&#39;',$title).'</label>'.(mb_strpos(mb_strtolower($title),'<span ')===false?'</span>':'').'':'');
-			$return .=  str_replace('"','\"',$toEscape);
+			$toEscape = '<INPUT type="text" id="input'.$name.'" name="'.$name.'" '.($value||$value==='0'?'value="'.htmlspecialchars($value).'"':'').' '.$options.' />'.($title!=''?'<BR />'.(mb_strpos(mb_strtolower($title),'<span ')===false?'<span class="legend-gray">':'').'<label for="input'.$name.'">'.str_replace("'",'&#39;',$title).'</label>'.(mb_strpos(mb_strtolower($title),'<span ')===false?'</span>':'').'':'');
+			$return = '<script type="text/javascript">var html'.str_replace(array('[',']'),'',$name).'=\''.$toEscape.'\';</script>'.$return;
 			
-			$return .= '","div'.$name.'",true); document.getElementById("input'.$name.'").focus();\'><span class="underline-dots">'.($value!=''?$value1:'-').'</span>'.($title!=''?'<BR />'.(mb_strpos(mb_strtolower($title),'<span ')===false?'<span class="legend-gray">':'').$title.(mb_strpos(mb_strtolower($title),'<span ')===false?'</span>':'').'':'').'</div></DIV>';
+			$return .= ',"div'.$name.'",true); document.getElementById("input'.$name.'").focus();\'><span class="underline-dots">'.($value!=''?$value1:'-').'</span>'.($title!=''?'<BR />'.(mb_strpos(mb_strtolower($title),'<span ')===false?'<span class="legend-gray">':'').$title.(mb_strpos(mb_strtolower($title),'<span ')===false?'</span>':'').'':'').'</div></DIV>';
 			return $return;
 		}
 	}
@@ -140,12 +140,12 @@ function TextAreaInput($value,$name,$title='',$options='',$div=true)
 		else
 //modif Francois: remove ereg
 		{
-			$return = '<DIV id="div'.$name.'"><div class="onclick" onclick=\'javascript:addHTML("';
+			$return = '<DIV id="div'.$name.'"><div class="onclick" onclick=\'javascript:addHTML(html'.str_replace(array('[',']'),'',$name);
 			
-			$toEscape = '<TEXTAREA id="textarea'.$name.'" name="'.$name.'" '.$options.'>'.preg_replace("/[\n\r]/",'\u000D\u000A',str_replace("\r\n",'\u000D\u000A',str_replace("'",'&#39;',$value))).'</TEXTAREA>'.($title!=''?'<BR />'.(mb_strpos(mb_strtolower($title),'<span ')===false?'<span class="legend-gray">':'').'<label for="'.$name.'">'.$title.'</label>'.(mb_strpos(mb_strtolower($title),'<span ')===false?'</span>':'').'':'');
-			$return .=  str_replace('"','\"',$toEscape);
+			$toEscape = '<TEXTAREA id="textarea'.$name.'" name="'.$name.'" '.$options.'>'.preg_replace("/[\n\r]/",'\u000D\u000A',str_replace("\r\n",'\u000D\u000A',str_replace("'",'&#39;',$value))).'</TEXTAREA>'.($title!=''?'<BR />'.(mb_strpos(mb_strtolower($title),'<span ')===false?'<span class="legend-gray">':'').'<label for="'.$name.'">'.str_replace("'",'&#39;',$title).'</label>'.(mb_strpos(mb_strtolower($title),'<span ')===false?'</span>':'').'':'');
+			$return = '<script type="text/javascript">var html'.str_replace(array('[',']'),'',$name).'=\''.$toEscape.'\';</script>'.$return;
 			
-			$return .= '","div'.$name.'",true); document.getElementById("textarea'.$name.'").value=unescape(document.getElementById("textarea'.$name.'").value);\'>'.'<TABLE style="height:100%;"><TR><TD>'.((mb_substr_count($value,"\r\n")>$rows)?'<DIV style="overflow:auto; height:'.(15*$rows).'px; width:'.($cols*9).'; padding-right:16px;" class="underline-dots">'.nl2br($value).'</DIV>':'<DIV style="overflow:auto; width:'.($cols*9).'; padding-right:16px;" class="underline-dots">'.nl2br($value).'</DIV>').'</TD></TR></TABLE>'.($title!=''?''.(mb_strpos(mb_strtolower($title),'<span ')===false?'<span class="legend-gray">':'').str_replace("'",'&#39;',$title).(mb_strpos(mb_strtolower($title),'<span ')===false?'</span>':'').'':'').'</div></DIV>';
+			$return .= ',"div'.$name.'",true); document.getElementById("textarea'.$name.'").value=unescape(document.getElementById("textarea'.$name.'").value);\'>'.'<TABLE style="height:100%;"><TR><TD>'.((mb_substr_count($value,"\r\n")>$rows)?'<DIV style="overflow:auto; height:'.(15*$rows).'px; width:'.($cols*9).'; padding-right:16px;" class="underline-dots">'.nl2br($value).'</DIV>':'<DIV style="overflow:auto; width:'.($cols*9).'; padding-right:16px;" class="underline-dots">'.nl2br($value).'</DIV>').'</TD></TR></TABLE>'.($title!=''?''.(mb_strpos(mb_strtolower($title),'<span ')===false?'<span class="legend-gray">':'').str_replace("'",'&#39;',$title).(mb_strpos(mb_strtolower($title),'<span ')===false?'</span>':'').'':'').'</div></DIV>';
 			return $return;
 		}
 			
@@ -205,12 +205,8 @@ function SelectInput($value,$name,$title='',$options,$allow_na='N/A',$extra='',$
 
 	if(AllowEdit() && !isset($_REQUEST['_ROSARIO_PDF']))
 	{
-		$escape_array = array('&#39;','&quot;');
 		if($value!='' && $div)
-		{
-			$return = '<DIV id="div'.$name.'"><div class="onclick" onclick=\'javascript:addHTML("';
-			$escape_array = array('&#39;','');
-		}
+			$return = '<DIV id="div'.$name.'"><div class="onclick" onclick=\'javascript:addHTML(html'.str_replace(array('[',']'),'',$name);
 		
 		$select = '<SELECT name="'.$name.'" id="'.$name.'" '.$extra.'>';
 
@@ -224,7 +220,7 @@ function SelectInput($value,$name,$title='',$options,$allow_na='N/A',$extra='',$
 			foreach($options as $key=>$val)
 			{
 				$key .= '';
-				$select .= '<OPTION value="'.str_replace(array("'",'"'),$escape_array,$key).'"'.($value==$key && (!($value==false && $value!==$key) || ($value===0 && $key==='0'))?' SELECTED="SELECTED"':'').'>'.str_replace("'",'&#39;',(is_array($val)?$val[0]:$val)).'</OPTION>';
+				$select .= '<OPTION value="'.htmlspecialchars($key).'"'.($value==$key && (!($value==false && $value!==$key) || ($value===0 && $key==='0'))?' SELECTED="SELECTED"':'').'>'.str_replace("'",'&#39;',(is_array($val)?$val[0]:$val)).'</OPTION>';
 			}
 		}
 		$select .= '</SELECT>';
@@ -233,9 +229,9 @@ function SelectInput($value,$name,$title='',$options,$allow_na='N/A',$extra='',$
 		
 		if($value!='' && $div)
 		{
-			$return .=  str_replace('"','\"',$select);
+			$return = '<script type="text/javascript">var html'.str_replace(array('[',']'),'',$name).'=\''.$select.'\';</script>'.$return;
 
-			$return .= '","div'.$name.'",true)\'><span class="underline-dots">'.(is_array($options[$value])?$options[$value][1]:$options[$value]).'</span>'.($title!=''?'<BR />'.(mb_strpos(mb_strtolower($title),'<span ')===false?'<span class="legend-gray">':'').str_replace("'",'&#39;',$title).(mb_strpos(mb_strtolower($title),'<span ')===false?'</span>':'').'':'').'</div></DIV>';
+			$return .= ',"div'.$name.'",true)\'><span class="underline-dots">'.(is_array($options[$value])?$options[$value][1]:$options[$value]).'</span>'.($title!=''?'<BR />'.(mb_strpos(mb_strtolower($title),'<span ')===false?'<span class="legend-gray">':'').str_replace("'",'&#39;',$title).(mb_strpos(mb_strtolower($title),'<span ')===false?'</span>':'').'':'').'</div></DIV>';
 		}
 		else
 			$return = $select;
@@ -263,12 +259,8 @@ function MLSelectInput($value,$name,$title='',$options,$allow_na='N/A',$extra=''
 
     if(AllowEdit() && !isset($_REQUEST['_ROSARIO_PDF']))
     {
-		$escape_array = array('&#39;','&quot;');
 		if($value!='' && $div)
-		{
-			$return = '<DIV id="div'.$name.'"><div class="onclick" onclick=\'javascript:addHTML("';
-			$escape_array = array('&#39;','');
-		}
+			$return = '<DIV id="div'.$name.'"><div class="onclick" onclick=\'javascript:addHTML(html'.str_replace(array('[',']'),'',$name);
 			
 		$select = '<SELECT name="'.$name.'" id="'.$name.'" '.$extra.'>';
 			
@@ -281,7 +273,7 @@ function MLSelectInput($value,$name,$title='',$options,$allow_na='N/A',$extra=''
             foreach($options as $key=>$val)
             {
                 $key .= '';
-                $select .= '<OPTION value="'.str_replace(array("'",'"'),$escape_array,$key).'"'.($value==$key && (!($value==false && $value!==$key) || ($value===0 && $key==='0'))?' SELECTED="SELECTED"':'').'>'.str_replace("'",'&#39;',(is_array($val)?ParseMLField($val[0], $locale):ParseMLField($val, $locale))).'</OPTION>';
+                $select .= '<OPTION value="'.htmlspecialchars($key).'"'.($value==$key && (!($value==false && $value!==$key) || ($value===0 && $key==='0'))?' SELECTED="SELECTED"':'').'>'.str_replace("'",'&#39;',(is_array($val)?ParseMLField($val[0], $locale):ParseMLField($val, $locale))).'</OPTION>';
             }
         }
         $select .= '</SELECT>';
@@ -290,9 +282,9 @@ function MLSelectInput($value,$name,$title='',$options,$allow_na='N/A',$extra=''
 			
         if($value!='' && $div)
 		{
-			$return .=  str_replace('"','\"',$select);
+			$return = '<script type="text/javascript">var html'.str_replace(array('[',']'),'',$name).'=\''.$select.'\';</script>'.$return;
 
-            $return .= '","div'.$name.'",true)\'><span class="underline-dots">'.ParseMLField((is_array($options[$value])?$options[$value][1]:$options[$value]), $locale).'</span>'.($title!=''?'<BR />'.(mb_strpos(mb_strtolower($title),'<span ')===false?'<span class="legend-gray">':'').str_replace("'",'&#39;',$title).(mb_strpos(mb_strtolower($title),'<span ')===false?'</span>':'').'':'').'</div></DIV>';
+            $return .= ',"div'.$name.'",true)\'><span class="underline-dots">'.ParseMLField((is_array($options[$value])?$options[$value][1]:$options[$value]), $locale).'</span>'.($title!=''?'<BR />'.(mb_strpos(mb_strtolower($title),'<span ')===false?'<span class="legend-gray">':'').str_replace("'",'&#39;',$title).(mb_strpos(mb_strtolower($title),'<span ')===false?'</span>':'').'':'').'</div></DIV>';
 		}
 		else
 			$return = $select;
@@ -313,12 +305,8 @@ function RadioInput($value,$name,$title='',$options,$allow_na='N/A',$extra='',$d
 
 	if(AllowEdit() && !isset($_REQUEST['_ROSARIO_PDF']))
 	{
-		$escape_array = array('&#39;','&quot;');
 		if($value!='' && $div)
-		{
-			$return = '<DIV id="div'.$name.'"><div class="onclick" onclick=\'javascript:addHTML("';
-			$escape_array = array('&#39;','');
-		}
+			$return = '<DIV id="div'.$name.'"><div class="onclick" onclick=\'javascript:addHTML(html'.str_replace(array('[',']'),'',$name);
 		
 		$table = '<TABLE class="cellpadding-0 cellspacing-0" '.$extra.'><TR class="center">';
 			
@@ -332,7 +320,7 @@ function RadioInput($value,$name,$title='',$options,$allow_na='N/A',$extra='',$d
 			foreach($options as $key=>$val)
 			{
 				$key .= '';
-				$table .= '<TD><label><INPUT type="radio" name="'.$name.'" value="'.str_replace(array("'",'"'),$escape_array,$key).'" '.($value==$key && (!($value==false && $value!==$key) || ($value==='0' && $key===0))?'checked':'').' /> '.str_replace("'",'&#39;',(is_array($val)?$val[0]:$val)).'</label></TD>';
+				$table .= '<TD><label><INPUT type="radio" name="'.$name.'" value="'.htmlspecialchars($key).'" '.($value==$key && (!($value==false && $value!==$key) || ($value==='0' && $key===0))?'checked':'').' /> '.str_replace("'",'&#39;',(is_array($val)?$val[0]:$val)).'</label></TD>';
 			}
 		}
 		$table .= '</TR></TABLE>';
@@ -341,9 +329,9 @@ function RadioInput($value,$name,$title='',$options,$allow_na='N/A',$extra='',$d
 			
 		if($value!='' && $div)
 		{
-			$return .=  str_replace('"','\"',$table);
+			$return = '<script type="text/javascript">var html'.str_replace(array('[',']'),'',$name).'=\''.$table.'\';</script>'.$return;
 
-            $return .= '","div'.$name.'",true)\'><span class="underline-dots">'.(is_array($options[$value])?$options[$value][1]:$options[$value]).'</span>'.($title!=''?'<BR />'.(mb_strpos(mb_strtolower($title),'<span ')===false?'<span class="legend-gray">':'').str_replace("'",'&#39;',$title).(mb_strpos(mb_strtolower($title),'<span ')===false?'</span>':'').'':'').'</div></DIV>';
+            $return .= ',"div'.$name.'",true)\'><span class="underline-dots">'.(is_array($options[$value])?$options[$value][1]:$options[$value]).'</span>'.($title!=''?'<BR />'.(mb_strpos(mb_strtolower($title),'<span ')===false?'<span class="legend-gray">':'').str_replace("'",'&#39;',$title).(mb_strpos(mb_strtolower($title),'<span ')===false?'</span>':'').'':'').'</div></DIV>';
 		}
 		else
 			$return = $table;
