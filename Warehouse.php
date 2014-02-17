930555504b7e49d45b3543a30d19ad1e29edd7ad
François Jacquet <user@example.com>
2014-02-17 15:16:49 -0500

    Version 2.1.1 update

--- Warehouse.php
+++ Warehouse.php
@@ -3,7 +3,7 @@
 if(!defined('WAREHOUSE_PHP'))
 {
 	define("WAREHOUSE_PHP",1);
-    $RosarioVersion = '2.1';
+    $RosarioVersion = '2.1.1';
 
     if (!file_exists ('config.inc.php'))
         die ('config.inc.php not found. Please read the configuration guide.');
