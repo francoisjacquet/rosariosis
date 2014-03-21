ff3a1d81cd6bc05450d1e114efae0264338f7ac6
François Jacquet <user@example.com>
2014-03-21 11:59:59 -0500

    Responsive Eligibility Student Screen

--- modules/Eligibility/Student.php
+++ modules/Eligibility/Student.php
@@ -96,8 +96,6 @@ if(UserStudentID() && !$_REQUEST['modfunc'])
 	DrawHeader('<SELECT name="start_date">'.$date_select.'</SELECT> '.SubmitButton(_('Go')));
 	echo '</FORM>';
 
-	echo '<div class="st">';
-	
 	$RET = DBGet(DBQuery("SELECT em.STUDENT_ID,em.ACTIVITY_ID,ea.TITLE,ea.START_DATE,ea.END_DATE FROM ELIGIBILITY_ACTIVITIES ea,STUDENT_ELIGIBILITY_ACTIVITIES em WHERE em.SYEAR='".UserSyear()."' AND em.STUDENT_ID='".UserStudentID()."' AND em.SYEAR=ea.SYEAR AND em.ACTIVITY_ID=ea.ID ORDER BY ea.START_DATE"),array('START_DATE'=>'ProperDate','END_DATE'=>'ProperDate'));
 
 	$activities_RET = DBGet(DBQuery("SELECT ID,TITLE FROM ELIGIBILITY_ACTIVITIES WHERE SYEAR='".UserSyear()."' AND SCHOOL_ID='".UserSchool()."'"));
@@ -119,13 +117,9 @@ if(UserStudentID() && !$_REQUEST['modfunc'])
 	ListOutput($RET,$columns,'Activity','Activities',$link);
 	echo '</FORM>';
 
-	echo '</div><div class="st">';
-	
 	$RET = DBGet(DBQuery("SELECT e.ELIGIBILITY_CODE,c.TITLE as COURSE_TITLE FROM ELIGIBILITY e,COURSES c,COURSE_PERIODS cp WHERE e.STUDENT_ID='".UserStudentID()."' AND e.SYEAR='".UserSyear()."' AND e.COURSE_PERIOD_ID=cp.COURSE_PERIOD_ID AND cp.COURSE_ID=c.COURSE_ID AND e.SCHOOL_DATE BETWEEN '$start_date' AND '$end_date'"),array('ELIGIBILITY_CODE'=>'_makeLower'));
 	$columns = array('COURSE_TITLE'=>_('Course'),'ELIGIBILITY_CODE'=>_('Grade'));
 	ListOutput($RET,$columns,'Course','Courses');
-	
-	echo '</div>';
 }
 
 function _makeLower($word)
