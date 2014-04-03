70b16091147f0834e42498f44f4ab4c29ae11bf4
François Jacquet <user@example.com>
2014-04-03 12:43:45 -0500

    Remove spaces

--- modules/Discipline/CategoryBreakdown.php
+++ modules/Discipline/CategoryBreakdown.php
@@ -259,7 +259,7 @@ if(empty($_REQUEST['modfunc']))
 	$advanced_link = ' <A HREF="Modules.php?modname='.$_REQUEST['modname'].'&modfunc=search&category_id='.$_REQUEST['category_id'].'&chart_type='.$_REQUEST['chart_type'].'&day_start='.$_REQUEST['day_start'].'&day_end='.$_REQUEST['day_end'].'&month_start='.$_REQUEST['month_start'].'&month_end='.$_REQUEST['month_end'].'&year_start='.$_REQUEST['year_start'].'&year_end='.$_REQUEST['year_end'].'&include_top=false">'._('Advanced').'</A>';
 
 	DrawHeader($select);
-	DrawHeader(' &nbsp; &nbsp; <B>'._('Report Timeframe').': </B>'.PrepareDate($start_date,'_start').' - '.PrepareDate($end_date,'_end').$advanced_link,SubmitButton(_('Go')));
+	DrawHeader('<B>'._('Report Timeframe').': </B>'.PrepareDate($start_date,'_start').' - '.PrepareDate($end_date,'_end').$advanced_link,SubmitButton(_('Go')));
 
 	echo '<BR />';
 	if($_REQUEST['category_id'])
