ad9c04dc0f654ee12a6bea8aad5659538e87b3b9
François Jacquet <user@example.com>
2014-04-28 13:20:52 -0500

    Help printing rework
    - create HelpPrint.php to unify code
    - use of translations

--- Help.php
+++ Help.php
@@ -151,54 +151,6 @@ else
 
 if(mb_strpos($_SERVER['PHP_SELF'],'Help.php'))
 {
-	switch(User('PROFILE'))
-	{
-		case 'admin':
-			$title = 'Administrator';
-		break;
-
-		case 'teacher':
-			$title = 'Teacher';
-		break;
-
-		case 'parent':
-			$title = 'Parent';
-		break;
-		
-		case 'parent':
-			$title = 'Student';
-		break;
-	}
-
-	$handle = PDFStart();
-	echo '<TABLE><TR><TD><IMG SRC="assets/themes/'.Preferences('THEME').'/logo.png" /></TD><TD><h1>'.sprintf(_('%s Handbook'),$title).'<BR />'.sprintf(_('version %s'),'1.0').'</h1></TD></TR></TABLE><HR>';
-
-	foreach($help as $program=>$value)
-	{
-		if(mb_strpos($program,'/'))
-		{
-			$modcat = str_replace('_',' ',mb_substr($program,0,mb_strpos($program,'/')));
-			
-			if (!$RosarioModules[str_replace(' ','_',$modcat)]) //module not activated
-				break;
-		
-			if($modcat!=$old_modcat)
-				echo '<div style="page-break-after: always;"></div><TABLE><TR><TD><IMG SRC="assets/icons/'.str_replace(' ','_',$modcat).'.png" class="headerIcon" /></TD><TD><h2>'.$modcat.'</h2></TD></TR></TABLE><HR>';
-			$old_modcat = $modcat;
-		}
-		$_REQUEST['modname'] = $program;
-		echo '<h3>';
-		if($program=='default')
-			echo ParseMLField(Config('TITLE')).' - '.sprintf(_('%s Handbook'),$title).'<BR />'.sprintf(_('version %s'),'1.0');
-		else
-			echo (ProgramTitle() == 'RosarioSIS' ? str_replace(' ','_',$program) : ProgramTitle());
-		echo '</h3>';
-		echo '<TABLE class="width-100p cellpadding-5"><TR><TD class="header2">';
-		if($student==true)
-			$value = str_replace('your child','yourself',str_replace('your child\'s','your',$value));
-		echo $value;
-		echo '</TD></TR></TABLE><BR />';
-	}
-	PDFStop($handle);
+	include('HelpPrint.php');
 }
 ?>
---
+++ HelpPrint.php
@@ -0,0 +1,51 @@
+<?php
+	switch(User('PROFILE'))
+	{
+		case 'admin':
+			$title = _('Administrator');
+		break;
+
+		case 'teacher':
+			$title = _('Teacher');
+		break;
+
+		case 'parent':
+			$title = _('Parent');
+		break;
+		
+		case 'parent':
+			$title = _('Student');
+		break;
+	}
+
+	$handle = PDFStart();
+	echo '<TABLE><TR><TD><IMG SRC="assets/themes/'.Preferences('THEME').'/logo.png" /></TD><TD><h1>'.sprintf(_('%s Handbook'),$title).'</h1></TD></TR></TABLE><HR>';
+
+	foreach($help as $program=>$value)
+	{
+		if(mb_strpos($program,'/'))
+		{
+			$modcat = str_replace('_',' ',mb_substr($program,0,mb_strpos($program,'/')));
+			
+			if (!$RosarioModules[str_replace(' ','_',$modcat)]) //module not activated
+				break;
+		
+			if($modcat!=$old_modcat)
+				echo '<div style="page-break-after: always;"></div><TABLE><TR><TD><h2><IMG SRC="assets/icons/'.str_replace(' ','_',$modcat).'.png" class="headerIcon" /> '._($modcat).'</h2></TD></TR></TABLE><HR>';
+			$old_modcat = $modcat;
+		}
+		$_REQUEST['modname'] = $program;
+		echo '<h3>';
+		if($program=='default')
+			echo ParseMLField(Config('TITLE')).' - '.sprintf(_('%s Handbook'),$title).'<BR />'.sprintf(_('version %s'),'1.0');
+		else
+			echo (ProgramTitle() == 'RosarioSIS' ? str_replace(' ','_',$program) : ProgramTitle());
+		echo '</h3>';
+		echo '<TABLE class="width-100p cellpadding-5"><TR><TD class="header2">';
+		if($student==true)
+			$value = str_replace('your child','yourself',str_replace('your child\'s','your',$value));
+		echo $value;
+		echo '</TD></TR></TABLE><BR />';
+	}
+	PDFStop($handle);
+?>
