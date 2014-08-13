9c667b7302dce861c7f43ac4fca3b3fe37d609d3
François Jacquet <user@example.com>
2014-08-13 18:19:20 -0500

    Add School submenu in School Setup module

--- modules/School_Setup/Menu.php
+++ modules/School_Setup/Menu.php
@@ -4,17 +4,18 @@ $menu['School_Setup']['admin'] = array(
 //modif Francois: Portal Polls
 						'School_Setup/PortalPolls.php'=>_('Portal Polls'),
 //modif Francois: add Database Backup
-						'School_Setup/DatabaseBackup.php' =>_('Database Backup'),
-						'School_Setup/Schools.php'=>_('School Information'),
-						'School_Setup/Schools.php&new_school=true'=>_('Add a School'),
-						'School_Setup/CopySchool.php'=>_('Copy School'),
-//modif Francois: add School Fields
-						'School_Setup/SchoolFields.php'=>_('School Fields'),
 						'School_Setup/MarkingPeriods.php'=>_('Marking Periods'),
 						'School_Setup/Calendar.php'=>_('Calendars'),
 						'School_Setup/Periods.php'=>_('Periods'),
 						'School_Setup/GradeLevels.php'=>_('Grade Levels'),
 						'School_Setup/Rollover.php'=>_('Rollover'),
+						'School_Setup/DatabaseBackup.php' =>_('Database Backup'),
+						1=>_('School'),
+						'School_Setup/Schools.php'=>_('School Information'),
+						'School_Setup/Schools.php&new_school=true'=>_('Add a School'),
+						'School_Setup/CopySchool.php'=>_('Copy School'),
+//modif Francois: add School Fields
+						'School_Setup/SchoolFields.php'=>_('School Fields'),
 //modif Francois: add School Configuration
 						'School_Setup/Configuration.php' =>_('School Configuration'),
 					);
