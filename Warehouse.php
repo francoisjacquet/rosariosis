140730861f61df989402033844c4a7c60ea76869
François Jacquet <user@example.com>
2014-08-13 18:48:49 -0500

    Version 1.4.12 update

--- Warehouse.php
+++ Warehouse.php
@@ -3,7 +3,7 @@
 if(!defined('WAREHOUSE_PHP'))
 {
 	define("WAREHOUSE_PHP",1);
-    $RosarioVersion = '1.4.11';
+    $RosarioVersion = '1.4.12';
 
     if (!file_exists ('config.inc.php'))
         die ('config.inc.php not found. Please read the configuration guide.');
