d05be14e49ddd021b31599c4245628ed4ee130c8
François Jacquet <user@example.com>
2014-09-01 16:22:49 -0500

    Remove modules/*/Search.php files

--- modules/Resources/Search.php
+++
@@ -1,9 +0,0 @@
-<?php
-$_REQUEST["modname"] = "Resources/Resources.php";
-
-if(AllowUse($_REQUEST['modname']))
-{
-	echo '<SCRIPT>modname="'.$_REQUEST['modname'].'"; openMenu(modname);</SCRIPT>';
-	include("modules/$_REQUEST[modname]");
-}
-?>
