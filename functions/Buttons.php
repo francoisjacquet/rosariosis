7af7151eba632c26a88124abf529cad058c0c551
François Jacquet <user@example.com>
2014-10-01 18:53:06 -0500

    Move Submit/ResetButton functions to Buttons.php

--- functions/Buttons.php
+++ functions/Buttons.php
@@ -28,4 +28,20 @@ function button($type,$text='',$link='',$height=18)
 
 	return $button;
 }
+
+function SubmitButton($value='Submit',$name='',$options='')
+{
+	if(AllowEdit())
+		return '<INPUT type="submit" value="'.$value.'"'.($name?' name="'.$name.'"':'').($options?' '.$options:'').' />';
+	else
+		return '';
+}
+
+function ResetButton($value='Reset',$options='')
+{
+	if(AllowEdit())
+		return '<INPUT type="reset" value="'.$value.'"'.($options?' '.$options:'').' />';
+	else
+		return '';
+}
 ?>
