de96b9c653573452a8e2c399faa05980d69e7e80
François Jacquet <user@example.com>
2014-10-01 18:36:40 -0500

    move BackPrompt function with *Prompt, rename file to Prompt.fnc.php

--- functions/Prompt.fnc.php
+++ functions/Prompt.fnc.php
@@ -40,4 +40,14 @@ function Prompt($title='Confirm',$question='',$message='',$pdf='')
 		return true;
 }
 
+function BackPrompt($message)
+{
+//modif Francois: errors not readable
+//	echo '<SCRIPT>alert("'.str_replace(array("'",'"'),array('&#39;','&quot;'),$message).'");self.history.go(-1);</SCRIPT>';
+	?>
+
+	<SCRIPT>alert(<?php echo json_encode($message); ?>);window.close();</SCRIPT>
+
+	<?php exit();
+}
 ?>
