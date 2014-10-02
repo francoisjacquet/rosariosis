84d3060a1c3668a75d57943cf5b352d8a68ac0b6
François Jacquet <user@example.com>
2014-10-02 17:13:36 -0500

    Bugfix PHP Notices undefined vars

--- functions/AllowEdit.fnc.php
+++ functions/AllowEdit.fnc.php
@@ -40,10 +40,10 @@ function AllowUse($modname=false)
 	if(!$modname)
 		$modname = $_REQUEST['modname'];
 
-	if(($modname=='Students/Student.php' || $modname=='Users/User.php') && $_REQUEST['category_id'])
+	if(($modname=='Students/Student.php' || $modname=='Users/User.php') && isset($_REQUEST['category_id']))
 		$modname = $modname.'&category_id='.$_REQUEST['category_id'];
 
-	if(!$_ROSARIO['AllowUse'])
+	if(!isset($_ROSARIO['AllowUse']))
 	{
 		if(User('PROFILE_ID')!='')
 			$_ROSARIO['AllowUse'] = DBGet(DBQuery("SELECT MODNAME FROM PROFILE_EXCEPTIONS WHERE PROFILE_ID='".User('PROFILE_ID')."' AND CAN_USE='Y'"),array(),array('MODNAME'));
