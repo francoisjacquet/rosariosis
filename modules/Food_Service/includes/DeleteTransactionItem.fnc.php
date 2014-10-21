9a046d2b499185a9d9be368aa563e4707eb07931
François Jacquet <francoisjacquet@users.noreply.github.com>
2014-10-21 12:03:26 -0500

    Bugfix if no more transaction items, delete transaction

--- modules/Food_Service/includes/DeleteTransactionItem.fnc.php
+++ modules/Food_Service/includes/DeleteTransactionItem.fnc.php
@@ -2,7 +2,7 @@
 
 function DeleteTransactionItem($transaction_id,$item_id,$type='student')
 {
-	if($_REQUEST['type']=='staff')
+	if($type=='staff')
 	{
 		$sql1 = "UPDATE FOOD_SERVICE_STAFF_TRANSACTIONS SET BALANCE=BALANCE-(SELECT AMOUNT FROM FOOD_SERVICE_STAFF_TRANSACTION_ITEMS WHERE TRANSACTION_ID='".$transaction_id."' AND ITEM_ID='".$item_id."') WHERE TRANSACTION_ID>='".$transaction_id."' AND STAFF_ID=(SELECT STAFF_ID FROM FOOD_SERVICE_STAFF_TRANSACTIONS WHERE TRANSACTION_ID='".$transaction_id."')";
 		$sql2 = "UPDATE FOOD_SERVICE_STAFF_ACCOUNTS SET BALANCE=BALANCE-(SELECT AMOUNT FROM FOOD_SERVICE_STAFF_TRANSACTION_ITEMS WHERE TRANSACTION_ID='".$transaction_id."' AND ITEM_ID='".$item_id."') WHERE STAFF_ID=(SELECT STAFF_ID FROM FOOD_SERVICE_STAFF_TRANSACTIONS WHERE TRANSACTION_ID='".$transaction_id."')";
@@ -17,8 +17,9 @@ function DeleteTransactionItem($transaction_id,$item_id,$type='student')
 	DBQuery('BEGIN; '.$sql1.'; '.$sql2.'; '.$sql3.'; COMMIT');
 	
 	//modif Francois: if no more transaction items, delete transaction
-	$trans_items_RET = DBGet(DBQuery("SELECT 1 FROM ".($_REQUEST['type']=='staff' ? "FOOD_SERVICE_STAFF_TRANSACTION_ITEMS" :  "FOOD_SERVICE_TRANSACTION_ITEMS")." WHERE TRANSACTION_ID='".$transaction_id."'"));
-	if (!is_null($trans_items_RET))
+	$trans_items_RET = DBGet(DBQuery("SELECT ITEM_ID FROM ".($type=='staff' ? "FOOD_SERVICE_STAFF_TRANSACTION_ITEMS" :  "FOOD_SERVICE_TRANSACTION_ITEMS")." WHERE TRANSACTION_ID='".$transaction_id."'"));
+	
+	if (empty($trans_items_RET))
 	{
 		require_once('modules/Food_Service/includes/DeleteTransaction.fnc.php');
 		DeleteTransaction($transaction_id,$type);
