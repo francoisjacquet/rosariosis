2b8ab4d5fb62e855cc6cbbcf975caf7a08c215ff
François Jacquet <francoisjacquet@users.noreply.github.com>
2014-11-10 16:17:18 -0500

    Example Resource
    Display # of Schools, Students, admins, teachers & parents
    Add translation

--- modules/Example/ExampleResource.php
+++ modules/Example/ExampleResource.php
@@ -13,28 +13,37 @@ DrawHeader(ProgramTitle()); //display main header with Module icon and Program t
 //get Resources from the database
 $QI = DBQuery("SELECT ID AS SCHOOL_ID, TITLE FROM SCHOOLS WHERE SYEAR='".UserSyear()."' ORDER BY ID");
 $schools_RET = DBGet($QI, array(), array('SCHOOL_ID'));
+
+//get the number of Students for each school
 $QI = DBQuery("SELECT SCHOOL_ID, COUNT(STUDENT_ID) AS STUDENT_NB FROM STUDENT_ENROLLMENT WHERE SYEAR='".UserSyear()."' GROUP BY SCHOOL_ID");
 $students_RET = DBGet($QI, array(), array('SCHOOL_ID'));
 
-$school_IDs = array_keys($schools_RET);
-
 $admins_RET = $teachers_RET = $parents_RET = array();
 
+$school_IDs = array_keys($schools_RET);
+
+//for each school
 foreach($school_IDs as $school_ID)
 {
+	//get the number of Administrators
 	$QI = DBQuery("SELECT ".$school_ID." AS SCHOOL_ID, COUNT(STAFF_ID) AS ADMIN_NB FROM STAFF WHERE SYEAR='".UserSyear()."'  AND profile='admin' AND (SCHOOLS LIKE '%,".$school_ID.",%' OR SCHOOLS IS NULL OR SCHOOLS='')");
+
 	$admins_RET = $admins_RET + DBGet($QI, array(), array('SCHOOL_ID'));
 
+	//get the number of Teachers
 	$QI = DBQuery("SELECT ".$school_ID." AS SCHOOL_ID, COUNT(STAFF_ID) AS TEACHER_NB FROM STAFF WHERE SYEAR='".UserSyear()."' AND profile='teacher' AND (SCHOOLS LIKE '%,".$school_ID.",%' OR SCHOOLS IS NULL OR SCHOOLS='')");
 	$teachers_RET = $teachers_RET + DBGet($QI, array(), array('SCHOOL_ID'));
-	
+
+	//get the number of Parents
 	$QI = DBQuery("SELECT ".$school_ID." AS SCHOOL_ID, COUNT(STAFF_ID) AS PARENT_NB FROM STAFF WHERE SYEAR='".UserSyear()."' AND profile='parent' AND (SCHOOLS LIKE '%,".$school_ID.",%' OR SCHOOLS IS NULL OR SCHOOLS='')");
 	$parents_RET = $parents_RET + DBGet($QI, array(), array('SCHOOL_ID'));
 }
 
+//build the Resources array for ListOutput
 $resources_RET = array();
-$i = 1;
+$i = 1; //the first key of the array should not be 0
 
+//for each school
 foreach($school_IDs as $school_ID)
 {
 	$resources_RET[$i] = $schools_RET[$school_ID][1];
@@ -45,14 +54,15 @@ foreach($school_IDs as $school_ID)
 	$i++;
 }
 
+//uncomment the following line to debug and see the Queries results
 //var_dump($schools_RET,$students_RET, $admins_RET, $teachers_RET, $parents_RET, $resources_RET);exit;
 
 //prepare ListOutput table options
 //see ListOutput.fnc.php for the complete list of options
-$columns = array('TITLE'=>_('Title'), 'STUDENT_NB'=>_('# of Students'), 'ADMIN_NB'=>_('# of Administrators'), 'TEACHER_NB'=>_('# of Teachers'), 'PARENT_NB'=>_('# of Parents'));
+$columns = array('TITLE'=>_('Title'), 'STUDENT_NB'=>dgettext('Example', '# of Students'), 'ADMIN_NB'=>dgettext('Example', '# of Administrators'), 'TEACHER_NB'=>dgettext('Example', '# of Teachers'), 'PARENT_NB'=>dgettext('Example', '# of Parents'));
 
 //display secondary header with text (aligned left)
-DrawHeader('This is the Resources program from the Example module. The program is the same but the source contains comments.');
+DrawHeader('This is the Example Resource program from the Example module.');
 
 ListOutput($resources_RET,$columns,'School','Schools');
 ?>
