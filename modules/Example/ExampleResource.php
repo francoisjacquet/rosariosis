2fca75619fef6d081c6bc6664fefaee994ff9d3e
François Jacquet <francoisjacquet@users.noreply.github.com>
2014-11-10 15:02:23 -0500

    Schools resources

--- modules/Example/ExampleResource.php
+++ modules/Example/ExampleResource.php
@@ -11,73 +11,48 @@ DrawHeader(ProgramTitle()); //display main header with Module icon and Program t
 
 
 //get Resources from the database
-$QI = DBQuery("SELECT TITLE FROM SCHOOLS WHERE SYEAR='".UserSyear()."' ORDER BY ID");
-$schools_RET = DBGet($QI);
-$QI = DBQuery("SELECT COUNT(STUDENT_ID) AS STUDENT_NB FROM STUDENT_ENROLLMENT WHERE SYEAR='".UserSyear()."' GROUP BY SCHOOL_ID");
-$students_RET = DBGet($QI);
-var_dump($schools_RET);exit;
+$QI = DBQuery("SELECT ID AS SCHOOL_ID, TITLE FROM SCHOOLS WHERE SYEAR='".UserSyear()."' ORDER BY ID");
+$schools_RET = DBGet($QI, array(), array('SCHOOL_ID'));
+$QI = DBQuery("SELECT SCHOOL_ID, COUNT(STUDENT_ID) AS STUDENT_NB FROM STUDENT_ENROLLMENT WHERE SYEAR='".UserSyear()."' GROUP BY SCHOOL_ID");
+$students_RET = DBGet($QI, array(), array('SCHOOL_ID'));
+
+$school_IDs = array_keys($schools_RET);
+
+$admins_RET = $teachers_RET = $parents_RET = array();
+
+foreach($school_IDs as $school_ID)
+{
+	$QI = DBQuery("SELECT ".$school_ID." AS SCHOOL_ID, COUNT(STAFF_ID) AS ADMIN_NB FROM STAFF WHERE SYEAR='".UserSyear()."'  AND profile='admin' AND (SCHOOLS LIKE '%,".$school_ID.",%' OR SCHOOLS IS NULL OR SCHOOLS='')");
+	$admins_RET = $admins_RET + DBGet($QI, array(), array('SCHOOL_ID'));
+
+	$QI = DBQuery("SELECT ".$school_ID." AS SCHOOL_ID, COUNT(STAFF_ID) AS TEACHER_NB FROM STAFF WHERE SYEAR='".UserSyear()."' AND profile='teacher' AND (SCHOOLS LIKE '%,".$school_ID.",%' OR SCHOOLS IS NULL OR SCHOOLS='')");
+	$teachers_RET = $teachers_RET + DBGet($QI, array(), array('SCHOOL_ID'));
+	
+	$QI = DBQuery("SELECT ".$school_ID." AS SCHOOL_ID, COUNT(STAFF_ID) AS PARENT_NB FROM STAFF WHERE SYEAR='".UserSyear()."' AND profile='parent' AND (SCHOOLS LIKE '%,".$school_ID.",%' OR SCHOOLS IS NULL OR SCHOOLS='')");
+	$parents_RET = $parents_RET + DBGet($QI, array(), array('SCHOOL_ID'));
+}
+
+$resources_RET = array();
+$i = 1;
+
+foreach($school_IDs as $school_ID)
+{
+	$resources_RET[$i] = $schools_RET[$school_ID][1];
+	$resources_RET[$i] = $resources_RET[$i] + $students_RET[$school_ID][1];
+	$resources_RET[$i] = $resources_RET[$i] + $admins_RET[$school_ID][1];
+	$resources_RET[$i] = $resources_RET[$i] + $teachers_RET[$school_ID][1];
+	$resources_RET[$i] = $resources_RET[$i] + $parents_RET[$school_ID][1];
+	$i++;
+}
+
+//var_dump($schools_RET,$students_RET, $admins_RET, $teachers_RET, $parents_RET, $resources_RET);exit;
 
 //prepare ListOutput table options
 //see ListOutput.fnc.php for the complete list of options
-$columns = array('TITLE'=>_('Title'),'LINK'=>_('Link'));
-$link['add']['html'] = array('TITLE'=>_makeTextInput('','TITLE'),'LINK'=>_makeLink('','LINK'));
-$link['remove']['link'] = 'Modules.php?modname='.$_REQUEST['modname'].'&modfunc=remove';
-$link['remove']['variables'] = array('id'=>'ID');
+$columns = array('TITLE'=>_('Title'), 'STUDENT_NB'=>_('# of Students'), 'ADMIN_NB'=>_('# of Administrators'), 'TEACHER_NB'=>_('# of Teachers'), 'PARENT_NB'=>_('# of Parents'));
 
-//form used to send the new / updated Resources to be processed by the same script (see at the top)
-echo '<FORM action="Modules.php?modname='.$_REQUEST['modname'].'&modfunc=update" method="POST">';
+//display secondary header with text (aligned left)
+DrawHeader('This is the Resources program from the Example module. The program is the same but the source contains comments.');
 
-//display secondary header with text (aligned left) and Save button (aligned right)
-DrawHeader('This is the Resources program from the Example module. The program is the same but the source contains comments.',SubmitButton(_('Save'))); //SubmitButton is diplayed only if AllowEdit
-
-ListOutput($resources_RET,$columns,'Resource','Resources',$link);
-echo '<span class="center">'.SubmitButton(_('Save')).'</span>'; //SubmitButton is diplayed only if AllowEdit
-echo '</FORM>';
-
-//local function called by DBGet
-//begin function name with an underscore "_" when it is local
-function _makeTextInput($value,$name)
-{	global $THIS_RET; //current returned data row
-	
-	if($THIS_RET['ID'])
-		$id = $THIS_RET['ID'];
-	else
-		$id = 'new';
-	
-	if($name=='LINK')
-		$extra = 'maxlength=1000';
-		
-	if($name=='TITLE')
-		$extra = 'maxlength=256';
-
-	return TextInput($value,'values['.$id.']['.$name.']','',$extra); //call TextInput function from functions/Inputs.php file
-}
-
-//local function called by DBGet
-//begin function name with an underscore "_" when it is local
-function _makeLink($value,$name)
-{	global $THIS_RET; //current returned data row
-
-	if (AllowEdit()) //if AllowEdit, display TextInput
-	{
-		if($value)
-			return '<div style="display:table-cell;"><a href="'.$value.'" target="_blank">'._('Link').'</a>&nbsp;</div><div style="display:table-cell;">'._makeTextInput($value,$name).'</div>';
-		else
-			return _makeTextInput($value,$name);
-	}
-	
-	//truncate links > 100 chars
-	$truncated_link = $value;
-	if (mb_strlen($truncated_link) > 100)
-	{
-		$separator = '/.../';
-		$separatorlength = mb_strlen($separator) ;
-		$maxlength = 100 - $separatorlength;
-		$start = $maxlength / 2 ;
-		$trunc =  mb_strlen($truncated_link) - $maxlength;
-		$truncated_link = substr_replace($truncated_link, $separator, $start, $trunc);
-	}
-	 
-	return '<a href="'.$value.'" target="_blank">'.$truncated_link.'</a>';
-}
+ListOutput($resources_RET,$columns,'School','Schools');
 ?>
