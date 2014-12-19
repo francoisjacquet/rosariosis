eeca4c7107720de1727bc9859219b362b996c259
François Jacquet <francoisjacquet@users.noreply.github.com>
2014-12-19 15:33:21 -0500

    Explicit RosarioLocales array example

--- config.inc.sample.php
+++ config.inc.sample.php
@@ -13,7 +13,6 @@ if(!defined('CONFIG_INC'))
 	// Server Names and Paths
 	$RosarioPath = dirname(__FILE__).'/';
 	$pg_dumpPath = '/usr/bin/pg_dump'; // Specify the path to the database dump utility for this server.
-	//modif Francois: wkhtmltopdf
 	$wkhtmltopdfPath = '/usr/bin/wkhtmltopdf'; // empty string means wkhtmltopdf will not be called and reports will be rendered in html instead of pdf
 	$wkhtmltopdfAssetsPath = '/var/www/rosariosis/assets/'; // way wkhtmltopdf accesses the assets/ directory, empty string means no translation
 	$StudentPicturesPath = 'assets/StudentPhotos/';
@@ -24,7 +23,7 @@ if(!defined('CONFIG_INC'))
 
 	$DefaultSyear = '2014'; // Default school year, should match the database to be able to login
 	$RosarioNotifyAddress = ''; // email address to send error and new administrator notifications
-	$RosarioLocales = array('en_US.utf8'); // Add other languages you want to support here, ex: 'fr_FR.utf8', 'es_ES.utf8', ...
+	$RosarioLocales = array('en_US.utf8'); // Add other languages you want to support here, ex: array('en_US.utf8', 'fr_FR.utf8', 'es_ES.utf8');
 	$CurrencySymbol = '$'; // local currency
 
 }
