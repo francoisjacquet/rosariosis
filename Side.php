f30c8e1aac57b7dbb52b37d8faa7facd53fb4415
François Jacquet <francoisjacquet@users.noreply.github.com>
2015-02-02 10:39:40 -0500

    Update program according to new School/SchoolYear/CoursePeriod
    if current User/Student not in new SchoolYear, remove, else update ID
    if student not scheduled in new CoursePeriod, remove
    if current School/SchoolYear changed from menu, reset Admin & Teacher's CoursePeriod

--- Side.php
+++ Side.php
@@ -5,45 +5,47 @@ include('Warehouse.php');
 
 $old_school = UserSchool();
 $old_syear = UserSyear();
-$old_period = UserCoursePeriod();
+$old_period = UserCoursePeriod().'.'.UserCoursePeriodSchoolPeriod();
 
 $addJavascripts = '';
 
-//change current School/Syear/CoursePeriod/MarkingPeriod/Student from menu
+//change current School/SchoolYear/MarkingPeriod/CoursePeriod/Student from menu
 if(isset($_REQUEST['sidefunc']) && $_REQUEST['sidefunc']=='update' && is_array($_POST))
 {
-	//update current School
-	if(isset($_POST['school']) && $_POST['school']!=$old_school)
+	//update Admin & Teachers's current School
+	if ((User('PROFILE')=='admin' || User('PROFILE')=='teacher') && isset($_POST['school']) && $_POST['school']!=$old_school)
 	{
 		unset($_SESSION['student_id']);
 		$_SESSION['unset_student'] = true;
-		unset($_SESSION['staff_id']);
-		unset($_SESSION['UserMP']);
-		unset($_POST['mp']);
 
-		if((User('PROFILE')=='admin' || User('PROFILE')=='teacher'))
-		{
-			$_SESSION['UserSchool'] = $_POST['school'];
-			DBQuery("UPDATE STAFF SET CURRENT_SCHOOL_ID='".UserSchool()."' WHERE STAFF_ID='".User('STAFF_ID')."'");
-		}
+		unset($_SESSION['staff_id']);
+
+		$_SESSION['UserSchool'] = $_POST['school'];
+		DBQuery("UPDATE STAFF SET CURRENT_SCHOOL_ID='".UserSchool()."' WHERE STAFF_ID='".User('STAFF_ID')."'");
+
+		//reset current MarkingPeriod
+		$_SESSION['UserMP'] = GetCurrentMP('QTR',DBDate(),false);
 	}
-	//update current Syear
+	//update current SchoolYear
 	elseif (isset($_POST['syear']) && $_POST['syear']!=$old_syear)
 	{
 		$_SESSION['UserSyear'] = $_POST['syear'];
 
-		//if current User, update user ID according to new Syear OR remove if does not exist
+		//reset current MarkingPeriod
+		$_SESSION['UserMP'] = GetCurrentMP('QTR',DBDate(),false);
+
+		//if current User, update user ID according to new SchoolYear OR remove if does not exist
 		if((User('PROFILE')=='admin' || User('PROFILE')=='teacher') && UserStaffID())
 		{
-			//search User in next Syear
+			//search User in next SchoolYear
 			if ($old_syear == UserSyear() - 1)
 				$new_staff_id_RET = DBGet(DBQuery("SELECT STAFF_ID FROM STAFF WHERE ROLLOVER_ID='".UserStaffID()."'"));
-			//search User in previous Syear
+			//search User in previous SchoolYear
 			elseif ($old_syear == UserSyear() + 1)
 				$new_staff_id_RET = DBGet(DBQuery("SELECT ROLLOVER_ID AS STAFF_ID FROM STAFF WHERE STAFF_ID='".UserStaffID()."'"));
-			//more than 1 year difference, remove User
+			//more than 1 year difference, search User by USERNAME... (could have changed)
 			else
-				$new_staff_id_RET = null;
+				$new_staff_id_RET = DBGet(DBQuery("SELECT STAFF_ID FROM STAFF WHERE USERNAME=(SELECT USERNAME FROM STAFF WHERE STAFF_ID='".UserStaffID()."') AND SYEAR='".UserSyear()."'"));
 
 			if(count($new_staff_id_RET))
 			{
@@ -56,99 +58,148 @@ if(isset($_REQUEST['sidefunc']) && $_REQUEST['sidefunc']=='update' && is_array($
 			else
 			{
 				unset($_SESSION['staff_id']);
+
+				//remove staff_id from URL
 				unset($_SESSION['_REQUEST_vars']['staff_id']);
 			}
 		}
 
-		//if current Student not enrolled in new Syear, remove
+		//if current Student not enrolled in new SchoolYear, remove
 		if(in_array(User('PROFILE'), array('admin', 'teacher', 'parent')) && UserStudentID())
 		{
-			$is_student_enrolled = DBGet(DBQuery("SELECT 'ENROLLED' FROM STUDENT_ENROLLMENT WHERE SYEAR='".UserSyear()."' AND STUDENT_ID='".UserStudentID()."'"));
+			if(User('PROFILE') == 'teacher')
+				//if student scheduled in first CoursePeriod
+				$sql_is_student_enrolled = "SELECT 'SCHEDULED'
+				FROM SCHEDULE
+				WHERE STUDENT_ID='".UserStudentID()."'
+				AND COURSE_PERIOD_ID=(SELECT COURSE_PERIOD_ID
+					FROM COURSE_PERIODS
+					WHERE SYEAR='".UserSyear()."' 
+					AND SCHOOL_ID='".UserSchool()."' 
+					AND TEACHER_ID='".User('STAFF_ID')."' 
+					AND MARKING_PERIOD_ID IN (".GetAllMP('QTR',UserMP()).")
+					ORDER BY SHORT_NAME
+					LIMIT 1)
+				AND '".DBDate()."'>=START_DATE
+				AND ('".DBDate()."'<=END_DATE OR END_DATE IS NULL)";
+			else
+				$sql_is_student_enrolled = "SELECT 'ENROLLED' FROM STUDENT_ENROLLMENT WHERE SYEAR='".UserSyear()."' AND SCHOOL_ID='".UserSchool()."' AND STUDENT_ID='".UserStudentID()."'";
 
-			//remove Student if not enrolled in new Syear
+			$is_student_enrolled = DBGet(DBQuery($sql_is_student_enrolled));
+
+			//remove Student if not enrolled in new SchoolYear
 			if(!count($is_student_enrolled))
 			{
 				unset($_SESSION['student_id']);
-				unset($_SESSION['_REQUEST_vars']['student_id']);
 				$_SESSION['unset_student'] = true;
+
+				//remove student_id from URL
+				unset($_SESSION['_REQUEST_vars']['student_id']);
 			}
 		}
-	}
-	//update current CoursePeriod
-	elseif (isset($_POST['period']) && $_POST['period']!=$old_period)
-	{
-		$_SESSION['UserCoursePeriod'] = $_POST['period'];
 	}
 	//update current MarkingPeriod
 	elseif (isset($_POST['mp']) && $_POST['mp']!=$_SESSION['UserMP'])
 	{
 		$_SESSION['UserMP'] = $_POST['mp'];
 	}
-	//update current Student for Parents
-	elseif(User('PROFILE')=='parent' && isset($_POST['student_id']) && UserStudentID()!=$_POST['student_id'])
+	//update Teacher's current CoursePeriod
+	elseif (User('PROFILE') == 'teacher' && isset($_POST['period']) && $_POST['period']!=$old_period)
 	{
-		unset($_SESSION['UserMP']);
+		list($_SESSION['UserCoursePeriod'], $_SESSION['UserCoursePeriodSchoolPeriod']) = explode('.', $_POST['period']);
 
+		if(UserStudentID())
+		{
+			$is_student_scheduled = DBGet(DBQuery("SELECT 'SCHEDULED'
+			FROM SCHEDULE
+			WHERE STUDENT_ID='".UserStudentID()."'
+			AND COURSE_PERIOD_ID='".UserCoursePeriod()."'
+			AND '".DBDate()."'>=START_DATE
+			AND ('".DBDate()."'<=END_DATE OR END_DATE IS NULL)"));
+
+			//if student not scheduled in new CoursePeriod, remove
+			if (!count($is_student_scheduled))
+			{
+				unset($_SESSION['student_id']);
+				$_SESSION['unset_student'] = true;
+
+				//remove student_id from URL
+				unset($_SESSION['_REQUEST_vars']['student_id']);
+			}
+		}
+	}
+	//update Parent's current Student
+	elseif (User('PROFILE')=='parent' && isset($_POST['student_id']) && UserStudentID()!=$_POST['student_id'])
+	{
 		SetUserStudentID($_POST['student_id']);
 	}
 
-	//update "body" Module page
+	//if current School/SchoolYear changed from menu, reset Admin & Teacher's CoursePeriod
+	if((UserSchool()!=$old_school || UserSyear()!=$old_syear) && (User('PROFILE')=='admin' || User('PROFILE')=='teacher'))
+	{
+		unset($_SESSION['UserPeriod']);
+		unset($_SESSION['UserCoursePeriod']);
+	}
+
+	//update "#body" Module page
 	$addJavascripts .= 'var body_link = document.createElement("a"); body_link.href = "'.str_replace('&amp;','&',PreparePHP_SELF($_SESSION['_REQUEST_vars'])).'"; body_link.target = "body"; ajaxLink(body_link);';
 }
-//set current Syear/Student/User/School/MarkingPeriod after login
-if(!UserSyear())
-	$_SESSION['UserSyear'] = Config('SYEAR');
-
-if(!UserStudentID() && User('PROFILE')=='student')
-	SetUserStudentID($_SESSION['STUDENT_ID']);
-
-if(!UserStaffID() && User('PROFILE')=='parent')
-	SetUserStaffID($_SESSION['STAFF_ID']);
-
-if(!UserSchool())
+//set current SchoolYear/Student/User/School/MarkingPeriod after login
+else
 {
-	if((User('PROFILE')=='admin' || User('PROFILE')=='teacher') && (!User('SCHOOLS') || mb_strpos(User('SCHOOLS'),','.User('CURRENT_SCHOOL_ID').',')!==false))
-		$_SESSION['UserSchool'] = User('CURRENT_SCHOOL_ID');
-	elseif(User('PROFILE')=='student')
-		$_SESSION['UserSchool'] = trim(User('SCHOOLS'),',');
-}
 
-if((!UserMP() || (isset($_POST['school']) && $_POST['school']!=$old_school) || (isset($_POST['syear']) && $_POST['syear']!=$old_syear)) && User('PROFILE')!='parent')
-	$_SESSION['UserMP'] = GetCurrentMP('QTR',DBDate(),false);
+	if(!UserSyear())
+		$_SESSION['UserSyear'] = Config('SYEAR');
 
-if((isset($_POST['school']) && $_POST['school']!=$old_school) || (isset($_POST['syear']) && $_POST['syear']!=$old_syear))
-{
-	unset($_SESSION['UserPeriod']);
-	unset($_SESSION['UserCoursePeriod']);
-}
+	if(!UserStudentID() && User('PROFILE')=='student')
+		SetUserStudentID($_SESSION['STUDENT_ID']);
 
-//Adding a Student/User OR removing current Student/User from menu
-if((User('PROFILE')=='admin' || User('PROFILE')=='teacher'))
-{
-	$new_student = isset($_REQUEST['student_id']) && $_REQUEST['student_id']=='new';
-	$new_user = isset($_REQUEST['staff_id']) && $_REQUEST['staff_id']=='new';
+	if(!UserStaffID() && User('PROFILE')=='parent')
+		SetUserStaffID($_SESSION['STAFF_ID']);
 
-	if($new_student || $new_user)
+	if(!UserSchool())
 	{
-		if ($new_student)
-		{
-			unset($_SESSION['student_id']);
-			unset($_SESSION['_REQUEST_vars']['student_id']);
-		}
-		elseif($new_user)
-		{
-			unset($_SESSION['staff_id']);
-			unset($_SESSION['_REQUEST_vars']['staff_id']);
-		}
+		if((User('PROFILE')=='admin' || User('PROFILE')=='teacher') && (!User('SCHOOLS') || mb_strpos(User('SCHOOLS'),','.User('CURRENT_SCHOOL_ID').',')!==false))
+			$_SESSION['UserSchool'] = User('CURRENT_SCHOOL_ID');
+		elseif(User('PROFILE')=='student')
+			$_SESSION['UserSchool'] = trim(User('SCHOOLS'),',');
+	}
 
-		unset($_SESSION['_REQUEST_vars']['search_modfunc']);
+	if(!UserMP())
+		$_SESSION['UserMP'] = GetCurrentMP('QTR',DBDate(),false);
 
-		//update "body" Module page
-		$addJavascripts .= 'var body_link = document.createElement("a"); body_link.href = "'.str_replace('&amp;','&',PreparePHP_SELF($_SESSION['_REQUEST_vars'],array('advanced'))).'"; body_link.target = "body"; ajaxLink(body_link);';
+	//Adding a Student/User OR removing current Student/User from menu
+	if(User('PROFILE')=='admin' || User('PROFILE')=='teacher')
+	{
+		$new_student = isset($_REQUEST['student_id']) && $_REQUEST['student_id']=='new';
+		$new_user = isset($_REQUEST['staff_id']) && $_REQUEST['staff_id']=='new';
+
+		if($new_student || $new_user)
+		{
+			if ($new_student)
+			{
+				unset($_SESSION['student_id']);
+				$_SESSION['unset_student'] = true;
+
+				//remove student_id from URL
+				unset($_SESSION['_REQUEST_vars']['student_id']);
+			}
+			elseif($new_user)
+			{
+				unset($_SESSION['staff_id']);
+
+				//remove staff_id from URL
+				unset($_SESSION['_REQUEST_vars']['staff_id']);
+			}
+
+			unset($_SESSION['_REQUEST_vars']['search_modfunc']);
+
+			//update "#body" Module page
+			$addJavascripts .= 'var body_link = document.createElement("a"); body_link.href = "'.str_replace('&amp;','&',PreparePHP_SELF($_SESSION['_REQUEST_vars'],array('advanced'))).'"; body_link.target = "body"; ajaxLink(body_link);';
+		}
 	}
 }
-
-//set menu Student/User/School/CoursePeriod, verify if have been changed in Modules.php
+//set menu Student/User/School/CoursePeriod, verify if have been changed in Warehouse.php
 $addJavascripts .= 'var menuStudentID = "'.UserStudentID().'"; var menuStaffID = "'.UserStaffID().'"; var menuSchool = "'.UserSchool().'"; var menuCoursePeriod = "'.UserCoursePeriod().'";';
 ?>
 
@@ -162,13 +213,12 @@ $addJavascripts .= 'var menuStudentID = "'.UserStudentID().'"; var menuStaffID =
 			<span class="br-after">&nbsp;<b><?php echo User('NAME'); ?></b></span>
 			&nbsp;<?php echo mb_convert_case(iconv('','UTF-8',strftime('%A %B %d, %Y')), MB_CASE_TITLE, "UTF-8"); ?><BR />
 
-			<?php if(User('PROFILE')=='admin' || User('PROFILE')=='teacher') :
+			<?php // School SELECT (Admins & Teachers only)
+			if(User('PROFILE')=='admin' || User('PROFILE')=='teacher') :
 			
 				$schools = mb_substr(str_replace(",","','",User('SCHOOLS')),2,-2);
-				$QI = DBQuery("SELECT ID,TITLE,SHORT_NAME FROM SCHOOLS WHERE SYEAR='".UserSyear()."'".($schools?" AND ID IN (".$schools.")":''));
-				$RET = DBGet($QI);
-				
-				
+				$RET = DBGet(DBQuery("SELECT ID,TITLE,SHORT_NAME FROM SCHOOLS WHERE SYEAR='".UserSyear()."'".($schools?" AND ID IN (".$schools.")":'')));
+
 				if(!UserSchool())
 				{
 					$_SESSION['UserSchool'] = $RET[1]['ID'];
@@ -187,6 +237,7 @@ $addJavascripts .= 'var menuStudentID = "'.UserStudentID().'"; var menuStaffID =
 
 			<?php endif;
 
+			// Student SELECT (Parents only)
 			if(User('PROFILE')=='parent') :
 			
 				$RET = DBGet(DBQuery("SELECT sju.STUDENT_ID,s.LAST_NAME||', '||s.FIRST_NAME AS FULL_NAME,se.SCHOOL_ID 
@@ -222,17 +273,16 @@ $addJavascripts .= 'var menuStudentID = "'.UserStudentID().'"; var menuStaffID =
 					$schools_RET = DBGet(DBQuery("SELECT ID,TITLE FROM SCHOOLS WHERE SYEAR='".UserSyear()."' LIMIT 1"));
 					$_SESSION['UserSchool'] = $schools_RET[1]['ID'];
 				}
-				if(!UserMP() || UserSchool()!=$old_school || UserSyear()!=$old_syear) :
-					$_SESSION['UserMP'] = GetCurrentMP('QTR',DBDate(),false);
-				endif;
 			endif;
 
+			// SchoolYear SELECT
 			if(User('PROFILE')!='student')
 				$sql = "SELECT sy.SYEAR FROM SCHOOLS sy,STAFF s WHERE sy.ID='".UserSchool()."' AND s.SYEAR=sy.SYEAR AND (s.SCHOOLS IS NULL OR position(','||sy.ID||',' IN s.SCHOOLS)>0) AND s.USERNAME=(SELECT USERNAME FROM STAFF WHERE STAFF_ID='".$_SESSION['STAFF_ID']."')";
 			else
 				//modif Francois: limit school years to the years the student was enrolled
 				//$sql = "SELECT DISTINCT sy.SYEAR FROM SCHOOLS sy,STUDENT_ENROLLMENT s WHERE s.SYEAR=sy.SYEAR";
 				$sql = "SELECT DISTINCT sy.SYEAR FROM SCHOOLS sy,STUDENT_ENROLLMENT s WHERE s.SYEAR=sy.SYEAR AND s.STUDENT_ID='".UserStudentID()."'";
+
 			$sql .= " ORDER BY sy.SYEAR DESC";
 			$years_RET = DBGet(DBQuery($sql)); ?>
 
@@ -246,7 +296,7 @@ $addJavascripts .= 'var menuStudentID = "'.UserStudentID().'"; var menuStaffID =
 
 			</SELECT></span>
 
-			<?php
+			<?php // MarkingPeriod SELECT
 			$RET = DBGet(DBQuery("SELECT MARKING_PERIOD_ID,TITLE FROM SCHOOL_MARKING_PERIODS WHERE MP='QTR' AND SCHOOL_ID='".UserSchool()."' AND SYEAR='".UserSyear()."' ORDER BY SORT_ORDER"));
 
 			if(User('PROFILE')=='teacher') : ?>
@@ -268,7 +318,7 @@ $addJavascripts .= 'var menuStudentID = "'.UserStudentID().'"; var menuStaffID =
 					$mp_array[] = $quarter['MARKING_PERIOD_ID'];			
 				endforeach;
 				
-				//modif Francois: update UserMP if invalid
+				//modif Francois: reset UserMP if invalid
 				if(!UserMP() || !in_array(UserMP(), $mp_array))
 				{
 					$_SESSION['UserMP'] = $RET[1]['MARKING_PERIOD_ID'];
@@ -281,14 +331,17 @@ $addJavascripts .= 'var menuStudentID = "'.UserStudentID().'"; var menuStaffID =
 
 			</SELECT>
 
-			<?php if(User('PROFILE')=='teacher') : ?>
+			<?php // CoursePeriod SELECT (Teachers only)
+			if(User('PROFILE')=='teacher') : ?>
 
 			</span>
 
-			<?php endif;
+			<?php	//modif Francois: error if no quarters
+				if (!count($RET))
+					$all_MP = '0';
+				else
+					$all_MP = GetAllMP('QTR',UserMP());
 
-			if(User('PROFILE')=='teacher') :
-			
 				//modif Francois: multiple school periods for a course period
 				//$QI = DBQuery("SELECT cp.PERIOD_ID,cp.COURSE_PERIOD_ID,sp.TITLE,sp.SHORT_NAME,cp.MARKING_PERIOD_ID,cp.DAYS,c.TITLE AS COURSE_TITLE FROM COURSE_PERIODS cp, SCHOOL_PERIODS sp,COURSES c WHERE c.COURSE_ID=cp.COURSE_ID AND cp.PERIOD_ID=sp.PERIOD_ID AND cp.SYEAR='".UserSyear()."' AND cp.SCHOOL_ID='".UserSchool()."' AND cp.TEACHER_ID='".User('STAFF_ID')."' AND cp.MARKING_PERIOD_ID IN (".GetAllMP('QTR',UserMP()).") ORDER BY sp.SORT_ORDER");
 				$QI = DBQuery("SELECT cpsp.PERIOD_ID,cp.COURSE_PERIOD_ID,cpsp.COURSE_PERIOD_SCHOOL_PERIODS_ID,sp.TITLE,sp.SHORT_NAME,cp.MARKING_PERIOD_ID,cpsp.DAYS,c.TITLE AS COURSE_TITLE, cp.SHORT_NAME AS CP_SHORT_NAME 
@@ -299,7 +352,7 @@ $addJavascripts .= 'var menuStudentID = "'.UserStudentID().'"; var menuStaffID =
 				AND cp.SYEAR='".UserSyear()."' 
 				AND cp.SCHOOL_ID='".UserSchool()."' 
 				AND cp.TEACHER_ID='".User('STAFF_ID')."' 
-				AND cp.MARKING_PERIOD_ID IN (".GetAllMP('QTR',UserMP()).") 
+				AND cp.MARKING_PERIOD_ID IN (".$all_MP.")
 				ORDER BY cp.SHORT_NAME, sp.SORT_ORDER");
 				
 				$RET = DBGet($QI);
@@ -307,19 +360,11 @@ $addJavascripts .= 'var menuStudentID = "'.UserStudentID().'"; var menuStaffID =
 				// get the fy marking period id, there should be exactly one fy marking period per school
 				$fy_RET = DBGet(DBQuery("SELECT MARKING_PERIOD_ID FROM SCHOOL_MARKING_PERIODS WHERE MP='FY' AND SYEAR='".UserSyear()."' AND SCHOOL_ID='".UserSchool()."'"));
 
-				if(isset($_POST['period']))
-				{
-					list($CoursePeriod, $CoursePeriodSchoolPeriod) = explode('.', $_POST['period']);
-					$_SESSION['UserCoursePeriod'] = $CoursePeriod;
-					$_SESSION['UserCoursePeriodSchoolPeriod'] = $CoursePeriodSchoolPeriod;
-				}
-
+				//set current CoursePeriod after login
 				if(!UserCoursePeriod() && isset($RET[1]))
 				{
 					$_SESSION['UserCoursePeriod'] = $RET[1]['COURSE_PERIOD_ID'];
 					$_SESSION['UserCoursePeriodSchoolPeriod'] = $RET[1]['COURSE_PERIOD_SCHOOL_PERIODS_ID'];
-					unset($_SESSION['student_id']);
-					$_SESSION['unset_student'] = true;
 				} ?>
 
 				<SELECT name="period" onChange="ajaxPostForm(this.form,true);">
@@ -330,7 +375,7 @@ $addJavascripts .= 'var menuStudentID = "'.UserStudentID().'"; var menuStaffID =
 					//modif Francois: add optroup to group periods by course periods
 					if (!empty($period['COURSE_TITLE']) && $optgroup!=$period['COURSE_TITLE']) : //new optgroup ?>
 
-						<optgroup label="<?php echo $optgroup = $period['COURSE_TITLE']; ?>">
+						<optgroup label="<?php echo htmlspecialchars($optgroup = $period['COURSE_TITLE']); ?>">
 
 					<?php endif;
 					
@@ -366,10 +411,15 @@ $addJavascripts .= 'var menuStudentID = "'.UserStudentID().'"; var menuStaffID =
 
 					<?php
 				}
+
+				//error: reset current CoursePeriod and remove current student
 				if(!$found)
 				{
 					$_SESSION['UserCoursePeriod'] = $RET[1]['COURSE_PERIOD_ID'];
 					$_SESSION['UserPeriod'] = $RET[1]['PERIOD_ID'];
+
+					unset($_SESSION['student_id']);
+					$_SESSION['unset_student'] = true;
 				} ?>
 
 				</SELECT>
