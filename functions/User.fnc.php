d13287ba6ac15108b137d049271868bd378de264
François Jacquet <francoisjacquet@users.noreply.github.com>
2015-02-02 11:03:01 -0500

    Rollback Fake STAFF_ID for Registration

--- functions/User.fnc.php
+++ functions/User.fnc.php
@@ -31,6 +31,9 @@ function User($item)
 			if($_ROSARIO['User'][1]['SCHOOL_ID']!=UserSchool())
 				$_SESSION['UserSchool'] = $_ROSARIO['User'][1]['SCHOOL_ID'];
 		}
+		//modif Francois: create account
+		elseif(basename($_SERVER['PHP_SELF'])=='index.php')
+			return false;
 		else
 			exit('Error');
 	}
