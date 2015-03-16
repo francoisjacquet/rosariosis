846821a6e7ff7e27bf9a5df73486e3927cdf0324
François Jacquet <francoisjacquet@users.noreply.github.com>
2015-03-16 18:50:07 -0500

    Beautify + JSHint

--- assets/js/warehouse.js
+++ assets/js/warehouse.js
@@ -1,243 +1,228 @@
 //Modules.php JS
 var locked;
-function addHTML(html,id,replace){
-	if(locked!=false){
-		if(replace==true) document.getElementById(id).innerHTML = html;
+
+function addHTML(html, id, replace) {
+	if (locked !== false) {
+		if (replace === true) document.getElementById(id).innerHTML = html;
 		else document.getElementById(id).innerHTML = document.getElementById(id).innerHTML + html;
 	}
 }
-function checkAll(form,value,name_like){
-	for(i=0;i<form.elements.length;i++){
+
+function checkAll(form, value, name_like) {
+	for (i = 0; i < form.elements.length; i++) {
 		chk = form.elements[i];
-		if(chk.type=='checkbox' && chk.name.substr(0,name_like.length)==name_like)
-			chk.checked = value;
+		if (chk.type == 'checkbox' && chk.name.substr(0, name_like.length) == name_like) chk.checked = value;
 	}
 }
-function switchMenu(el){
+
+function switchMenu(el) {
 	var $table = $(el).nextAll('table').first();
 	var $arrow = $(el).children('.arrow').first();
-	if($table.css('display')=='none')
-		$arrow.removeClass('right').addClass('down');
-	else
-		$arrow.removeClass('down').addClass('right');
+	if ($table.css('display') == 'none') $arrow.removeClass('right').addClass('down');
+	else $arrow.removeClass('down').addClass('right');
 	$table.toggle();
 }
 
 //IE8 HTML5 tags fix
-var tags='article|aside|footer|header|hgroup|nav|section'.split('|'), i=0, max=tags.length;
-for(;i<max;i++) {
-    document.createElement(tags[i]);
+var tags = 'article|aside|footer|header|hgroup|nav|section'.split('|'),
+	i = 0,
+	max = tags.length;
+for (; i < max; i++) {
+	document.createElement(tags[i]);
 }
 
 //touchScroll, enables overflow:auto on mobile
 //https://gist.github.com/chrismbarr/4107472
-function touchScroll(el){
-	var startY=startX=0;
+function touchScroll(el) {
+	var startY = 0,
+		startX = 0;
 
-	el.addEventListener("touchstart", function(e) {
-		startY=this.scrollTop+e.touches[0].pageY;
-		startX=this.scrollLeft+e.touches[0].pageX;
-	},false);
+	el.addEventListener("touchstart", function (e) {
+		startY = this.scrollTop + e.touches[0].pageY;
+		startX = this.scrollLeft + e.touches[0].pageX;
+	}, false);
 
-	el.addEventListener("touchmove", function(e) {
-		var tch=e.touches[0];
-		if ((this.scrollTop < this.scrollHeight-this.offsetHeight &&
-			this.scrollTop+tch.pageY < startY-5) ||
-			(this.scrollTop != 0 && this.scrollTop+tch.pageY > startY+5))
-				e.preventDefault(); 
-		if ((this.scrollLeft < this.scrollWidth-this.offsetWidth &&
-			this.scrollLeft+tch.pageX < startX-5) ||
-			(this.scrollLeft != 0 && this.scrollLeft+tch.pageX > startX+5))
-				e.preventDefault(); 
-		this.scrollTop=startY-tch.pageY;
-		this.scrollLeft=startX-tch.pageX;
-	},false);
+	el.addEventListener("touchmove", function (e) {
+		var tch = e.touches[0];
+		if ((this.scrollTop < this.scrollHeight - this.offsetHeight && this.scrollTop + tch.pageY < startY - 5) || (this.scrollTop !== 0 && this.scrollTop + tch.pageY > startY + 5)) e.preventDefault();
+		if ((this.scrollLeft < this.scrollWidth - this.offsetWidth && this.scrollLeft + tch.pageX < startX - 5) || (this.scrollLeft !== 0 && this.scrollLeft + tch.pageX > startX + 5)) e.preventDefault();
+		this.scrollTop = startY - tch.pageY;
+		this.scrollLeft = startX - tch.pageX;
+	}, false);
 }
-function isTouchDevice(){
-	try{
+
+function isTouchDevice() {
+	try {
 		document.createEvent("TouchEvent");
 		return true;
-	}catch(e){
+	} catch (e) {
 		return false;
 	}
 }
-if (isTouchDevice())
-	$(document).bind("cbox_complete", function(){ touchScroll(document.getElementById("cboxLoadedContent")); });
+if (isTouchDevice()) $(document).bind("cbox_complete", function () {
+	touchScroll(document.getElementById("cboxLoadedContent"));
+});
 
-function ajaxOptions(target,url,form)
-{
+function ajaxOptions(target, url, form) {
 	return {
-		beforeSend: function(data){
-			$('#BottomSpinner').css('visibility','visible');
+		beforeSend: function (data) {
+			$('#BottomSpinner').css('visibility', 'visible');
 		},
-		success: function(data){
-			if(form && form.method=='get')
-				url = url+(url.indexOf('?')!=-1 ? '&' : '?')+$(form).formSerialize();
-			ajaxSuccess(data,target,url);
+		success: function (data) {
+			if (form && form.method == 'get') url = url + (url.indexOf('?') != -1 ? '&' : '?') + $(form).formSerialize();
+			ajaxSuccess(data, target, url);
 		},
-		error: function(x,st,err){
-			alert("ajax get Status: "+st+" - Error: "+err+" - URL: "+url);
+		error: function (x, st, err) {
+			alert("ajax get Status: " + st + " - Error: " + err + " - URL: " + url);
 		},
-		complete: function(){
-			$('#BottomSpinner').css('visibility','hidden');
+		complete: function () {
+			$('#BottomSpinner').css('visibility', 'hidden');
 
 			hideHelp();
 		}
 	};
 }
 
-function ajaxLink(link){	
+function ajaxLink(link) {
 	//will work only if in the onclick there is no error!
 	var target = link.target;
-	if (link.href.indexOf('#')!=-1 || target=='_blank' || target=='_top') //internal/external/index.php anchor
+	if (link.href.indexOf('#') != -1 || target == '_blank' || target == '_top') //internal/external/index.php anchor
 		return true;
-	if (!target)
-	{
-		if (link.href.indexOf('Modules.php')!=-1)
-			target = 'body';
-		else
-			return true;
+	if (!target) {
+		if (link.href.indexOf('Modules.php') != -1) target = 'body';
+		else return true;
 	}
 
-	$.ajax(link.href, ajaxOptions(target,link.href,false));
+	$.ajax(link.href, ajaxOptions(target, link.href, false));
 	return false;
 }
 
-function ajaxPostForm(form,submit){
+function ajaxPostForm(form, submit) {
 	var target = form.target;
-	if (!target)
-			target = 'body';
-	if (form.action.indexOf('_ROSARIO_PDF')!=-1) //print PDF
+	if (!target) target = 'body';
+	if (form.action.indexOf('_ROSARIO_PDF') != -1) //print PDF
 	{
 		form.target = '_blank';
 		form.method = 'post';
 		return true;
 	}
 
-	var options = ajaxOptions(target,form.action,form);
-	if (submit)
-		$(form).ajaxSubmit(options);
-	else
-		$(form).ajaxForm(options);
+	var options = ajaxOptions(target, form.action, form);
+	if (submit) $(form).ajaxSubmit(options);
+	else $(form).ajaxForm(options);
 	return false;
 }
 
-function ajaxSuccess(data,target,url){
+function ajaxSuccess(data, target, url) {
 	//change URL after AJAX
 	//http://stackoverflow.com/questions/5525890/how-to-change-url-after-an-ajax-request#5527095
-	$('#'+target).html(data);
-	
-	if (history.pushState && target == 'body')
-		history.pushState(null, document.title, url);
+	$('#' + target).html(data);
 
-	ajaxPrepare('#'+target);
+	if (history.pushState && target == 'body') history.pushState(null, document.title, url);
+
+	ajaxPrepare('#' + target);
 }
 
-function ajaxPrepare(target){
-	if (scrollTop=='Y' && target)
-		body.scrollIntoView();
+function ajaxPrepare(target) {
+	if (scrollTop == 'Y' && target) body.scrollIntoView();
 
-	$(target+' form').each(function(){ ajaxPostForm(this,false); });
-	$(target+' a').click(function(e){ return $(this).css('pointer-events')=='none' ? e.preventDefault() : ajaxLink(this); });
+	$(target + ' form').each(function () {
+		ajaxPostForm(this, false);
+	});
+	$(target + ' a').click(function (e) {
+		return $(this).css('pointer-events') == 'none' ? e.preventDefault() : ajaxLink(this);
+	});
 
-	if (target == '#menu' && window.modname)
-		openMenu(modname);
+	if (target == '#menu' && window.modname) openMenu(modname);
 
-	if (isTouchDevice())
-		$('.rt').each(function(i,e){
-			touchScroll(e.tBodies[0]);
-		});
+	if (isTouchDevice()) $('.rt').each(function (i, e) {
+		touchScroll(e.tBodies[0]);
+	});
 
 	var h3 = $('#body h3.title').text();
-	document.title = $('#body h2').text()+(h3 ? ' | '+h3 : '');
+	document.title = $('#body h2').text() + (h3 ? ' | ' + h3 : '');
 }
 
 //disable links while AJAX
-$(document).ajaxStart(function(){
-	$('input[type="submit"],input[type="button"],a').css('pointer-events','none').attr('disabled',true);
+$(document).ajaxStart(function () {
+	$('input[type="submit"],input[type="button"],a').css('pointer-events', 'none').attr('disabled', true);
 });
-$(document).ajaxStop(function(){
-	$('input[type="submit"],input[type="button"],a').css('pointer-events','').attr('disabled',false);
+$(document).ajaxStop(function () {
+	$('input[type="submit"],input[type="button"],a').css('pointer-events', '').attr('disabled', false);
 });
 
 //onload
-window.onload = function(){
+window.onload = function () {
 	ajaxPrepare('');
 
 	//reload page after browser history
-	if (history.pushState)
-		window.setTimeout(function() {
-			window.addEventListener('popstate', function (e) {
-				document.location.href = document.URL;
-			}, false);
-		}, 1);
+	if (history.pushState) window.setTimeout(function () {
+		window.addEventListener('popstate', function (e) {
+			document.location.href = document.URL;
+		}, false);
+	}, 1);
 };
 
 //Side.php JS
 var old_modcat = false;
-var menu_link = document.createElement("a"); menu_link.href = "Side.php"; menu_link.target = "menu";
+var menu_link = document.createElement("a");
+menu_link.href = "Side.php";
+menu_link.target = "menu";
 
-function openMenu(modname)
-{
-	if (modname!='misc/Portal.php')
-	{
-		if (oldA = document.getElementById("selectedMenuLink"))
-			oldA.id = "";
-		$('.wp-submenu a[href$="'+modname+'"]:first').each(function(){this.id = "selectedMenuLink";});
+function openMenu(modname) {
+	if (modname != 'misc/Portal.php') {
+		if ((oldA = document.getElementById("selectedMenuLink"))) oldA.id = "";
+		$('.wp-submenu a[href$="' + modname + '"]:first').each(function () {
+			this.id = "selectedMenuLink";
+		});
 		//add selectedModuleLink
-		if (oldA = document.getElementById("selectedModuleLink"))
-			oldA.id = "";
+		if ((oldA = document.getElementById("selectedModuleLink"))) oldA.id = "";
 
 		var modcat;
-		if (modname=='')
-			modcat = old_modcat;
-		else
-			$('#selectedMenuLink').parents('div.wp-submenu').each(function(){ modcat = this.id.replace('menu_', ''); });
+		if (modname === '') modcat = old_modcat;
+		else $('#selectedMenuLink').parents('div.wp-submenu').each(function () {
+			modcat = this.id.replace('menu_', '');
+		});
 
-		$('a[href*="'+modcat+'"].menu-top').each(function(){this.id = "selectedModuleLink";});
-		
-		$("#menu_"+modcat).show();
-		if(old_modcat!=false && old_modcat!=modcat)
-			$("#menu_"+old_modcat).hide();
+		$('a[href*="' + modcat + '"].menu-top').each(function () {
+			this.id = "selectedModuleLink";
+		});
+
+		$("#menu_" + modcat).show();
+		if (old_modcat !== false && old_modcat != modcat) $("#menu_" + old_modcat).hide();
 		old_modcat = modcat;
-	}
-	else if(old_modcat!=false)
-		$("#menu_"+old_modcat).hide();
+	} else if (old_modcat !== false) $("#menu_" + old_modcat).hide();
 }
 
 //Bottom.php JS
-function toggleHelp(){
-	if ($('#footerhelp').css('display') !== 'block')
-		showHelp();
-	else
-		hideHelp();
+function toggleHelp() {
+	if ($('#footerhelp').css('display') !== 'block') showHelp();
+	else hideHelp();
 }
 
-var old_modname=false;
-function showHelp(){
-	if (modname!==old_modname)
-	{
-		$.get("Bottom.php?modfunc=help&modname="+modname, function(data){
+var old_modname = false;
+
+function showHelp() {
+	if (modname !== old_modname) {
+		$.get("Bottom.php?modfunc=help&modname=" + modname, function (data) {
 			$('#footerhelp').html(data);
-			if (isTouchDevice())
-				touchScroll(document.getElementById('footerhelp'));
-		})
-		.fail(function(){
-			alert('Error: expandHelp '+modname);
-		})
+			if (isTouchDevice()) touchScroll(document.getElementById('footerhelp'));
+		}).fail(function () {
+			alert('Error: expandHelp ' + modname);
+		});
 		old_modname = modname;
 	}
 	$('#footerhelp').show();
-	$('#footer').css('height', function(i, val){
+	$('#footer').css('height', function (i, val) {
 		return parseInt(val) + parseInt($('#footerhelp').css('height'));
 	});
 }
 
-function hideHelp(){
+function hideHelp() {
 	$('#footerhelp').hide();
 	$('#footer').css('height', '');
 }
 
-function expandMenu(){
+function expandMenu() {
 	$('#menu,#menuback').toggle();
 }
