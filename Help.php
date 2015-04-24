ae0a6afe5e34f5044c65ddc7efb1a924b29f68ad
François Jacquet <francoisjacquet@users.noreply.github.com>
2015-04-24 17:27:47 -0500

    Formatted code and simplified it a bit

--- Help.php
+++ Help.php
@@ -1,109 +1,150 @@
 <?php
-error_reporting(0);
-include('Warehouse.php');
 
-$help_translated = 'Help_'.mb_substr($locale, 0, 2).'.php';
+error_reporting(0);
+
+include( 'Warehouse.php' );
+
+$help_translated = 'Help_' . mb_substr( $locale, 0, 2 ) . '.php';
 $help_english = 'Help_en.php';
 
-if (file_exists($help_translated)) //FJ translated help
-	include($help_translated);
+if ( file_exists( $help_translated ) ) //FJ translated help
+	include( $help_translated );
+
 else
-	include($help_english);
+	include( $help_english );
 
 //FJ add help for non-core modules
-$not_core_modules = array_diff(array_keys($RosarioModules),$RosarioCoreModules);
+$not_core_modules = array_diff( array_keys( $RosarioModules ), $RosarioCoreModules );
 
-foreach($not_core_modules as $not_core_module)
+foreach( $not_core_modules as $not_core_module )
 {
-	$not_core_dir = 'modules/'.$not_core_module.'/';
+	$not_core_dir = 'modules/' . $not_core_module . '/';
 
-	if (file_exists($not_core_dir.$help_translated)) //FJ translated help
-		include($not_core_dir.$help_translated);
-	elseif (file_exists($not_core_dir.$help_english))
-		include($not_core_dir.$help_english);
+	if ( file_exists( $not_core_dir . $help_translated ) ) //FJ translated help
+		include( $not_core_dir . $help_translated );
+
+	elseif ( file_exists( $not_core_dir . $help_english ) )
+		include( $not_core_dir . $help_english );
 }
 
-switch(User('PROFILE'))
+switch( User( 'PROFILE' ) )
 {
 	case 'admin':
-		$title = _('Administrator');
+		$title = _( 'Administrator' );
 	break;
 
 	case 'teacher':
-		$title = _('Teacher');
+		$title = _( 'Teacher' );
 	break;
 
 	case 'parent':
-		$title = _('Parent');
+		$title = _( 'Parent' );
 	break;
 	
 	case 'student':
-		$title = _('Student');
+		$title = _( 'Student' );
 	break;
 }
 
 $handle = PDFStart();
 ?>
+
 <TABLE>
 	<TR>
-		<TD><IMG SRC="assets/themes/<?php echo Preferences('THEME'); ?>/logo.png" /></TD>
-		<TD><h1><?php echo $_REQUEST['modname'] = sprintf(_('%s Handbook'),$title); ?></h1></TD>
+		<TD>
+			<IMG SRC="assets/themes/<?php echo Preferences( 'THEME' ); ?>/logo.png" />
+		</TD>
+		<TD>
+			<h1>&nbsp;<?php echo sprintf( _( '%s Handbook' ), $title ); ?></h1>
+		</TD>
 	</TR>
 </TABLE>
-<HR>
+<HR />
+
 <?php
-foreach($help as $program=>$value)
+foreach( $help as $program => $value )
 {
 	$_REQUEST['modname'] = $program;
-	if(mb_strpos($program,'/'))
+
+	if( mb_strpos( $program, '/' ) )
 	{
-		$modcat = str_replace('_',' ',mb_substr($program,0,mb_strpos($program,'/')));
-		
-		if (!$RosarioModules[str_replace(' ','_',$modcat)]) //module not activated
+		$modcat = mb_substr( $program, 0, mb_strpos( $program, '/' ) );
+
+		if ( !$RosarioModules[$modcat] ) //module not activated
 			break;
 	
-		if($modcat!=$old_modcat && $modcat!='Custom') : ?>
+		if( $modcat != $old_modcat
+			&& $modcat != 'Custom' ) : ?>
 
 			<div style="page-break-after: always;"></div>
 
 <?php
-			unset($_ROSARIO['DrawHeader']);
-			$_ROSARIO['HeaderIcon'] = 'modules/'.str_replace(' ','_',$modcat).'/icon.png';
-			echo DrawHeader(_($modcat));
-?>
-			<HR>
+			unset( $_ROSARIO['DrawHeader'] );
 
-		<?php endif;
-		if ($modcat!='Custom')
+			$_ROSARIO['HeaderIcon'] = 'modules/' . $modcat . '/icon.png';
+
+			$modcat_echo = str_replace( '_', ' ',  $modcat );
+
+			echo DrawHeader( _( $modcat_echo ) );
+?>
+			<HR />
+
+		<?php
+		endif;
+
+		if ( $modcat != 'Custom' )
 			$old_modcat = $modcat;
 	}
-	 ?>
+?>
 
-	<div style="page-break-inside: avoid;"><h3>
+<div style="page-break-inside: avoid;">
+	<h3>
 
-	<?php if($program=='default')
-		echo ParseMLField(Config('TITLE')).' - '.sprintf(_('%s Handbook'),$title).'<BR />'.sprintf(_('version %s'),'1.1');
+<?php
+	if( $program == 'default' )
+		echo ParseMLField( Config( 'TITLE' ) )
+			. ' - ' . sprintf( _( '%s Handbook' ), $title ) . '<BR />'
+			. sprintf( _( 'version %s' ), '1.1' );
+
 	else
-		echo (ProgramTitle() == 'RosarioSIS' ? $program : ProgramTitle());
-	?>
+		echo ( ProgramTitle() == 'RosarioSIS' ? $program : ProgramTitle() );
+?>
 
 	</h3>
-	<TABLE class="width-100p"><TR><TD class="header2">
-
-	<?php if(User('PROFILE') == 'student')
-		$value = str_replace('your child','yourself',str_replace('your child\'s','your',$value));
-	$value = str_replace('RosarioSIS', Config('NAME'),$value);
-	echo $value; ?>
-
-	</TD></TR></TABLE>
-	</div>
-	<BR />
+	<TABLE class="width-100p">
+		<TR>
+			<TD class="header2">
 
 <?php
-}
+	if( User( 'PROFILE' ) == 'student' )
+		$value = str_replace(
+			'your child',
+			'yourself',
+			str_replace( 'your child\'s', 'your', $value ) );
+
+	$value = str_replace( 'RosarioSIS', Config( 'NAME' ), $value );
+
+	echo $value;
 ?>
-<div style="text-align:center;font-weight:bold;"><a href="http://www.rosariosis.org/">http://www.rosariosis.org/</a></div>
+
+			</TD>
+		</TR>
+	</TABLE>
+</div>
+<BR />
+
 <?php
+} //end foreach
+?>
+
+<div style="text-align: center; font-weight: bold;">
+	<a href="http://www.rosariosis.org/">http://www.rosariosis.org/</a>
+</div>
+
+<?php
+
 $_REQUEST['modname'] = '';
-PDFStop($handle);
+
+PDFStop( $handle );
+
 ?>
