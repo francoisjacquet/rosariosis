befb8890301572047107a2defcc7ca214d2fc701
François Jacquet <francoisjacquet@users.noreply.github.com>
2015-05-02 18:09:04 -0500

    Reorganize & comment code

--- ProgramFunctions/HackingLog.fnc.php
+++ ProgramFunctions/HackingLog.fnc.php
@@ -1,27 +1,61 @@
 <?php
-//FJ create HackingLog function to centralize code
+
+/**
+ * Log Hacking attempt
+ * Send email to $RosarioNotifyAddress if set
+ *
+ * @return outputs error message and exit
+ */
 function HackingLog()
 {
-	global $RosarioNotifyAddress, $RosarioVersion;
+	global $RosarioNotifyAddress,
+		$_ROSARIO;
 	
-	if($RosarioNotifyAddress)
+	// Send email to $RosarioNotifyAddress if set
+	if( filter_var( $RosarioNotifyAddress, FILTER_VALIDATE_EMAIL ) )
 	{
-		if (isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+		if ( isset( $_SERVER['HTTP_X_FORWARDED_FOR'] ) )
 			$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
 		else
 			$ip = $_SERVER['REMOTE_ADDR'];
 
 		//FJ add SendEmail function
-		include('ProgramFunctions/SendEmail.fnc.php');
+		include( 'ProgramFunctions/SendEmail.fnc.php' );
 		
-		$message = "INSERT INTO HACKING_LOG (HOST_NAME,IP_ADDRESS,LOGIN_DATE,VERSION,PHP_SELF,DOCUMENT_ROOT,SCRIPT_NAME,MODNAME,QUERY_STRING,USERNAME) 
-values('".$_SERVER['SERVER_NAME']."','".$ip."','".date('Y-m-d')."','".$RosarioVersion."','".$_SERVER['PHP_SELF']."','".$_SERVER['DOCUMENT_ROOT']."','".$_SERVER['SCRIPT_NAME']."','".$_REQUEST['modname']."','".$_SERVER['QUERY_STRING']."','".User('USERNAME')."')";
+		$message = "INSERT INTO HACKING_LOG
+			(
+				HOST_NAME,
+				IP_ADDRESS,
+				LOGIN_DATE,
+				VERSION,
+				PHP_SELF,
+				DOCUMENT_ROOT,
+				SCRIPT_NAME,
+				MODNAME,
+				QUERY_STRING,
+				USERNAME
+			)
+			values(
+				'" . $_SERVER['SERVER_NAME'] . "',
+				'" . $ip . "',
+				'" . date( 'Y-m-d' ) . "',
+				'" . $_ROSARIO['version'] . "',
+				'" . $_SERVER['PHP_SELF'] . "',
+				'" . $_SERVER['DOCUMENT_ROOT'] . "',
+				'" . $_SERVER['SCRIPT_NAME'] . "',
+				'" . $_REQUEST['modname'] . "',
+				'" . $_SERVER['QUERY_STRING'] . "',
+				'" . User( 'USERNAME' ) . "'
+			)";
 		
-		SendEmail($RosarioNotifyAddress,'HACKING ATTEMPT', $message);
+		SendEmail( $RosarioNotifyAddress, 'HACKING ATTEMPT', $message );
 	}
 
-	$error[] = _('You\'re not allowed to use this program!').' '._('This attempted violation has been logged and your IP address was captured.');
+	$error[] = _('You\'re not allowed to use this program!') . ' ' .
+		_('This attempted violation has been logged and your IP address was captured.');
 
-	return ErrorMessage($error, 'fatal');
+	// display error message and exit
+	return ErrorMessage( $error, 'fatal' );
 }
+
 ?>
