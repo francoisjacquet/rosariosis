feb0a7aca8e3477d56c60209e8d9269fadd83f51
François Jacquet <francoisjacquet@users.noreply.github.com>
2015-05-02 19:35:36 -0500

    Comment & format code

--- functions/PopTable.fnc.php
+++ functions/PopTable.fnc.php
@@ -1,25 +1,37 @@
 <?php
 
-// DRAWS A TABLE WITH A BLUE TAB, SURROUNDING SHADOW
-// REQUIRES A TITLE
+/**
+ * PopTable
+ *
+ * @param string $action    'header' or 'footer'
+ * @param string $title     PopTable Tab(s) title(s)
+ * @param string $table_att <table [attributes]>
+ *
+ * @return  outputs PopTable HTML
+ */
+function PopTable( $action, $title = 'Search', $table_att = '' )
+{
+	global $_ROSARIO;
 
-function PopTable($action,$title='Search',$table_att='')
-{	global $_ROSARIO;
-
-	if($action=='header')
+	if( $action == 'header' )
 	{
-//FJ css WPadmin
-		echo '<TABLE class="postbox cellspacing-0" '.$table_att.'>';
-		echo '<THEAD><TR><TH class="center">';
-		if(is_array($title))
-			echo WrapTabs($title,$_ROSARIO['selected_tab']);
+		echo '<table class="postbox cellspacing-0" ' . $table_att . '>
+			<thead><tr><th class="center">';
+
+		// multiple Tabs
+		if( is_array( $title ) )
+			echo WrapTabs( $title, $_ROSARIO['selected_tab'] );
+		// one Tab
 		else
-			echo DrawTab($title);
-		echo '</TH></TR></THEAD><TBODY><TR><TD class="popTable">';
+			echo DrawTab( $title );
+
+		echo '</th></tr></thead>
+			<tbody><tr><td class="popTable">';
 	}
-	elseif($action=='footer')
+	elseif( $action == 'footer' )
 	{
-		echo '</TD></TR></TBODY></TABLE>';
+		echo '</td></tr></tbody></table>';
 	}
 }
+
 ?>
