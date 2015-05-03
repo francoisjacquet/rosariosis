3f475b344aa7dd125e92cabe06e7c8c32556b4e4
François Jacquet <francoisjacquet@users.noreply.github.com>
2015-05-02 19:36:55 -0500

    Comment & format & simplify code

--- functions/Tabs.php
+++ functions/Tabs.php
@@ -1,68 +1,62 @@
 <?php
 
-function WrapTabs($tabs,$selected='',&$selected_key='')
+/**
+ * Wrap PopTable Tabs
+ *
+ * @param array $tabs           Tabs, titles + links
+ * @param string $selected      Selected Tab link
+ *
+ * @return string PopTable Tabs HTML
+ */
+function WrapTabs( $tabs, $selected = '' )
 {
-		$row = 0;
-     	$characters = 0;
-//FJ css WPadmin
-	if(count($tabs))
+	$tabs_html = array();
+
+	foreach( (array)$tabs as $key => $tab )
 	{
-		$rows[0] = '<div class="h3multi">';
-		foreach($tabs as $key=>$tab)
+		if( $tab['link'] == PreparePHP_SELF()
+			|| $tab['link'] == $selected )
 		{
-			if(mb_substr($tab['title'],0,1)!='<')
-				$tab_len = mb_strlen($tab['title']);
-			else
-				$tab_len = 0;
-
-			if($tab['link']==PreparePHP_SELF() || $tab['link']==$selected)
-			{
-				$rows[$row] .= '<!--BOTTOM-->'.'<span class="h3selected">' . DrawTab($tab['title'],$tab['link'],true) . '</span>';
-				$selected_key = $key;
-			}
-			else
-				$rows[$row] .= DrawTab($tab['title'],$tab['link']);
-
-			$characters += $tab_len + 6;
-		}
-	}
-	$rows[$row] .= "\n\n";
-
-	$i = 0;
-	$row_count = count($rows) - 1;
-
-	for($key=$row_count;$key>=0;$key--)
-	{
-//FJ remove ereg
-//		if(!ereg("<!--BOTTOM-->",$rows[$key]))
-		if(mb_strpos($rows[$key],"<!--BOTTOM-->")===FALSE)
-		{
-			$table .= $rows[$key];
-			$i++;
+			$selected_tab = true;
 		}
 		else
-			$bottom = $key;
-	}
-	$table .= $rows[$bottom] . '</div>';
+			$selected_tab = false;
 
-	return $table;
+		$tabs_html[] = DrawTab( $tab['title'], $tab['link'], $selected_tab );
+	}
+
+	$return = '<div class="h3multi">' . implode( $tabs_html ) . '</div>';
+
+	return $return;
 }
 
-//FJ remove DrawTab params
-//function DrawTab($title,$link='',$tabcolor='',$textcolor='#FFFFFF',$type='',$rollover='')
-function DrawTab($title,$link='',$h3selected=false)
-{
-    $title = ParseMLField($title);
-	if(mb_substr($title,0,1)!='<')
-		$title = str_replace(" ","&nbsp;",$title);
-/*	if(!$tabcolor)
-		$tabcolor = Preferences('HEADER');*/
 
-//FJ css WPadmin
-	if($link && !isset($_REQUEST['_ROSARIO_PDF']))
-		$block_table .= '<h3'.($h3selected ? ' class="title"' : '').'><A HREF="'.$link.'" class="BoxHeading" id="tab_link['.preg_replace('/[^a-zA-Z0-9]/','_',$link).']">'._($title).'</A></h3>';
+/**
+ * Draw PopTable tab
+ *
+ * @param string  $title      Tab title
+ * @param string  $link       Tab link
+ * @param boolean $selected   Selected Tab
+ *
+ * @return  string Tab HTML
+ */
+function DrawTab( $title, $link = '', $selected = false )
+{
+	$title = ParseMLField( $title );
+
+	// non breaking spaces in title
+	if( mb_substr( $title, 0, 1) != '<' )
+		$title = str_replace( ' ', '&nbsp;', $title );
+
+	// link if not printing PDF
+	if( $link && !isset( $_REQUEST['_ROSARIO_PDF'] ) )
+		$block_table = '<h3' . ( $selected ? ' class="h3selected"' : '' ) . '>
+			<a href="' . $link . '">'
+			. _( $title )
+			. '</a>
+			</h3>';
 	else
-		$block_table .= '<h3 class="title">'.$title.'</h3>';
+		$block_table = '<h3>' . $title . '</h3>';
 		
 	return $block_table;
 }
