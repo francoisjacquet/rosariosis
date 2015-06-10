127f463e3f61c4c31c317d9949c04b4cf03597b5
François Jacquet <francoisjacquet@users.noreply.github.com>
2015-06-10 17:15:02 +0200

    2015-2016 School Year update

--- config.inc.sample.php
+++ config.inc.sample.php
@@ -15,7 +15,7 @@ if(!defined('CONFIG_INC'))
 	$pg_dumpPath = '/usr/bin/pg_dump'; // Specify the path to the database dump utility for this server.
 	$wkhtmltopdfPath = '/usr/bin/wkhtmltopdf'; // empty string means wkhtmltopdf will not be called and reports will be rendered in html instead of pdf
 
-	$DefaultSyear = '2014'; // Default school year, should match the database to be able to login
+	$DefaultSyear = '2015'; // Default school year, should match the database to be able to login
 	$RosarioNotifyAddress = ''; // email address to send error and new administrator notifications
 	$RosarioLocales = array('en_US.utf8'); // Add other languages you want to support here, ex: array('en_US.utf8', 'fr_FR.utf8', 'es_ES.utf8');
 
