bad9594ac3cc81ce05fa54d8a41f2d8a985191da
François Jacquet <francoisjacquet@users.noreply.github.com>
2015-06-18 18:07:51 +0200

    Comment & format Menu.php

--- Menu.php
+++ Menu.php
@@ -1,58 +1,106 @@
 <?php
-if(empty($_ROSARIO['Menu']))
+
+/**
+ * Generate Menu entries
+ *
+ * Depending on:
+ * Activated modules
+ * User profile & exceptions
+ *
+ * Save it in $_ROSARIO['Menu'] global var
+ */
+if ( empty( $_ROSARIO['Menu'] ) )
 {
-	if (is_null($RosarioModules))
+	if ( is_null( $RosarioModules ) )
 		global $RosarioModules;
-	
-	foreach($RosarioModules as $module=>$include)
-		if($include)
-			include('modules/'.$module.'/Menu.php');
 
-	$profile = User('PROFILE');
+	// include Menu.php for each active module
+	foreach ( $RosarioModules as $module => $active )
+		if ( $active )
+			include( 'modules/' . $module . '/Menu.php' );
 
-	if($profile!='student')
-		if(User('PROFILE_ID'))
-			$_ROSARIO['AllowUse'] = DBGet(DBQuery("SELECT MODNAME FROM PROFILE_EXCEPTIONS WHERE PROFILE_ID='".User('PROFILE_ID')."' AND CAN_USE='Y'"),array(),array('MODNAME'));
+	$profile = User( 'PROFILE' );
+
+	if ( $profile != 'student' )
+	{
+		if ( User( 'PROFILE_ID' ) )
+		{
+			$_ROSARIO['AllowUse'] = DBGet( DBQuery( "SELECT MODNAME
+				FROM PROFILE_EXCEPTIONS
+				WHERE PROFILE_ID='" . User( 'PROFILE_ID' ) . "'
+				AND CAN_USE='Y'" ),	array(), array( 'MODNAME' )	);
+		}
+		// if user has custom exceptions
 		else
-			$_ROSARIO['AllowUse'] = DBGet(DBQuery("SELECT MODNAME FROM STAFF_EXCEPTIONS WHERE USER_ID='".User('STAFF_ID')."' AND CAN_USE='Y'"),array(),array('MODNAME'));
+		{
+			$_ROSARIO['AllowUse'] = DBGet( DBQuery( "SELECT MODNAME
+				FROM STAFF_EXCEPTIONS
+				WHERE USER_ID='" . User( 'STAFF_ID' ) . "'
+				AND CAN_USE='Y'" ),	array(), array( 'MODNAME' ) );
+		}
+	}
 	else
 	{
-		$_ROSARIO['AllowUse'] = DBGet(DBQuery("SELECT MODNAME FROM PROFILE_EXCEPTIONS WHERE PROFILE_ID='0' AND CAN_USE='Y'"),array(),array('MODNAME'));
+		$_ROSARIO['AllowUse'] = DBGet( DBQuery( "SELECT MODNAME
+			FROM PROFILE_EXCEPTIONS
+			WHERE PROFILE_ID='0'
+			AND CAN_USE='Y'" ), array(), array( 'MODNAME' ) );
+
+		// force student profile to parent (same rights in Menu.php files)
 		$profile = 'parent';
 	}
 
-	foreach($menu as $modcat=>$profiles)
+	// Loop menu entries for each module & profile
+	// Save menu entries in $_ROSARIO['Menu'] global var
+	foreach ( $menu as $modcat => $profiles )
 	{
 		//FJ bugfix remove modules with no programs
 		$no_programs_in_module = true;
 		
 		$programs = $profiles[$profile];
-		foreach($programs as $program=>$title)
+
+		foreach ( $programs as $program => $title )
 		{
-			if(!is_numeric($program))
+			if ( !is_numeric( $program ) )
 			{
-//				if($_ROSARIO['AllowUse'][$program] && ($profile!='admin' || !$exceptions[$modcat][$program] || AllowEdit($program)))
-				if($program == 'default' && (!empty($_ROSARIO['AllowUse'][$title]) && ($profile!='admin' || empty($exceptions[$modcat][$title]) || AllowEdit($title))))
+				// if($_ROSARIO['AllowUse'][$program] && ($profile!='admin' || !$exceptions[$modcat][$program] || AllowEdit($program)))
+				// default program when opening module
+				if ( $program == 'default'
+					&& ( !empty( $_ROSARIO['AllowUse'][$title] )
+						&& ( $profile != 'admin'
+							|| empty( $exceptions[$modcat][$title] )
+							|| AllowEdit( $title ) ) ) )
+				{
 					$_ROSARIO['Menu'][$modcat]['default'] = $title;
-				elseif(!empty($_ROSARIO['AllowUse'][$program]) && ($profile!='admin' || empty($exceptions[$modcat][$program]) || AllowEdit($program)))
+				}
+				// if program allowed, add it
+				elseif( !empty( $_ROSARIO['AllowUse'][$program] )
+					&& ( $profile != 'admin'
+						|| empty( $exceptions[$modcat][$program] )
+						|| AllowEdit( $program ) ) )
 				{
 					$_ROSARIO['Menu'][$modcat][$program] = $title;
-					if (!isset($_ROSARIO['Menu'][$modcat]['default'])) //default to first allowed program if default not allowed
+
+					// default to first allowed program if default not allowed
+					if ( !isset($_ROSARIO['Menu'][$modcat]['default'] ) )
 						$_ROSARIO['Menu'][$modcat]['default'] = $program;
+
 					$no_programs_in_module = false;
 				}
 			}
+			// if program is numeric, it is a section
+			// eg.: [1] => _( 'Setup' )
 			else
 				$_ROSARIO['Menu'][$modcat][$program] = $title;
 		}
 		
-		if ($no_programs_in_module)
-			unset($_ROSARIO['Menu'][$modcat]);
+		if ( $no_programs_in_module )
+			unset( $_ROSARIO['Menu'][$modcat] );
 	}
 
-//FJ enable password change for students
-	if(User('PROFILE')=='student')
+	//FJ enable password change for students
+	if ( User( 'PROFILE' ) == 'student' )
 		//unset($_ROSARIO['Menu']['Users']);
-		unset($_ROSARIO['Menu']['Users']['parent']['Users/User.php']);
+		unset( $_ROSARIO['Menu']['Users']['parent']['Users/User.php'] );
 }
 ?>
