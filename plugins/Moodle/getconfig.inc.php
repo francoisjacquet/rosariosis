ffb43df631e7ffd88523cf12be40f5a6447107f8
François Jacquet <francoisjacquet@users.noreply.github.com>
2015-07-09 12:46:19 +0200

    Format & comment code + fix notices when user not logged in

--- plugins/Moodle/getconfig.inc.php
+++ plugins/Moodle/getconfig.inc.php
@@ -1,12 +1,36 @@
 <?php
 
-//get config values from PROGRAM_CONFIG table
-$program_config = DBGet(DBQuery("SELECT * FROM PROGRAM_CONFIG WHERE SCHOOL_ID='".UserSchool()."' AND SYEAR='".UserSyear()."' AND PROGRAM='moodle'"),array(),array('TITLE'));
+// user logged in
+if ( UserSchool() > 0 )
+{
+	//get config values from PROGRAM_CONFIG table
+	$program_config = DBGet( DBQuery( "SELECT * FROM
+		PROGRAM_CONFIG WHERE
+		SCHOOL_ID='" . UserSchool() . "'
+		AND SYEAR='" . UserSyear() . "'
+		AND PROGRAM='moodle'"), array(), array( 'TITLE' ) );
 
-//define constants for plugin use
-define('MOODLE_URL', $program_config['MOODLE_URL'][1]['VALUE']); //example: http://localhost/moodle
-define('MOODLE_TOKEN', $program_config['MOODLE_TOKEN'][1]['VALUE']); //example: d6c51ea6ffd9857578722831bcb070e1
-define('MOODLE_PARENT_ROLE_ID', $program_config['MOODLE_PARENT_ROLE_ID'][1]['VALUE']); //example: 10
-define('ROSARIO_STUDENTS_EMAIL_FIELD_ID', $program_config['ROSARIO_STUDENTS_EMAIL_FIELD_ID'][1]['VALUE']); //example: 11
+	//define constants for plugin use
+
+	//example: http://localhost/moodle
+	define( 'MOODLE_URL', $program_config['MOODLE_URL'][1]['VALUE'] );
+
+	//example: d6c51ea6ffd9857578722831bcb070e1
+	define( 'MOODLE_TOKEN', $program_config['MOODLE_TOKEN'][1]['VALUE'] );
+
+	//example: 10
+	define( 'MOODLE_PARENT_ROLE_ID', $program_config['MOODLE_PARENT_ROLE_ID'][1]['VALUE'] );
+
+	//example: 11
+	define( 'ROSARIO_STUDENTS_EMAIL_FIELD_ID', $program_config['ROSARIO_STUDENTS_EMAIL_FIELD_ID'][1]['VALUE'] );
+}
+// not logged in
+else
+{
+	define( 'MOODLE_URL', null );
+	define( 'MOODLE_TOKEN', null );
+	define( 'MOODLE_PARENT_ROLE_ID', null );
+	define( 'ROSARIO_STUDENTS_EMAIL_FIELD_ID', null );
+}
 
 ?>
