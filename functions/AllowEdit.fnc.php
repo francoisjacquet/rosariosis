724137d0f96247780a94beae9f34163a7d3bf1f8
François Jacquet <francoisjacquet@users.noreply.github.com>
2015-07-10 17:41:24 +0200

    Comment, format & rework functions AllowEdit() & AllowUse()

--- functions/AllowEdit.fnc.php
+++ functions/AllowEdit.fnc.php
@@ -1,31 +1,56 @@
 <?php
+/**
+ * User edit / usage rights check functions
+ * Determined by profiles / user permissions
+ *
+ * @see Users > User Profiles & User Permissions
+ */
 
-function AllowEdit($modname=false)
-{	global $_ROSARIO;
+/**
+ * Can Edit program check
+ *
+ * Always perform AllowEdit() check:
+ * before displaying fields / options to edit data
+ * AND before saving or updating data
+ *
+ * @param  string  $modname Specify program name (optional)
+ *                          defaults to current program
+ *
+ * @return boolean          false if not allowed, true if allowed
+ */
+function AllowEdit( $modname = false )
+{
+	global $_ROSARIO;
 
-	if(User('PROFILE')=='admin')
+	if ( User( 'PROFILE' ) === 'admin' )
 	{
-		if(!$modname && isset($_ROSARIO['allow_edit']))
+		if ( !$modname
+			&& isset( $_ROSARIO['allow_edit'] ) )
 			return $_ROSARIO['allow_edit'];
 
-		if(!$modname)
+		if ( !$modname )
 			$modname = $_REQUEST['modname'];
 
-		if(($modname=='Students/Student.php' || $modname=='Users/User.php') && $_REQUEST['category_id'])
-			$modname = $modname.'&category_id='.$_REQUEST['category_id'];
+		// Student / User Info tabs
+		if ( ( $modname === 'Students/Student.php'
+			|| $modname === 'Users/User.php' )
+			&& isset( $_REQUEST['category_id'] ) )
+			$modname = $modname . '&category_id=' . $_REQUEST['category_id'];
 
-		if(empty($_ROSARIO['AllowEdit']))
+		// get CAN_EDIT programs from database
+		if ( !isset( $_ROSARIO['AllowEdit'] ) )
 		{
-			if(User('PROFILE_ID'))
-				$_ROSARIO['AllowEdit'] = DBGet(DBQuery("SELECT MODNAME FROM PROFILE_EXCEPTIONS WHERE PROFILE_ID='".User('PROFILE_ID')."' AND CAN_EDIT='Y'"),array(),array('MODNAME'));
+			if ( User( 'PROFILE_ID' ) )
+				$_ROSARIO['AllowEdit'] = DBGet( DBQuery( "SELECT MODNAME
+					FROM PROFILE_EXCEPTIONS
+					WHERE PROFILE_ID='" . User( 'PROFILE_ID' ) . "' AND CAN_EDIT='Y'" ), array(), array( 'MODNAME' ) );
 			else
-				$_ROSARIO['AllowEdit'] = DBGet(DBQuery("SELECT MODNAME FROM STAFF_EXCEPTIONS WHERE USER_ID='".User('STAFF_ID')."' AND CAN_EDIT='Y'"),array(),array('MODNAME'));
+				$_ROSARIO['AllowEdit'] = DBGet( DBQuery( "SELECT MODNAME
+					FROM STAFF_EXCEPTIONS
+					WHERE USER_ID='" . User( 'STAFF_ID' ) . "' AND CAN_EDIT='Y'"), array(), array( 'MODNAME' ) );
 		}
 
-		if(!$_ROSARIO['AllowEdit'])
-			$_ROSARIO['AllowEdit'] = array(true);
-
-		if(!empty($_ROSARIO['AllowEdit'][$modname]))
+		if ( isset( $_ROSARIO['AllowEdit'][$modname] ) )
 			return true;
 		else
 			return false;
@@ -34,29 +59,45 @@ function AllowEdit($modname=false)
 		return $_ROSARIO['allow_edit'];
 }
 
-function AllowUse($modname=false)
-{	global $_ROSARIO;
 
-	if(!$modname)
+/**
+ * Can Use program check
+ *
+ * @param  string  $modname Specify program name (optional)
+ *                          defaults to current program
+ *
+ * @return boolean          false if not allowed, true if allowed
+ */
+function AllowUse( $modname = false )
+{
+	global $_ROSARIO;
+
+	if ( !$modname )
 		$modname = $_REQUEST['modname'];
 
-	if(($modname=='Students/Student.php' || $modname=='Users/User.php') && isset($_REQUEST['category_id']))
-		$modname = $modname.'&category_id='.$_REQUEST['category_id'];
+	// Student / User Info tabs
+	if ( ( $modname === 'Students/Student.php'
+			|| $modname ==='Users/User.php' )
+		&& isset( $_REQUEST['category_id'] ) )
+		$modname = $modname . '&category_id=' . $_REQUEST['category_id'];
 
-	if(!isset($_ROSARIO['AllowUse']))
+	// get CAN_USE programs from database
+	if ( !isset( $_ROSARIO['AllowUse'] ) )
 	{
-		if(User('PROFILE_ID')!='')
-			$_ROSARIO['AllowUse'] = DBGet(DBQuery("SELECT MODNAME FROM PROFILE_EXCEPTIONS WHERE PROFILE_ID='".User('PROFILE_ID')."' AND CAN_USE='Y'"),array(),array('MODNAME'));
+		if ( User( 'PROFILE_ID' ) )
+			$_ROSARIO['AllowUse'] = DBGet( DBQuery( "SELECT MODNAME
+				FROM PROFILE_EXCEPTIONS
+				WHERE PROFILE_ID='" . User( 'PROFILE_ID' ) . "' AND CAN_USE='Y'"), array(), array( 'MODNAME' ) );
 		else
-			$_ROSARIO['AllowUse'] = DBGet(DBQuery("SELECT MODNAME FROM STAFF_EXCEPTIONS WHERE USER_ID='".User('STAFF_ID')."' AND CAN_USE='Y'"),array(),array('MODNAME'));
+			$_ROSARIO['AllowUse'] = DBGet( DBQuery( "SELECT MODNAME
+				FROM STAFF_EXCEPTIONS
+				WHERE USER_ID='" . User( 'STAFF_ID' ) . "' AND CAN_USE='Y'" ), array(), array( 'MODNAME' ) );
 	}
 
-	if(!$_ROSARIO['AllowUse'])
-		$_ROSARIO['AllowUse'] = array(true);
-
-	if(count($_ROSARIO['AllowUse'][$modname]))
+	if ( isset( $_ROSARIO['AllowUse'][$modname] ) )
 		return true;
 	else
 		return false;
 }
+
 ?>
