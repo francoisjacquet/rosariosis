ebb4ba44f01e069d86344e3d13287c6096caaaca
François Jacquet <francoisjacquet@users.noreply.github.com>
2015-07-10 17:42:26 +0200

    Comment, format & rework function Config()

--- functions/Config.fnc.php
+++ functions/Config.fnc.php
@@ -1,15 +1,37 @@
 <?php
 
-function Config($item)
-{	global $_ROSARIO,$DefaultSyear;
+/**
+ * Get Configuration value
+ *
+ * @example  Config( 'SYEAR' )
+ *
+ * @param  string $item Config title
+ *
+ * @return string       Config value
+ */
+function Config( $item )
+{
+	global $_ROSARIO,
+		$DefaultSyear;
 
-	if(empty($_ROSARIO['Config']) || !isset($_ROSARIO['Config'][$item]))
+	// Get General & School Config
+	if ( !isset( $_ROSARIO['Config'][$item] ) )
 	{
-		$QI=DBQuery("SELECT TITLE, CONFIG_VALUE FROM CONFIG WHERE ".(UserSchool() < 1 ? '' : "SCHOOL_ID='".UserSchool()."' OR")." SCHOOL_ID='0'");
-		$_ROSARIO['Config'] = DBGet($QI, array(), array('TITLE'));
+		// General (for every school) Config is stored with SCHOOL_ID=0
+		$school_where = "SCHOOL_ID='0'";
+
+		// If user logged in
+		if ( UserSchool() > 0 )
+			$school_where = "SCHOOL_ID='" . UserSchool() . "' OR " . $school_where;
+
+		$_ROSARIO['Config'] = DBGet( DBQuery( "SELECT TITLE, CONFIG_VALUE
+			FROM CONFIG
+			WHERE " . $school_where ), array(), array( 'TITLE' ) );
+
 		$_ROSARIO['Config']['SYEAR'][1]['CONFIG_VALUE'] = $DefaultSyear;
 	}
 
 	return $_ROSARIO['Config'][$item][1]['CONFIG_VALUE'];
 }
+
 ?>
