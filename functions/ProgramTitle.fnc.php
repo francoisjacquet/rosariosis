06c4c1414131c2ada2ac0c657e85c4e85fcca1c1
François Jacquet <francoisjacquet@users.noreply.github.com>
2015-07-10 17:43:10 +0200

    Comment, format & rework function ProgramTitle()

--- functions/ProgramTitle.fnc.php
+++ functions/ProgramTitle.fnc.php
@@ -1,34 +1,55 @@
 <?php
 
-function ProgramTitle($modname='')
-{	global $_ROSARIO;
+/**
+ * Get Program Title
+ *
+ * @example ProgramTitle()
+ *
+ * @param  string $modname Specify program/modname (optional)
+ *
+ * @return string          Program title or 'RosarioSIS' if not found
+ */
+function ProgramTitle( $modname = '' )
+{
+	global $_ROSARIO;
 
-	if(!$modname)
+	if ( empty( $modname ) )
 		$modname = $_REQUEST['modname'];
 
-	if(!isset($_ROSARIO['Menu']))
-		include('Menu.php');
+	// generate Menu if needed
+	if ( !isset( $_ROSARIO['Menu'] ) )
+		include( 'Menu.php' );
 
-	foreach($_ROSARIO['Menu'] as $modcat=>$programs)
+	// loop modules
+	foreach ( (array)$_ROSARIO['Menu'] as $modcat => $programs )
 	{
-		if(count($programs))
+		foreach( (array)$programs as $program => $title )
 		{
-			foreach($programs as $program=>$title)
+			// if current program
+			if ( $modname === $program )
 			{
-				if($modname==$program)
-				{
-					if($_ROSARIO['HeaderIcon']!==false)
-						if(mb_substr($modname,0,25)=='Users/TeacherPrograms.php')
-							$_ROSARIO['HeaderIcon'] = 'modules/'.mb_substr($modname,34,mb_strpos($modname,'/',34)-34).'/icon.png';
-						else
-							$_ROSARIO['HeaderIcon'] = 'modules/'.$modcat.'/icon.png';
-					return $title;
-				}
+				// set Header Icon
+				if ( !isset( $_ROSARIO['HeaderIcon'] )
+					|| $_ROSARIO['HeaderIcon'] !== false )
+					// get right icon for Teacher Programs
+					if ( mb_substr( $modname, 0, 25 ) === 'Users/TeacherPrograms.php' )
+						$_ROSARIO['HeaderIcon'] = 'modules/' .
+							mb_substr( $modname, 34, mb_strpos( $modname, '/', 34 ) - 34 ) .
+							'/icon.png';
+					else
+						$_ROSARIO['HeaderIcon'] = 'modules/' . $modcat . '/icon.png';
+
+				return $title;
 			}
 		}
 	}
-	if($_ROSARIO['HeaderIcon']!==false)
-		unset($_ROSARIO['HeaderIcon']);
+	// Program not found!
+
+	if ( isset( $_ROSARIO['HeaderIcon'] )
+		&& $_ROSARIO['HeaderIcon'] !== false )
+		unset( $_ROSARIO['HeaderIcon'] );
+
 	return 'RosarioSIS';
 }
+
 ?>
