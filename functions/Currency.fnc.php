1893807182dd119f08229d4856ec7a89d0008b8a
François Jacquet <francoisjacquet@users.noreply.github.com>
2015-07-10 17:42:36 +0200

    Comment, format & rework function Currency()

--- functions/Currency.fnc.php
+++ functions/Currency.fnc.php
@@ -1,29 +1,54 @@
 <?php
 
-function Currency($num,$sign='before',$red=false)
+/**
+ * Format Money amount and add Currency symbol
+ *
+ * @example Currency( $total )
+ *
+ * @param  float   $num  Money amount
+ * @param  string  $sign Minus sign or credit (CR) (optional)
+ * @param  boolean $red  Red if negative amount (optional)
+ *
+ * @return string        Formatted number
+ */
+function Currency( $num, $sign = 'before', $red = false )
 {
 	$original = $num;
-	//FJ Bugfix Currency direct call via $extra['functions']
-	if($sign=='CR' && $num<0)
+
+	$negative = $cr = false;
+
+	// FJ Bugfix Currency direct call via $extra['functions']
+	if ( $sign === 'CR'
+		&& $num < 0 )
 	{
 		$cr = true;
+
 		$num *= -1;
 	}
-	elseif($num<0)
+	elseif ( $num < 0 )
 	{
 		$negative = true;
+
 		$num *= -1;
 	}
 
-//	$num = "\$".number_format($num,2,'.',',');
-	$num = Config('CURRENCY').number_format($num,2,'.',',');
-	if($negative)
-		$num = '-'.$num;
-	elseif($cr)
-		$num = $num.'CR';
-	if($red && $original<0)
-		$num = '<span style="color:red">'.$num.'</span>';
+	// Add currency symbol & format amount
+	$num = Config( 'CURRENCY' ) . number_format( $num, 2, '.', ',' );
 
-	return '<!-- '.$original.' -->'.$num;
+	// Add minus if negative
+	if ( $negative )
+		$num = '-' . $num;
+
+	// Add CR if credit
+	elseif ( $cr )
+		$num = $num . 'CR';
+
+	// Red if negative amount
+	if ( $red
+		&& $original < 0 )
+		$num = '<span style="color: red;">' . $num . '</span>';
+
+	return '<!-- ' . $original . ' -->' . $num;
 }
+
 ?>
