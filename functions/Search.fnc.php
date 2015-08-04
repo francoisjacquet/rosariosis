3b26d2d2d50137222bd4d5225f30c77b74a54f35
François Jacquet <francoisjacquet@users.noreply.github.com>
2015-08-04 17:41:51 -0500

    Remove -Edit- option for edits & autos Pull-Downs

--- functions/Search.fnc.php
+++ functions/Search.fnc.php
@@ -250,8 +250,6 @@ function Search($type,$extra=null)
 						foreach($options_RET as $option)
 							echo '<OPTION value="'.$option.'">'.$option.'</OPTION>';
 
-						//FJ new option
-						echo '<OPTION value="---">-'. _('Edit') .'-</OPTION>';
 						echo '</SELECT>';
 						echo '</TD></TR>';
 					}
@@ -276,8 +274,6 @@ function Search($type,$extra=null)
 						foreach($options_RET as $option)
 							echo '<OPTION value="'.$option.'">'.$option.'</OPTION>';
 
-						//FJ new option
-						echo '<OPTION value="---">-'. _('Edit') .'-</OPTION>';
 						echo '<OPTION value="~">'._('Other Value').'</OPTION>';
 						echo '</SELECT>';
 						echo '</TD></TR>';
