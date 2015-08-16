b1ba23c68e803e888d7cc4419bbb74ac84c5bbc9
François Jacquet <francoisjacquet@users.noreply.github.com>
2015-08-16 12:19:54 -0500

    Version 2.8.19 update

--- Warehouse.php
+++ Warehouse.php
@@ -2,7 +2,7 @@
 if(!defined('WAREHOUSE_PHP'))
 {
 	define("WAREHOUSE_PHP",1);
-	$RosarioVersion = '2.8.18';
+	$RosarioVersion = '2.8.19';
 
 	if (!file_exists ('config.inc.php'))
 		die ('config.inc.php not found. Please read the configuration guide.');
