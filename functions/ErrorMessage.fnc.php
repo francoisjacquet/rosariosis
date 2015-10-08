c31f3b59d87bdf5577a1b511e813710e2d966e3f
François Jacquet <francoisjacquet@users.noreply.github.com>
2015-10-08 18:21:25 -0500

    Format & comment ErrorMessage() TOFINISH

--- functions/ErrorMessage.fnc.php
+++ functions/ErrorMessage.fnc.php
@@ -22,43 +22,68 @@ ErrorMessage($error);
 
 // If a 2ND is sent, the list will not be treated as errors, but shown anyway
 
-function ErrorMessage($errors,$code='error')
+function ErrorMessage( $errors, $code = 'error' )
 {
-	if($errors)
-	{
-//FJ css WPadmin
-		if($code=='error' || $code=='fatal')
-			$return .= '<div class="error"><p>'. button('x') .'&nbsp;<b>'._('Error').':</b> ';
-		elseif($code=='warning')
-			$return .= '<div class="error"><p>'. button('warning') .'&nbsp;<b>'._('Warning').':</b> ';
-		else
-			$return .= '<div class="updated"><p><b>'._('Note').':</b> ';
+	$return = '';
 
-		if(count($errors)==1)
-			$return .= ($errors[0]?$errors[0]:$errors[1]) .'</p>';
+	if ( is_array( $errors )
+		&& count( $errors ) )
+	{
+		// Error
+		if ( $code === 'error'
+			|| $code === 'fatal' )
+		{
+			$return .= '<div class="error"><p>' . button( 'x' ) .'&nbsp;<b>' . _( 'Error' ) . ':</b> ';
+		}
+		// Warning
+		elseif ( $code === 'warning' )
+		{
+			$return .= '<div class="error"><p>' . button( 'warning' ) . '&nbsp;<b>' . _( 'Warning' ) . ':</b> ';
+		}
+		// Note / Update
+		else
+		{
+			$return .= '<div class="updated"><p><b>' . _( 'Note' ) . ':</b> ';
+		}
+
+		if( count( $errors ) === 1 )
+		{
+			$return .= ( isset( $errors[0] ) ? $errors[0] : $errors[1] ) . '</p>';
+		}
+		// More than one error: list
 		else
 		{
 			$return .= '</p><ul>';
-			foreach($errors as $value)
-					$return .= '<LI class="size-1">'.$value.'</LI>'."\n";
+			
+			foreach( (array)$errors as $error )
+			{
+				$return .= '<LI>' . $error . '</LI>';
+			}
+			
 			$return .= '</ul>';
 		}
+
 		$return .= '</div><BR />';
 
-		if($code=='fatal')
+		// Fatal error, display error and exit
+		if ( $code === 'fatal' )
 		{
 			echo $return;
-			if(!isset($_REQUEST['_ROSARIO_PDF']))
-				Warehouse('footer');
-//FJ force PDF on fatal error
+
+			if ( !isset( $_REQUEST['_ROSARIO_PDF'] ) )
+				Warehouse( 'footer' );
+
+			//FJ force PDF on fatal error
 			else
 			{
 				global $print_data;
-				PDFStop($print_data);
+
+				PDFStop( $print_data );
 			}
+
 			exit;
 		}
-
-		return $return;
 	}
+
+	return $return;
 }
