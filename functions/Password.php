8a8735029075a380364a8b9413ede234f62ccbe3
François Jacquet <francoisjacquet@users.noreply.github.com>
2015-10-15 18:47:14 -0500

    Comment & format Password functions

--- functions/Password.php
+++ functions/Password.php
@@ -1,16 +1,43 @@
 <?php
-//FJ add password encryption
-if (!defined('CRYPT_SHA512'))
-	define('CRYPT_SHA512', 1);
-	
-function encrypt_password($plain) {
-	$password = rand(999999999, 9999999999);
-	$salt = '$6$'.mb_substr(sha1($password), 0, 16	);
-	$password = crypt($plain, $salt);
-	return $password;
+
+// We use SHA512 algorithm
+if ( !defined( 'CRYPT_SHA512' ) )
+	define( 'CRYPT_SHA512', 1 );
+
+/**
+ * Encrypt Password
+ *
+ * @see http://php.net/crypt
+ *
+ * @uses SHA512 algorithm (PHP 5.3.2+ required)
+ *
+ * @param  string $plain Plain text password
+ *
+ * @return string Encrypted password
+ */
+function encrypt_password( $plain )
+{
+	$rand = rand( 999999999, 9999999999 );
+
+	$salt = '$6$' . mb_substr( sha1( $rand ), 0, 16	);
+
+	return crypt( $plain, $salt );
 }
 
-function match_password($password, $plain) {
+
+/**
+ * Match Password
+ *
+ * @see http://php.net/hash-equals
+ *
+ * @param  string  $crypted Crypted password
+ * @param  string  $plain   Plain text password
+ *
+ * @return boolean true if password match, else false
+ */
+function match_password( $crypted, $plain )
+{
 	//$salt = mb_substr($password, 0, 19);
-	return $password == crypt($plain, $password);
+
+	return hash_equals( $crypted, crypt( $plain, $crypted ) );
 }
