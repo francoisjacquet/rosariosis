1fefaf38baa09cf0cfb97acf150905502c1a1395
François Jacquet <francoisjacquet@users.noreply.github.com>
2015-10-20 11:34:29 -0500

    Comment & Format PreparePHP_SELF, SaveData, School & Syear functions

--- functions/School.php
+++ functions/School.php
@@ -1,21 +1,58 @@
 <?php
-function UpdateSchoolArray($school_id=null){
-	if(!isset($school_id)) 
+
+/**
+ * Update School Array
+ * Fill $_SESSION['SchoolData'] session var with SCHOOLS data
+ *
+ * @example UpdateSchoolArray( UserSchool() );
+ *
+ * @param  integer $school_id School ID (optional). Defaults to User School ID
+ *
+ * @return void    Fill $_SESSION['SchoolData'] session var
+ */
+function UpdateSchoolArray( $school_id = null )
+{
+	if ( is_null( $school_id ) )
+	{
 		$school_id = UserSchool();
-	$_SESSION['SchoolData'] = DBGet(DBQuery("SELECT * FROM SCHOOLS WHERE ID = '".$school_id."' AND SYEAR = '".UserSyear()."'"));
+	}
+
+	$_SESSION['SchoolData'] = DBGet( DBQuery( "SELECT *
+		FROM SCHOOLS
+		WHERE ID = '" . $school_id . "'
+		AND SYEAR = '" . UserSyear() . "'" ) );
+
 	$_SESSION['SchoolData'] = $_SESSION['SchoolData'][1];
-//FJ if only one school, no Search All Schools option
-	$schools_nb = DBGet(DBQuery("SELECT COUNT(*) AS SCHOOLS_NB FROM SCHOOLS WHERE SYEAR = '".UserSyear()."';"));
+	
+	//FJ if only one school, no Search All Schools option
+	$schools_nb = DBGet( DBQuery( "SELECT COUNT(*)
+		AS SCHOOLS_NB
+		FROM SCHOOLS
+		WHERE SYEAR = '" . UserSyear() . "';" ) );
+
 	$_SESSION['SchoolData']['SCHOOLS_NB'] = $schools_nb[1]['SCHOOLS_NB'];
-	$_SESSION['SchoolData'];  
 }
 
-function SchoolInfo($field=null){
-	if($_SESSION['SchoolData']['ID']!=UserSchool())
-		UpdateSchoolArray(UserSchool());
+/**
+ * Get School Info
+ *
+ * @example DrawHeader( SchoolInfo( 'TITLE' ) );
+ *
+ * @param  string       $field SCHOOLS DB table field name (optional). Defaults to every School Fields
+ * @return string|array School Field or array with every School Fields
+ */
+function SchoolInfo( $field = null )
+{
+	if ( !isset( $_SESSION['SchoolData'] )
+		|| $_SESSION['SchoolData']['ID'] != UserSchool() )
+	{
+		UpdateSchoolArray( UserSchool() );
+	}
 		
-	if(isset($field))
+	if ( !is_null( $field ) )
+	{
 		return $_SESSION['SchoolData'][$field];
+	}
 	else
 		return $_SESSION['SchoolData'];
 }
--- functions/Syear.php
+++ functions/Syear.php
@@ -1,18 +1,47 @@
 <?php
 
-function GetSyear($date)
+/**
+ * Get School Year
+ * Return School Year corresponding to $date
+ *
+ * @example GetSyear( $start_date );
+ *
+ * @param  string $date DB Date
+ *
+ * @return string School Year
+ */
+function GetSyear( $date )
 {
 	//$RET = DBGet(DBQuery("SELECT SYEAR FROM SCHOOL_MARKING_PERIODS WHERE MP='FY' AND '".$date."' BETWEEN START_DATE AND END_DATE"));
-	$RET = DBGet(DBQuery("SELECT max(SYEAR) AS SYEAR FROM SCHOOL_MARKING_PERIODS WHERE MP='FY' AND START_DATE<='".$date."'"));
+
+	// Get greatest SYEAR where START_DATE <= $date
+	$RET = DBGet( DBQuery( "SELECT max(SYEAR) AS SYEAR
+		FROM SCHOOL_MARKING_PERIODS
+		WHERE MP='FY'
+		AND START_DATE<='" . $date . "'" ) );
 
 	return $RET[1]['SYEAR'];
 }
 
-//FJ school year over one/two calendar years format
-function FormatSyear($syear, $syear_over_two_years=true)
+
+/**
+ * Format School Year
+ * If school year over two calendar years, return "[SYEAR]-[SYEAR+1]"
+ * @see School Setup > School Configuration > School
+ *
+ * @example FormatSyear( UserSyear(), Config( 'SCHOOL_SYEAR_OVER_2_YEARS' ) )
+ *
+ * @param  string  $syear                School Year
+ * @param  boolean $syear_over_two_years School Year over two calendar years?
+ *
+ * @return string  Formatted School Year
+ */
+function FormatSyear( $syear, $syear_over_two_years = true )
 {
-	if ($syear_over_two_years)
-		return $syear.'-'.($syear + 1);
+	if ( $syear_over_two_years )
+	{
+		return $syear . '-' . ( $syear + 1 );
+	}
 	else
 		return $syear;
 }
