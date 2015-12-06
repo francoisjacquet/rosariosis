2364da7afdb7c3db75b614378820c979fc32c6a6
François Jacquet <francoisjacquet@users.noreply.github.com>
2015-12-05 19:08:22 -0500

    Format code & return fatal error if no gettext | mbstring | pgsql extensions

--- diagnostic.php
+++ diagnostic.php
@@ -22,26 +22,36 @@ if ( !isset( $_SESSION['STAFF_ID'] ) )
 $inipath = php_ini_loaded_file();
 
 if ( $inipath )
+{
 	$inipath = ' Loaded php.ini: ' . $inipath;
+}
 else
 	$inipath = ' Note: No php.ini file is loaded!';
+
+//pgsql
+if ( !extension_loaded( 'pgsql' ) )
+{
+	$error[] = 'PHP extensions: RosarioSIS relies on the pgsql (PostgreSQL) extension. See the php.ini file to activate it.' . $inipath;
+}
 
 //gettext
 if ( !extension_loaded( 'gettext' )
 	|| !function_exists( 'bindtextdomain' ) )
-	$error[] = 'PHP extensions: RosarioSIS relies on the gettext extensions. See the php.ini file to activate it.' . $inipath;
+{
+	$error[] = 'PHP extensions: RosarioSIS relies on the gettext extension. See the php.ini file to activate it.' . $inipath;
+}
 
 //mbstring
 if ( !extension_loaded( 'mbstring' ) )
-	$error[] = 'PHP extensions: RosarioSIS relies on the mbstring extensions. See the php.ini file to activate it.' . $inipath;
+{
+	$error[] = 'PHP extensions: RosarioSIS relies on the mbstring extension. See the php.ini file to activate it.' . $inipath;
+}
 
-//xmlrpc
-if ( !extension_loaded( 'xmlrpc' ) )
-	$error[] = 'PHP extensions: RosarioSIS relies on the xmlrpc extensions (only used to connect to Moodle). See the php.ini file to activate it.' . $inipath;
+if ( count( $error ) )
+{
+	echo _ErrorMessage( $error, 'fatal' );
+}
 
-//session.auto_start
-if ( (bool)ini_get( 'session.auto_start' ) )
-	$error[] = 'session.auto_start is set to On in your PHP configuration. See the php.ini file to deactivate it.' . $inipath;
 
 if ( !file_exists( './Warehouse.php' ) )
 {
@@ -52,53 +62,74 @@ else
 	require_once './Warehouse.php';
 
 	if ( !@opendir( $RosarioPath . '/functions' ) )
+	{
 		$error[] = 'The value for $RosarioPath in config.inc.php is not correct or else the functions directory does not have the correct permissions to be read by the webserver. Make sure $RosarioPath points to the RosarioSIS installation directory and that it is readable by all users.';
+	}
 
 	if ( !function_exists( 'pg_connect' ) )
+	{
 		$error[] = 'The pgsql extension (see the php.ini file) is not activated OR PHP was not compiled with PostgreSQL support. You may need to recompile PHP using the --with-pgsql option for RosarioSIS to work.';
-
+	}
 	else
 	{
 		if ( $DatabaseServer != 'localhost' )
+		{
 			$connectstring = 'host=' . $DatabaseServer . ' ';
+		}
 
 		if ( $DatabasePort != '5432' )
+		{
 			$connectstring .= 'port=' . $DatabasePort .' ';
+		}
 
 		$connectstring .= 'dbname=' . $DatabaseName . ' user=' . $DatabaseUsername;
 
 		if ( $DatabasePassword !== '' )
+		{
 			$connectstring .= ' password=' . $DatabasePassword;
+		}
 
 		$connection = pg_connect( $connectstring );
 
 		if ( !$connection )
-			$error[] = 'RosarioSIS cannot connect to the Postgres database. Either Postgres is not running, it was not started with the -i option, or connections from this host are not allowed in the pg_hba.conf file. Last Postgres Error: ' . pg_last_error();
-
+		{
+			$error[] = 'RosarioSIS cannot connect to the PostgreSQL database. Either Postgres is not running, it was not started with the -i option, or connections from this host are not allowed in the pg_hba.conf file. Last Postgres Error: ' . pg_last_error();
+		}
 		else
 		{
 			$result = @pg_exec( $connection, 'SELECT * FROM CONFIG' );
 
 			if ( $result === false )
+			{
 				$errstring = pg_last_error( $connection );
 
-			if ( mb_strpos( $errstring, 'permission denied' ) !== false )
-				$error[] = 'The database was created with the wrong permissions. The user specified in the config.inc.php file does not have permission to access the rosario database. Use the super-user (postgres) or recreate the database adding \connect - YOUR_USERNAME to the top of the rosariosis.sql file.';
+				if ( mb_strpos( $errstring, 'permission denied' ) !== false )
+				{
+					$error[] = 'The database was created with the wrong permissions. The user specified in the config.inc.php file does not have permission to access the rosario database. Use the super-user (postgres) or recreate the database adding \connect - YOUR_USERNAME to the top of the rosariosis.sql file.';
+				}
+				elseif ( mb_strpos( $errstring, 'elation "config" does not exist' ) !== false )
+				{
+					$error[] = 'At least one of the tables does not exist. Make sure you ran the rosariosis.sql file as described in the INSTALL file.';
+				}
+				elseif ( $errstring )
+				{
+					$error[] = $errstring;
+				}
+			}
 
-			elseif ( mb_strpos( $errstring, 'elation "config" does not exist' ) !== false )
-				$error[] = 'At least one of the tables does not exist. Make sure you ran the rosariosis.sql file as described in the INSTALL file.';
-
-			elseif ( $errstring )
-				$error[] = $errstring;
 			
 			$result = @pg_exec( $connection, "SELECT * FROM STAFF WHERE SYEAR='" . $DefaultSyear . "'" );
 
 			if ( !pg_fetch_all( $result ) )
+			{
 				$error[] = 'The value for $DefaultSyear in config.inc.php is not correct.';
+			}
 
 			if ( !is_array( $RosarioLocales )
 				|| empty( $RosarioLocales ) )
+			{
 				$error[] = 'The value for $RosarioLocales in config.inc.php is not correct.';
+			}
 		}
 	}
 
@@ -106,20 +137,39 @@ else
 	if ( !empty( $wkhtmltopdfPath )
 		&& ( !file_exists( $wkhtmltopdfPath )
 			|| strpos( basename( $wkhtmltopdfPath ), 'wkhtmltopdf' ) !== 0 ) )
+	{
 		$error[] = 'The value for $wkhtmltopdfPath in config.inc.php is not correct.';
+	}
 
 	//FJ check pg_dump binary exists
 	if ( !empty( $pg_dumpPath )
 		&& ( !file_exists( $pg_dumpPath )
 			|| strpos( basename( $pg_dumpPath ), 'pg_dump' ) !== 0 ) )
+	{
 		$error[] = 'The value for $pg_dumpPath in config.inc.php is not correct.';
+	}
 
 }
+
+//xmlrpc
+if ( !extension_loaded( 'xmlrpc' ) )
+{
+	$error[] = 'PHP extensions: RosarioSIS relies on the xmlrpc extension (only used to connect to Moodle). See the php.ini file to activate it.' . $inipath;
+}
+
+//session.auto_start
+if ( (bool)ini_get( 'session.auto_start' ) )
+{
+	$error[] = 'session.auto_start is set to On in your PHP configuration. See the php.ini file to deactivate it.' . $inipath;
+}
+
 
 echo _ErrorMessage( $error, 'error' );
 
 if ( !count( $error ) )
+{
 	echo '<h3>Your RosarioSIS installation is properly configured.</h3>';
+}
 
 if ( $unset_username )
 {
@@ -137,8 +187,9 @@ function _ErrorMessage( $errors, $code = 'error' )
 		{
 			if ( $code == 'error'
 				|| $code == 'fatal' )
+			{
 				$return .= '<b><span style="color:#CC0000">Error:</span></b> ';
-
+			}
 			else
 				$return .= '<b><span style="color:#00CC00">Note:</span></b> ';
 
@@ -148,15 +199,18 @@ function _ErrorMessage( $errors, $code = 'error' )
 		{
 			if ( $code == 'error'
 				|| $code == 'fatal' )
+			{
 				$return .= '<b><span style="color:#CC0000">Errors:</span></b>';
-
+			}
 			else
 				$return .= '<b><span style="color:#00CC00">Note:</span></b>';
 
 			$return .= '<ul>';
 
 			foreach ( (array)$errors as $value )
+			{
 				$return .= '<li>' . $value . '</li>';
+			}
 
 			$return .= '</ul>';
 		}
@@ -169,7 +223,9 @@ function _ErrorMessage( $errors, $code = 'error' )
 
 			if ( !isset( $_REQUEST['_ROSARIO_PDF'] )
 				&& function_exists( 'Warehouse' ) )
+			{
 				Warehouse( 'footer' );
+			}
 
 			exit;
 		}
