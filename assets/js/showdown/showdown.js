884b9e427704d59a70e8353cc81ec28792d1f417
François Jacquet <francoisjacquet@users.noreply.github.com>
2016-03-21 11:54:19 -0500

    Add target="_blank" to showdown (MD) generated links

--- assets/js/showdown/showdown.js
+++ assets/js/showdown/showdown.js
@@ -1045,7 +1045,8 @@ showdown.subParser('anchors', function (text, options, globals) {
       result += ' title="' + title + '"';
     }
 
-    result += '>' + linkText + '</a>';
+	// FJ open link in new tab.
+    result += ' target="_blank">' + linkText + '</a>';
 
     return result;
   };
@@ -1140,13 +1141,15 @@ showdown.subParser('autoLinks', function (text, options, globals) {
       simpleMailRegex = /(?:^|[ \n\t])([A-Za-z0-9!#$%&'*+-/=?^_`\{|}~\.]+@[-a-z0-9]+(\.[-a-z0-9]+)*\.[a-z]+)(?:$|[ \n\t])/gi,
       delimMailRegex  = /<(?:mailto:)?([-.\w]+@[-a-z0-9]+(\.[-a-z0-9]+)*\.[a-z]+)>/gi;
 
-  text = text.replace(delimUrlRegex, '<a href=\"$1\">$1</a>');
+  // FJ open links in new tab.
+  text = text.replace(delimUrlRegex, '<a href=\"$1\" target=\"_blank\">$1</a>');
   text = text.replace(delimMailRegex, replaceMail);
   //simpleURLRegex  = /\b(((https?|ftp|dict):\/\/|www\.)[-.+~:?#@!$&'()*,;=[\]\w]+)\b/gi,
   // Email addresses: <address@domain.foo>
 
   if (options.simplifiedAutoLink) {
-    text = text.replace(simpleURLRegex, '<a href=\"$1\">$1</a>');
+    // FJ open links in new tab.
+    text = text.replace(simpleURLRegex, '<a href=\"$1\" target=\"_blank\">$1</a>');
     text = text.replace(simpleMailRegex, replaceMail);
   }
 
