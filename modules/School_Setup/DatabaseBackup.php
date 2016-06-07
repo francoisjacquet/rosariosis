3ba8607bc7dcbe9789d91b624118b8b740d05c48
François Jacquet <francoisjacquet@users.noreply.github.com>
2016-06-07 15:27:10 -0500

    Format code in DatabaseBackup.php

--- modules/School_Setup/DatabaseBackup.php
+++ modules/School_Setup/DatabaseBackup.php
@@ -1,50 +1,60 @@
 <?php
 
-if ( $_REQUEST['modfunc']!='backup')
-	DrawHeader(ProgramTitle());
-
-if ( $_REQUEST['modfunc']=='backup' && isset($_REQUEST['_ROSARIO_PDF']))
+if ( $_REQUEST['modfunc'] !== 'backup' )
 {
-	
-//FJ code inspired by phpPgAdmin
-	putenv('PGHOST=' . $DatabaseServer);
-	putenv('PGPORT=' . $DatabasePort);
-	putenv('PGDATABASE=' . $DatabaseName);
-	putenv('PGUSER=' . $DatabaseUsername);
-	putenv('PGPASSWORD=' . $DatabasePassword);
-	
-	$exe = escapeShellCmd($pg_dumpPath);
-	
-	// Obtain the pg_dump version number and check if the path is good
-	$version = array();
-	preg_match("/(\d+(?:\.\d+)?)(?:\.\d+)?.*$/", exec($exe . " --version"), $version);
+	Drawheader( ProgramTitle() );
+}
 
-	if (empty($version)) {
+if ( $_REQUEST['modfunc'] === 'backup'
+	&& isset( $_REQUEST['_ROSARIO_PDF'] ) )
+{
+
+	// FJ code inspired by phpPgAdmin.
+	putenv( 'PGHOST=' . $DatabaseServer );
+	putenv( 'PGPORT=' . $DatabasePort );
+	putenv( 'PGDATABASE=' . $DatabaseName );
+	putenv( 'PGUSER=' . $DatabaseUsername );
+	putenv( 'PGPASSWORD=' . $DatabasePassword );
+
+	$exe = escapeShellCmd( $pg_dumpPath );
+
+	// Obtain the pg_dump version number and check if the path is good.
+	$version = array();
+	preg_match( "/(\d+(?:\.\d+)?)(?:\.\d+)?.*$/", exec( $exe . " --version" ), $version );
+
+	if ( empty( $version ) )
+	{
 		$error[] = sprintf('The path to the database dump utility specified in the configuration file is wrong! (%s)', $pg_dumpPath);
-		ErrorMessage($error, 'fatal');
+
+		ErrorMessage( $error, 'fatal' );
 	}
 
-	$ctype="application/force-download"; 
-	header("Pragma: public"); 
-	header("Expires: 0"); 
-	header("Cache-Control: must-revalidate, post-check=0, pre-check=0"); 
-	header("Cache-Control: public"); 
-	header("Content-Description: File Transfer"); 
-	header("Content-Type: $ctype"); 
-	$filename = Config('NAME').'_database_backup_'.date('Y.m.d').'.sql';
-	$header="Content-Disposition: attachment; filename=".$filename.";"; 
-	header($header); 
-	header("Content-Transfer-Encoding: binary"); 
+	$ctype = "application/force-download";
+
+	header( "Pragma: public" );
+	header( "Expires: 0" );
+	header( "Cache-Control: must-revalidate, post-check=0, pre-check=0" );
+	header( "Cache-Control: public" );
+	header( "Content-Description: File Transfer" );
+	header( "Content-Type: $ctype" );
+
+	$filename = Config( 'NAME' ) . '_database_backup_' . date( 'Y.m.d' ) . '.sql';
+	$header="Content-Disposition: attachment; filename=" . $filename . ";";
+
+	header( $header );
+	header( "Content-Transfer-Encoding: binary" );
 
 	// Build command for executing pg_dump.  '-i' means ignore version differences.
 	$cmd = $exe . " -i";
 	$cmd .= ' --inserts';
-	// Execute command and return the output to the screen
-	passthru($cmd);
+
+	// Execute command and return the output to the screen.
+	passthru( $cmd );
+
 	exit;
 }
 
-if ( ! $_REQUEST['modfunc'])
+if ( ! $_REQUEST['modfunc'] )
 {
 	echo '<br />';
 	PopTable('header',_('Database Backup'));
