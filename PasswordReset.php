7fda3499f5798d19d7fcc78d9bb45f7aef9c7fb8
François Jacquet <francoisjacquet@users.noreply.github.com>
2016-07-21 17:42:05 -0500

    Add HTTPS default port to _currentPageURL()

--- PasswordReset.php
+++ PasswordReset.php
@@ -421,7 +421,8 @@ function _currentPageURL()
 
 	$page_url .= '://';
 
-	if ( $_SERVER['SERVER_PORT'] != '80' )
+	if ( $_SERVER['SERVER_PORT'] != '80'
+		&& $_SERVER['SERVER_PORT'] != '443' )
 	{
 		$page_url .= $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . $_SERVER['REQUEST_URI'];
 	}
