2766ffa6e1370cc76af10021187321773c151c38
François Jacquet <francoisjacquet@users.noreply.github.com>
2016-08-09 21:35:05 -0500

    Format code in Enrollment.inc.php

--- modules/Students/includes/Enrollment.inc.php
+++ modules/Students/includes/Enrollment.inc.php
@@ -1,45 +1,83 @@
 <?php
 require_once 'ProgramFunctions/StudentsUsersInfo.fnc.php';
 
-$functions = array('START_DATE' => '_makeStartInput','END_DATE' => '_makeEndInput','SCHOOL_ID' => '_makeSchoolInput');
-unset($THIS_RET);
-$RET = DBGet(DBQuery("SELECT e.ID,e.ENROLLMENT_CODE,e.START_DATE,e.DROP_CODE,e.END_DATE,e.END_DATE AS END,e.SCHOOL_ID,e.NEXT_SCHOOL,e.CALENDAR_ID,e.GRADE_ID FROM STUDENT_ENROLLMENT e WHERE e.STUDENT_ID='".UserStudentID()."' AND e.SYEAR='".UserSyear()."' ORDER BY e.START_DATE"),$functions);
+$functions = array(
+	'START_DATE' => '_makeStartInput',
+	'END_DATE' => '_makeEndInput',
+	'SCHOOL_ID' => '_makeSchoolInput',
+);
+
+unset( $THIS_RET );
+
+$RET = DBGet( DBQuery( "SELECT e.ID,e.ENROLLMENT_CODE,e.START_DATE,e.DROP_CODE,e.END_DATE,
+		e.END_DATE AS END,e.SCHOOL_ID,e.NEXT_SCHOOL,e.CALENDAR_ID,e.GRADE_ID
+	FROM STUDENT_ENROLLMENT e
+	WHERE e.STUDENT_ID='" . UserStudentID() . "'
+	AND e.SYEAR='" . UserSyear() . "'
+	ORDER BY e.START_DATE" ), $functions );
 
 $add = true;
-if (count($RET))
+
+foreach ( (array) $RET as $value )
 {
-	foreach ( (array) $RET as $value)
+	if ( ( $value['DROP_CODE'] == ''
+			|| ! $value['DROP_CODE'] )
+		&& ( $value['END'] == ''
+			|| ! $value['END'] ) )
 	{
-		if (($value['DROP_CODE']=='' || ! $value['DROP_CODE']) && ($value['END']=='' || ! $value['END']))
-			$add = false;
+		$add = false;
 	}
 }
 
-if ( $add)
-	$link['add']['html'] = array('START_DATE'=>_makeStartInput('','START_DATE'),'SCHOOL_ID'=>_makeSchoolInput('','SCHOOL_ID'));
+if ( $add )
+{
+	$link['add']['html'] = array(
+		'START_DATE' => _makeStartInput( '','START_DATE' ),
+		'SCHOOL_ID' => _makeSchoolInput( '', 'SCHOOL_ID' ),
+	);
+}
 
-$columns = array('START_DATE' => _('Attendance Start Date this School Year'),'END_DATE' => _('Dropped'),'SCHOOL_ID' => _('School'));
+$columns = array(
+	'START_DATE' => _( 'Attendance Start Date this School Year' ),
+	'END_DATE' => _( 'Dropped' ),
+	'SCHOOL_ID' => _( 'School' ),
+);
 
-$schools_RET = DBGet(DBQuery("SELECT ID,TITLE FROM SCHOOLS WHERE ID!='".UserSchool()."' AND SYEAR='".UserSyear()."'"));
+$schools_RET = DBGet( DBQuery( "SELECT ID,TITLE
+	FROM SCHOOLS
+	WHERE ID!='" . UserSchool() . "'
+	AND SYEAR='" . UserSyear() . "'" ) );
+
 $next_school_options = array(UserSchool() => _('Next grade at current school'),'0' => _('Retain'),'-1' => _('Do not enroll after this school year'));
-if (count($schools_RET))
+
+foreach ( (array) $schools_RET as $school )
 {
-	foreach ( (array) $schools_RET as $school)
-		$next_school_options[$school['ID']] = $school['TITLE'];
+	$next_school_options[ $school['ID'] ] = $school['TITLE'];
 }
 
-$calendars_RET = DBGet(DBQuery("SELECT CALENDAR_ID,DEFAULT_CALENDAR,TITLE FROM ATTENDANCE_CALENDARS WHERE SYEAR='".UserSyear()."' AND SCHOOL_ID='".UserSchool()."' ORDER BY DEFAULT_CALENDAR ASC"));
-if (count($calendars_RET))
+$calendars_RET = DBGet( DBQuery( "SELECT CALENDAR_ID,DEFAULT_CALENDAR,TITLE
+	FROM ATTENDANCE_CALENDARS
+	WHERE SYEAR='" . UserSyear() . "'
+	AND SCHOOL_ID='" . UserSchool() . "'
+	ORDER BY DEFAULT_CALENDAR ASC" ) );
+
+$calendar_options = array();
+
+foreach ( (array) $calendars_RET as $calendar )
 {
-	foreach ( (array) $calendars_RET as $calendar)
-		$calendar_options[$calendar['CALENDAR_ID']] = $calendar['TITLE'];
+	$calendar_options[ $calendar['CALENDAR_ID'] ] = $calendar['TITLE'];
 }
 
-$gradelevels_RET = DBGet(DBQuery("SELECT ID,TITLE FROM SCHOOL_GRADELEVELS WHERE SCHOOL_ID='".UserSchool()."' ORDER BY SORT_ORDER"));
-if (count($gradelevels_RET))
+$gradelevels_RET = DBGet( DBQuery( "SELECT ID,TITLE
+	FROM SCHOOL_GRADELEVELS
+	WHERE SCHOOL_ID='" . UserSchool() . "'
+	ORDER BY SORT_ORDER" ) );
+
+$gradelevel_options = array();
+
+foreach ( (array) $gradelevels_RET as $gradelevel )
 {
-	foreach ( (array) $gradelevels_RET as $gradelevel)
-		$gradelevel_options[$gradelevel['ID']] = $gradelevel['TITLE'];
+	$gradelevel_options[ $gradelevel['ID'] ] = $gradelevel['TITLE'];
 }
 
 if ( $_REQUEST['student_id']!='new' && count($RET))
@@ -108,7 +146,15 @@ if ( $PopTable_opened )
 	PopTable( 'footer' );
 }
 
-ListOutput($RET,$columns,'Enrollment Record','Enrollment Records',$link,array(),array('save'=>false,'search'=>false));
+ListOutput(
+	$RET,
+	$columns,
+	'Enrollment Record',
+	'Enrollment Records',
+	$link,
+	array(),
+	array( 'save' => false, 'search' => false )
+);
 
 if ( $PopTable_opened )
 {
