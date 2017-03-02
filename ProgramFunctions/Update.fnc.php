7d383fd2328a8cd5ebf9ccb5f1048aa382736660
François Jacquet <francoisjacquet@users.noreply.github.com>
2017-03-02 09:29:38 -0500

    Version 3.0.1 update

--- ProgramFunctions/Update.fnc.php
+++ ProgramFunctions/Update.fnc.php
@@ -31,7 +31,7 @@ function Update()
 	 * Prevent DB version update if new Update.fnc.php file has NOT been uploaded YET.
 	 * Update must be run once both new Warehouse.php & Update.fnc.php files are uploaded.
 	 */
-	if ( version_compare( '3.0', ROSARIO_VERSION, '<' ) )
+	if ( version_compare( '3.0.1', ROSARIO_VERSION, '<' ) )
 	{
 		return false;
 	}
