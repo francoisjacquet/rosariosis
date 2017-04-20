40a7a7603d391e61226897af75aa75baf90bd4ef
François Jacquet <francoisjacquet@users.noreply.github.com>
2017-04-20 17:29:11 -0500

    CSS add .tipmsg-label class to TipMessage label

--- ProgramFunctions/TipMessage.fnc.php
+++ ProgramFunctions/TipMessage.fnc.php
@@ -33,7 +33,7 @@ function MakeTipMessage( $message, $title, $label )
 		json_encode( (string) $title ) . ',' .
 		json_encode( (string) $message ) . '];</script>';
 
-	$tip_msg .= '<div class="tipmsg" onMouseOver="stm(tipmsg' . $tip_msg_ID . ');" onMouseOut="htm();" onclick="return false;">' .
+	$tip_msg .= '<div class="tipmsg-label" onMouseOver="stm(tipmsg' . $tip_msg_ID . ');" onMouseOut="htm();" onclick="return false;">' .
 		$label . '</div>';
 
 	$tip_msg_ID++;
