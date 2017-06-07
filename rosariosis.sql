895e671f66c26473d8a515d3477119b015e46a68
François Jacquet <francoisjacquet@users.noreply.github.com>
2017-06-07 11:10:22 -0500

    Fix default student enrollment date

--- rosariosis.sql
+++ rosariosis.sql
@@ -4301,7 +4301,7 @@ INSERT INTO staff_field_categories VALUES (3, 'Food Service', 3, NULL, 'Food_Ser
 -- Data for Name: student_enrollment; Type: TABLE DATA; Schema: public; Owner: rosariosis
 --
 
-INSERT INTO student_enrollment VALUES (1, 2017, 1, 1, 7, '2017-06-22', NULL, 3, NULL, 1, 1, 1);
+INSERT INTO student_enrollment VALUES (1, 2017, 1, 1, 7, '2017-06-12', NULL, 3, NULL, 1, 1, 1);
 
 
 --
