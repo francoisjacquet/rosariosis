cebbd6abad9f5f68eed2c3166f0402eeef2f06e2
François Jacquet <francoisjacquet@users.noreply.github.com>
2017-08-17 19:42:18 +0200

    Fix Remove double / escaped quotes in LO_search input value

--- functions/ListOutput.fnc.php
+++ functions/ListOutput.fnc.php
@@ -558,7 +558,7 @@ function ListOutput( $result, $column_names, $singular = '.', $plural = '.', $li
 			$search_URL = PreparePHP_SELF( $_REQUEST, array( 'LO_search' ) );
 
 			echo '<input type="text" id="LO_search" name="LO_search" value="' .
-				htmlspecialchars( $LO_search, ENT_QUOTES ) .
+				htmlspecialchars( str_replace( "''", "'", $LO_search ), ENT_QUOTES ) .
 				'" placeholder="' . _( 'Search' ) . '" onkeypress="LOSearch(event, this.value, \'' .
 					$search_URL . '\');" /><input type="button" value="' . _( 'Go' ) .
 				'" onclick="LOSearch(event, $(\'#LO_search\').val(), \'' .
