73197c9e8f32f62620b80105d4c7163e4d4f2088
François Jacquet <francoisjacquet@users.noreply.github.com>
2018-07-11 18:30:03 +0200

    Fix PHP notice variable undefined

--- Bottom.php
+++ Bottom.php
@@ -15,8 +15,106 @@ if ( isAJAX() )
 	ETagCache( 'start' );
 }
 
+// Output Bottom menu.
+if ( empty( $_REQUEST['bottomfunc'] ) ) : ?>
+
+	<div id="footerwrap">
+		<a id="BottomButtonMenu" href="#" onclick="expandMenu(); return false;" title="<?php echo _( 'Menu' ); ?>" class="BottomButton">
+			<span><?php echo _( 'Menu' ); ?></span>
+		</a>
+
+		<?php // FJ icons.
+
+		$btn_path = 'assets/themes/' . Preferences( 'THEME' ) . '/btn/';
+
+		if ( isset( $_SESSION['List_PHP_SELF'] )
+			&& ( User( 'PROFILE' ) === 'admin'
+				|| User( 'PROFILE' ) === 'teacher') ) :
+
+			switch ( $_SESSION['Back_PHP_SELF'] )
+			{
+				case 'student':
+
+					$back_text = _( 'Student List' );
+				break;
+
+				case 'staff':
+
+					$back_text = _( 'User List' );
+				break;
+
+				case 'course':
+
+					$back_text = _( 'Course List' );
+				break;
+
+				default:
+
+					$back_text = sprintf( _( '%s List' ), $_SESSION['Back_PHP_SELF'] );
+			} ?>
+
+			<a href="<?php echo $_SESSION['List_PHP_SELF']; ?>&amp;bottom_back=true" title="<?php echo $back_text; ?>" class="BottomButton">
+				<img src="<?php echo $btn_path; ?>back.png" />
+				<span><?php echo $back_text; ?></span>
+			</a>
+
+		<?php endif;
+
+		if ( isset( $_SESSION['Search_PHP_SELF'] )
+			&& ( User( 'PROFILE' ) === 'admin'
+				|| User( 'PROFILE' ) === 'teacher' ) ) :
+
+			switch ( $_SESSION['Back_PHP_SELF'] )
+			{
+				case 'student':
+
+					$back_text = _( 'Student Search' );
+				break;
+
+				case 'staff':
+
+					$back_text = _( 'User Search' );
+				break;
+
+				case 'course':
+
+					$back_text = _( 'Course Search' );
+				break;
+
+				default:
+
+					$back_text = sprintf( _( '%s Search' ), $_SESSION['Back_PHP_SELF'] );
+			} ?>
+
+			<a href="<?php echo $_SESSION['Search_PHP_SELF']; ?>&amp;bottom_back=true" title="<?php echo $back_text; ?>" class="BottomButton">
+				<img src="<?php echo $btn_path; ?>back.png" />
+				<span><?php echo $back_text; ?></span>
+			</a>
+
+		<?php endif;
+
+		// Do bottom_buttons hook.
+		do_action( 'Bottom.php|bottom_buttons' ); ?>
+
+		<a href="Bottom.php?bottomfunc=print" target="_blank" title="<?php echo _( 'Print' ); ?>" class="BottomButton">
+			<img src="<?php echo $btn_path; ?>print.png" />
+			<span><?php echo _( 'Print' ); ?></span>
+		</a>
+		<a href="#" onclick="toggleHelp();return false;" title="<?php echo _( 'Help' ); ?>" class="BottomButton">
+			<img src="<?php echo $btn_path; ?>help.png" />
+			<span><?php echo _( 'Help' ); ?></span>
+		</a>
+		<a href="index.php?modfunc=logout" target="_top" title="<?php echo _( 'Logout' ); ?>" class="BottomButton">
+			<img src="<?php echo $btn_path; ?>logout.png" />
+			<span><?php echo _( 'Logout' ); ?></span>
+		</a>
+		<span class="loading BottomButton"></span>
+	</div>
+
+	<div id="footerhelp"></div>
+<?php
 // Print PDF.
-if ( $_REQUEST['bottomfunc'] === 'print' ) :
+elseif ( $_REQUEST['bottomfunc'] === 'print' ) :
 
 	if ( ! empty( $_REQUEST['expanded_view'] ) )
 	{
@@ -114,105 +212,7 @@ elseif ( $_REQUEST['bottomfunc'] === 'help' ) :
 
 	echo $help_text;
 
-// Output Bottom menu.
-else : ?>
-
-	<div id="footerwrap">
-		<a id="BottomButtonMenu" href="#" onclick="expandMenu(); return false;" title="<?php echo _( 'Menu' ); ?>" class="BottomButton">
-			<span><?php echo _( 'Menu' ); ?></span>
-		</a>
-
-		<?php // FJ icons.
-
-		$btn_path = 'assets/themes/' . Preferences( 'THEME' ) . '/btn/';
-
-		if ( isset( $_SESSION['List_PHP_SELF'] )
-			&& ( User( 'PROFILE' ) === 'admin'
-				|| User( 'PROFILE' ) === 'teacher') ) :
-
-			switch ( $_SESSION['Back_PHP_SELF'] )
-			{
-				case 'student':
-
-					$back_text = _( 'Student List' );
-				break;
-
-				case 'staff':
-
-					$back_text = _( 'User List' );
-				break;
-
-				case 'course':
-
-					$back_text = _( 'Course List' );
-				break;
-
-				default:
-
-					$back_text = sprintf( _( '%s List' ), $_SESSION['Back_PHP_SELF'] );
-			} ?>
-
-			<a href="<?php echo $_SESSION['List_PHP_SELF']; ?>&amp;bottom_back=true" title="<?php echo $back_text; ?>" class="BottomButton">
-				<img src="<?php echo $btn_path; ?>back.png" />
-				<span><?php echo $back_text; ?></span>
-			</a>
-
-		<?php endif;
-
-		if ( isset( $_SESSION['Search_PHP_SELF'] )
-			&& ( User( 'PROFILE' ) === 'admin'
-				|| User( 'PROFILE' ) === 'teacher' ) ) :
-
-			switch ( $_SESSION['Back_PHP_SELF'] )
-			{
-				case 'student':
-
-					$back_text = _( 'Student Search' );
-				break;
-
-				case 'staff':
-
-					$back_text = _( 'User Search' );
-				break;
-
-				case 'course':
-
-					$back_text = _( 'Course Search' );
-				break;
-
-				default:
-
-					$back_text = sprintf( _( '%s Search' ), $_SESSION['Back_PHP_SELF'] );
-			} ?>
-
-			<a href="<?php echo $_SESSION['Search_PHP_SELF']; ?>&amp;bottom_back=true" title="<?php echo $back_text; ?>" class="BottomButton">
-				<img src="<?php echo $btn_path; ?>back.png" />
-				<span><?php echo $back_text; ?></span>
-			</a>
-
-		<?php endif;
-
-		// Do bottom_buttons hook.
-		do_action( 'Bottom.php|bottom_buttons' ); ?>
-
-		<a href="Bottom.php?bottomfunc=print" target="_blank" title="<?php echo _( 'Print' ); ?>" class="BottomButton">
-			<img src="<?php echo $btn_path; ?>print.png" />
-			<span><?php echo _( 'Print' ); ?></span>
-		</a>
-		<a href="#" onclick="toggleHelp();return false;" title="<?php echo _( 'Help' ); ?>" class="BottomButton">
-			<img src="<?php echo $btn_path; ?>help.png" />
-			<span><?php echo _( 'Help' ); ?></span>
-		</a>
-		<a href="index.php?modfunc=logout" target="_top" title="<?php echo _( 'Logout' ); ?>" class="BottomButton">
-			<img src="<?php echo $btn_path; ?>logout.png" />
-			<span><?php echo _( 'Logout' ); ?></span>
-		</a>
-		<span class="loading BottomButton"></span>
-	</div>
-
-	<div id="footerhelp"></div>
-
-<?php endif;
+endif;
 
 if ( isAJAX() )
 {
