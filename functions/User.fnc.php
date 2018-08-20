d17685da38ebf18ffa2390725691ec63a8b99346
François Jacquet <francoisjacquet@users.noreply.github.com>
2018-08-20 21:05:11 +0200

    Add LAST_LOGIN column to Student user SQL

--- functions/User.fnc.php
+++ functions/User.fnc.php
@@ -55,7 +55,8 @@ function User( $item )
 		elseif ( ! empty( $_SESSION['STUDENT_ID'] ) )
 		{
 			$sql = "SELECT '0' AS STAFF_ID,s.USERNAME," . DisplayNameSQL( 's' ) . " AS NAME,
-				'student' AS PROFILE,'0' AS PROFILE_ID,','||se.SCHOOL_ID||',' AS SCHOOLS,se.SYEAR,se.SCHOOL_ID
+				'student' AS PROFILE,'0' AS PROFILE_ID,LAST_LOGIN,
+				','||se.SCHOOL_ID||',' AS SCHOOLS,se.SYEAR,se.SCHOOL_ID
 				FROM STUDENTS s,STUDENT_ENROLLMENT se
 				WHERE s.STUDENT_ID='" . $_SESSION['STUDENT_ID'] . "'
 				AND se.SYEAR='" . UserSyear() . "'
