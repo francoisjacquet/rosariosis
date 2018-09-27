2b70a1c1300a51959b0ad5e2c9ff5c98afca65f4
François Jacquet <francoisjacquet@users.noreply.github.com>
2018-09-27 21:28:14 +0200

    Use Buttons() function so .primary-button CSS colors are applied.

--- modules/Grades/Transcripts.php
+++ modules/Grades/Transcripts.php
@@ -392,7 +392,8 @@ if ( ! $_REQUEST['modfunc'] )
 		//FJ include gentranscript.php in Transcripts.php
 		//echo '<form action="modules/Grades/gentranscript.php" method="POST">';
 		echo '<form action="Modules.php?modname='.$_REQUEST['modname'].'&modfunc=save&_ROSARIO_PDF=true" method="POST">';
-		$extra['header_right'] = '<input type="submit" value="'._('Create Transcripts for Selected Students').'" />';
+
+		$extra['header_right'] = Buttons( _('Create Transcripts for Selected Students') );
 
 		$extra['extra_header_left'] = '<table>';
 
@@ -544,7 +545,7 @@ if ( ! $_REQUEST['modfunc'] )
 
 	if ( $_REQUEST['search_modfunc']=='list')
 	{
-		echo '<br /><div class="center"><input type="submit" value="'._('Create Transcripts for Selected Students').'" /></div>';
+		echo '<br /><div class="center">' . Buttons( _('Create Transcripts for Selected Students') ) . '</div>';
 		echo '</form>';
 	}
 }
