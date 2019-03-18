d0e3ebc86e5db9c3044779cd12370c74fad73c32
François Jacquet <francoisjacquet@users.noreply.github.com>
2019-03-18 20:07:18 +0100

    Use GET form & SubmitButton()
    Format PHP code

--- modules/Food_Service/Students/ActivityReport.php
+++ modules/Food_Service/Students/ActivityReport.php
@@ -1,74 +1,84 @@
 <?php
 
-Widgets('fsa_discount');
-Widgets('fsa_status');
-Widgets('fsa_barcode');
-Widgets('fsa_account_id');
+Widgets( 'fsa_discount' );
+Widgets( 'fsa_status' );
+Widgets( 'fsa_barcode' );
+Widgets( 'fsa_account_id' );
 
 $extra['SELECT'] .= ",coalesce(fssa.STATUS,'" . DBEscapeString( _( 'Active' ) ) . "') AS STATUS";
 $extra['SELECT'] .= ",(SELECT BALANCE FROM FOOD_SERVICE_ACCOUNTS WHERE ACCOUNT_ID=fssa.ACCOUNT_ID) AS BALANCE";
-if ( !mb_strpos($extra['FROM'],'fssa'))
+
+if ( ! mb_strpos( $extra['FROM'], 'fssa' ) )
 {
 	$extra['FROM'] = ",FOOD_SERVICE_STUDENT_ACCOUNTS fssa";
 	$extra['WHERE'] .= " AND fssa.STUDENT_ID=s.STUDENT_ID";
 }
-$extra['functions'] += array('BALANCE' => 'red');
-$extra['columns_after'] = array('BALANCE' => _('Balance'),'STATUS' => _('Status'));
 
-Search('student_id',$extra);
+$extra['functions'] += array( 'BALANCE' => 'red' );
+$extra['columns_after'] = array( 'BALANCE' => _( 'Balance' ), 'STATUS' => _( 'Status' ) );
+
+Search( 'student_id', $extra );
 
 if ( UserStudentID()
 	&& ! $_REQUEST['modfunc'] )
 {
-
 	$where = '';
+
 	if ( ! empty( $_REQUEST['type_select'] ) )
-		$where .= "AND fst.SHORT_NAME='".$_REQUEST['type_select']."' ";
+	{
+		$where .= "AND fst.SHORT_NAME='" . $_REQUEST['type_select'] . "' ";
+	}
 
 	if ( ! empty( $_REQUEST['staff_select'] ) )
-		$where .= "AND fst.SELLER_ID='".$_REQUEST['staff_select']."' ";
-
-	if ( $_REQUEST['detailed_view']=='true')
 	{
-	    $RET = DBGet( "SELECT fst.TRANSACTION_ID AS TRANS_ID,fst.TRANSACTION_ID,
+		$where .= "AND fst.SELLER_ID='" . $_REQUEST['staff_select'] . "' ";
+	}
+
+	if ( $_REQUEST['detailed_view'] == 'true' )
+	{
+		$RET = DBGet( "SELECT fst.TRANSACTION_ID AS TRANS_ID,fst.TRANSACTION_ID,
 	    	fst.ACCOUNT_ID,fst.SHORT_NAME,fst.STUDENT_ID,fst.DISCOUNT,
 	    	(SELECT sum(AMOUNT)
 	    		FROM FOOD_SERVICE_TRANSACTION_ITEMS
 	    		WHERE TRANSACTION_ID=fst.TRANSACTION_ID) AS AMOUNT,
 	    	fst.BALANCE,fst.TIMESTAMP AS DATE,fst.DESCRIPTION," .
-	    	db_case( array(
-	    		'fst.STUDENT_ID',
-	    		"''",
-	    		'NULL',
-	    		"(SELECT " . DisplayNameSQL() . " FROM STUDENTS WHERE STUDENT_ID=fst.STUDENT_ID)"
-	    	) ) . " AS FULL_NAME," .
-	    	db_case( array(
-	    		'fst.SELLER_ID',
-	    		"''",
-	    		'NULL',
-	    		"(SELECT " . DisplayNameSQL() . " FROM STAFF WHERE STAFF_ID=fst.SELLER_ID)"
-	    	) ) . " AS SELLER
+			db_case( array(
+				'fst.STUDENT_ID',
+				"''",
+				'NULL',
+				"(SELECT " . DisplayNameSQL() . " FROM STUDENTS WHERE STUDENT_ID=fst.STUDENT_ID)",
+			) ) . " AS FULL_NAME," .
+			db_case( array(
+				'fst.SELLER_ID',
+				"''",
+				'NULL',
+				"(SELECT " . DisplayNameSQL() . " FROM STAFF WHERE STAFF_ID=fst.SELLER_ID)",
+			) ) . " AS SELLER
 		FROM FOOD_SERVICE_TRANSACTIONS fst
 		WHERE SYEAR='" . UserSyear() . "'
 		AND fst.TIMESTAMP BETWEEN '" . $date . "' AND date '" . $date . "' +1
 		AND SCHOOL_ID='" . UserSchool() . "'" . $where . "
 		ORDER BY " . ( $_REQUEST['by_name'] ? "FULL_NAME," : '' ) . "fst.TRANSACTION_ID DESC", array( 'DATE' => 'ProperDateTime', 'SHORT_NAME' => 'bump_count' ) );
 
-		foreach ( (array) $RET as $RET_key => $RET_val) {
-			$RET[ $RET_key ]=array_map('types_locale', $RET_val);
+		foreach ( (array) $RET as $RET_key => $RET_val )
+		{
+			$RET[$RET_key] = array_map( 'types_locale', $RET_val );
 		}
 
-		foreach ( (array) $RET as $key => $value)
+		foreach ( (array) $RET as $key => $value )
 		{
 			// get details of each transaction
-			$tmpRET = DBGet( "SELECT TRANSACTION_ID AS TRANS_ID,*,'".$value['SHORT_NAME']."' AS TRANSACTION_SHORT_NAME FROM FOOD_SERVICE_TRANSACTION_ITEMS WHERE TRANSACTION_ID='".$value['TRANSACTION_ID']."'",array('SHORT_NAME' => 'bump_items_count'));
+			$tmpRET = DBGet( "SELECT TRANSACTION_ID AS TRANS_ID,*,'" . $value['SHORT_NAME'] . "' AS TRANSACTION_SHORT_NAME FROM FOOD_SERVICE_TRANSACTION_ITEMS WHERE TRANSACTION_ID='" . $value['TRANSACTION_ID'] . "'", array( 'SHORT_NAME' => 'bump_items_count' ) );
 
-			foreach ( (array) $tmpRET as $RET_key => $RET_val) {
-				$tmpRET[ $RET_key ]=array_map('options_locale', $RET_val);
+			foreach ( (array) $tmpRET as $RET_key => $RET_val )
+			{
+				$tmpRET[$RET_key] = array_map( 'options_locale', $RET_val );
 			}
+
 			// merge transaction and detail records
-			$RET[ $key ] = array($value) + $tmpRET;
+			$RET[$key] = array( $value ) + $tmpRET;
 		}
+
 		//echo '<pre>'; var_dump($RET); echo '</pre>';
 
 		$columns = array(
@@ -99,18 +109,18 @@ if ( UserStudentID()
 	}
 	else
 	{
-	    $RET = DBGet( "SELECT fst.TRANSACTION_ID,fst.ACCOUNT_ID,fst.SHORT_NAME,
+		$RET = DBGet( "SELECT fst.TRANSACTION_ID,fst.ACCOUNT_ID,fst.SHORT_NAME,
 	    fst.STUDENT_ID,fst.DISCOUNT,
 		(SELECT sum(AMOUNT)
 			FROM FOOD_SERVICE_TRANSACTION_ITEMS
 			WHERE TRANSACTION_ID=fst.TRANSACTION_ID) AS AMOUNT,
 		fst.BALANCE,fst.TIMESTAMP AS DATE,fst.DESCRIPTION," .
-		db_case( array(
-			'fst.STUDENT_ID',
-			"''",
-			'NULL',
-			"(SELECT " . DisplayNameSQL() . " FROM STUDENTS WHERE STUDENT_ID=fst.STUDENT_ID)"
-		) ) . " AS FULL_NAME
+			db_case( array(
+				'fst.STUDENT_ID',
+				"''",
+				'NULL',
+				"(SELECT " . DisplayNameSQL() . " FROM STUDENTS WHERE STUDENT_ID=fst.STUDENT_ID)",
+			) ) . " AS FULL_NAME
 		FROM FOOD_SERVICE_TRANSACTIONS fst
 		WHERE SYEAR='" . UserSyear() . "'
 		AND fst.TIMESTAMP BETWEEN '" . $date . "' AND date '" . $date . "' +1
@@ -128,14 +138,19 @@ if ( UserStudentID()
 			'AMOUNT' => _( 'Amount' ),
 		);
 
-		foreach ( (array) $RET as $RET_key => $RET_val) {
-			$RET[ $RET_key ]=array_map('types_locale', $RET_val);
+		foreach ( (array) $RET as $RET_key => $RET_val )
+		{
+			$RET[$RET_key] = array_map( 'types_locale', $RET_val );
 		}
 	}
 
-	$type_select = '<span class="nobr">'._('Type').' <select name="type_select"><option value="">'._('Not Specified').'</option>';
-	foreach ( (array) $types as $short_name => $type)
-		$type_select .= '<option value="'.$short_name.'"'.($_REQUEST['type_select']==$short_name ? ' selected' : '').'>'.$type['DESCRIPTION'].'</option>';
+	$type_select = '<span class="nobr">' . _( 'Type' ) . ' <select name="type_select"><option value="">' . _( 'Not Specified' ) . '</option>';
+
+	foreach ( (array) $types as $short_name => $type )
+	{
+		$type_select .= '<option value="' . $short_name . '"' . ( $_REQUEST['type_select'] == $short_name ? ' selected' : '' ) . '>' . $type['DESCRIPTION'] . '</option>';
+	}
+
 	$type_select .= '</select></span>';
 
 	$staff_RET = DBGet( "SELECT STAFF_ID," . DisplayNameSQL() . " AS FULL_NAME
@@ -145,51 +160,80 @@ if ( UserStudentID()
 		AND PROFILE='admin'
 		ORDER BY LAST_NAME" );
 
-	$staff_select = '<span class="nobr">'._('User').' <select name=staff_select><option value="">'._('Not Specified').'</option>';
-	foreach ( (array) $staff_RET as $staff)
-		$staff_select .= '<option value="'.$staff['STAFF_ID'].'"'.($_REQUEST['staff_select']==$staff['STAFF_ID'] ? ' selected' : '').'>'.$staff['FULL_NAME'].'</option>';
+	$staff_select = '<span class="nobr">' . _( 'User' ) . ' <select name=staff_select><option value="">' . _( 'Not Specified' ) . '</option>';
+
+	foreach ( (array) $staff_RET as $staff )
+	{
+		$staff_select .= '<option value="' . $staff['STAFF_ID'] . '"' . ( $_REQUEST['staff_select'] == $staff['STAFF_ID'] ? ' selected' : '' ) . '>' . $staff['FULL_NAME'] . '</option>';
+	}
+
 	$staff_select .= '</select></span>';
 
 	$PHP_tmp_SELF = PreparePHP_SELF();
-	echo '<form action="'.$PHP_tmp_SELF.'" method="POST">';
-	//FJ add label on checkbox
-	DrawHeader(PrepareDate($date,'_date').' : '.$type_select.' : '.$staff_select.' : <input type="submit" value="'._('Go').'" />');
+
+	echo '<form action="' . $PHP_tmp_SELF . '" method="GET">';
+
+	// FJ add label on checkbox.
+	DrawHeader(
+		PrepareDate( $date, '_date' ) . ' - ' .
+		$type_select . ' - ' . $staff_select .
+		SubmitButton( _( 'Go' ) ) );
 
 	DrawHeader( CheckBoxOnclick( 'by_name', _( 'Sort by Name' ) ) );
 
 	echo '</form>';
 
-
-	if ( $_REQUEST['detailed_view']!='true')
-		DrawHeader('<a href="'.PreparePHP_SELF($_REQUEST,array(),array('detailed_view' => 'true')).'">'._('Detailed View').'</a>');
-	else
-		DrawHeader('<a href="'.PreparePHP_SELF($_REQUEST,array(),array('detailed_view' => 'false')).'">'._('Original View').'</a>');
-
-	if ( $_REQUEST['detailed_view']=='true')
+	if ( $_REQUEST['detailed_view'] != 'true' )
 	{
-		$LO_types = array(array(array()));
-		foreach ( (array) $types as $type)
-			if ( $type['COUNT'])
+		DrawHeader( '<a href="' . PreparePHP_SELF( $_REQUEST, array(), array( 'detailed_view' => 'true' ) ) . '">' .
+			_( 'Detailed View' ) . '</a>' );
+	}
+	else
+	{
+		DrawHeader( '<a href="' . PreparePHP_SELF( $_REQUEST, array(), array( 'detailed_view' => 'false' ) ) . '">' . _( 'Original View' ) . '</a>' );
+	}
+
+	if ( $_REQUEST['detailed_view'] == 'true' )
+	{
+		$LO_types = array( array( array() ) );
+
+		foreach ( (array) $types as $type )
+		{
+			if ( $type['COUNT'] )
 			{
-				$LO_types[] = array(array('DESCRIPTION' => $type['DESCRIPTION'],'DETAIL' => '','COUNT' => $type['COUNT'],'AMOUNT'=>number_format($type['AMOUNT'],2)));
-				foreach ( (array) $type['ITEMS'] as $item)
-					if ( $item[1]['COUNT'])
-						$LO_types[last($LO_types)][] = array('DESCRIPTION' => $type['DESCRIPTION'],'DETAIL' => $item[1]['DESCRIPTION'],'COUNT' => $item[1]['COUNT'],'AMOUNT'=>number_format($item[1]['AMOUNT'],2));
+				$LO_types[] = array( array( 'DESCRIPTION' => $type['DESCRIPTION'], 'DETAIL' => '', 'COUNT' => $type['COUNT'], 'AMOUNT' => number_format( $type['AMOUNT'], 2 ) ) );
+
+				foreach ( (array) $type['ITEMS'] as $item )
+				{
+					if ( $item[1]['COUNT'] )
+					{
+						$LO_types[last( $LO_types )][] = array( 'DESCRIPTION' => $type['DESCRIPTION'], 'DETAIL' => $item[1]['DESCRIPTION'], 'COUNT' => $item[1]['COUNT'], 'AMOUNT' => number_format( $item[1]['AMOUNT'], 2 ) );
+					}
+				}
 			}
-		$types_columns = array('DESCRIPTION' => _('Description'),'DETAIL' => _('Detail'),'COUNT' => _('Count'),'AMOUNT' => _('Amount'));
-		$types_group = array('DESCRIPTION');
+		}
+
+		$types_columns = array( 'DESCRIPTION' => _( 'Description' ), 'DETAIL' => _( 'Detail' ), 'COUNT' => _( 'Count' ), 'AMOUNT' => _( 'Amount' ) );
+		$types_group = array( 'DESCRIPTION' );
 	}
 	else
 	{
-		$LO_types = array(array());
-		foreach ( (array) $types as $type)
-			if ( $type['COUNT'])
-				$LO_types[] = array('DESCRIPTION' => $type['DESCRIPTION'],'COUNT' => $type['COUNT'],'AMOUNT'=>number_format($type['AMOUNT'],2));
-		$types_columns = array('DESCRIPTION' => _('Description'),'COUNT' => _('Count'),'AMOUNT' => _('Amount'));
+		$LO_types = array( array() );
+
+		foreach ( (array) $types as $type )
+		{
+			if ( $type['COUNT'] )
+			{
+				$LO_types[] = array( 'DESCRIPTION' => $type['DESCRIPTION'], 'COUNT' => $type['COUNT'], 'AMOUNT' => number_format( $type['AMOUNT'], 2 ) );
+			}
+		}
+
+		$types_columns = array( 'DESCRIPTION' => _( 'Description' ), 'COUNT' => _( 'Count' ), 'AMOUNT' => _( 'Amount' ) );
 	}
-	unset($LO_types[0]);
 
-	ListOutput($LO_types,$types_columns,'Transaction Type','Transaction Types',false,$types_group,array('save'=>false,'search'=>false,'print'=>false));
+	unset( $LO_types[0] );
 
-	ListOutput($RET,$columns,'Transaction','Transactions',$link,$group,array('save'=>false,'search'=>false,'print'=>false));
+	ListOutput( $LO_types, $types_columns, 'Transaction Type', 'Transaction Types', false, $types_group, array( 'save' => false, 'search' => false, 'print' => false ) );
+
+	ListOutput( $RET, $columns, 'Transaction', 'Transactions', $link, $group, array( 'save' => false, 'search' => false, 'print' => false ) );
 }
--- modules/Food_Service/Users/ActivityReport.php
+++ modules/Food_Service/Users/ActivityReport.php
@@ -1,66 +1,75 @@
 <?php
 
-StaffWidgets('fsa_status');
-StaffWidgets('fsa_barcode');
-StaffWidgets('fsa_exists_Y');
+StaffWidgets( 'fsa_status' );
+StaffWidgets( 'fsa_barcode' );
+StaffWidgets( 'fsa_exists_Y' );
 
 $extra['SELECT'] .= ",(SELECT coalesce(STATUS,'" . DBEscapeString( _( 'Active' ) ) . "') FROM FOOD_SERVICE_STAFF_ACCOUNTS WHERE STAFF_ID=s.STAFF_ID) AS STATUS";
 $extra['SELECT'] .= ",(SELECT BALANCE FROM FOOD_SERVICE_STAFF_ACCOUNTS WHERE STAFF_ID=s.STAFF_ID) AS BALANCE";
-$extra['functions'] += array('BALANCE' => 'red');
-$extra['columns_after'] = array('BALANCE' => _('Balance'),'STATUS' => _('Status'));
+$extra['functions'] += array( 'BALANCE' => 'red' );
+$extra['columns_after'] = array( 'BALANCE' => _( 'Balance' ), 'STATUS' => _( 'Status' ) );
 
-Search('staff_id',$extra);
+Search( 'staff_id', $extra );
 
-if (UserStaffID() && ! $_REQUEST['modfunc'])
+if ( UserStaffID() && ! $_REQUEST['modfunc'] )
 {
-
 	$where = '';
+
 	if ( ! empty( $_REQUEST['type_select'] ) )
-		$where .= "AND fst.SHORT_NAME='".$_REQUEST['type_select']."' ";
+	{
+		$where .= "AND fst.SHORT_NAME='" . $_REQUEST['type_select'] . "' ";
+	}
 
 	if ( ! empty( $_REQUEST['staff_select'] ) )
-		$where .= "AND fst.SELLER_ID='".$_REQUEST['staff_select']."' ";
-
-	if ( $_REQUEST['detailed_view']=='true')
 	{
-	    $RET = DBGet( "SELECT fst.TRANSACTION_ID AS TRANS_ID,fst.TRANSACTION_ID,
+		$where .= "AND fst.SELLER_ID='" . $_REQUEST['staff_select'] . "' ";
+	}
+
+	if ( $_REQUEST['detailed_view'] == 'true' )
+	{
+		$RET = DBGet( "SELECT fst.TRANSACTION_ID AS TRANS_ID,fst.TRANSACTION_ID,
 		fst.SHORT_NAME,fst.STAFF_ID,
 		(SELECT sum(AMOUNT) FROM FOOD_SERVICE_STAFF_TRANSACTION_ITEMS WHERE TRANSACTION_ID=fst.TRANSACTION_ID) AS AMOUNT,
 		fst.BALANCE,fst.TIMESTAMP AS DATE,fst.DESCRIPTION," .
-		db_case( array(
-			'fst.STAFF_ID',
-			"''",
-			'NULL',
-			"(SELECT " . DisplayNameSQL() . " FROM STAFF WHERE STAFF_ID=fst.STAFF_ID)"
-		) ) . " AS FULL_NAME," .
-		db_case( array(
-			'fst.SELLER_ID',
-			"''",
-			'NULL',
-			"(SELECT " . DisplayNameSQL() . " FROM STAFF WHERE STAFF_ID=fst.SELLER_ID)"
-		) ) . " AS SELLER
+			db_case( array(
+				'fst.STAFF_ID',
+				"''",
+				'NULL',
+				"(SELECT " . DisplayNameSQL() . " FROM STAFF WHERE STAFF_ID=fst.STAFF_ID)",
+			) ) . " AS FULL_NAME," .
+			db_case( array(
+				'fst.SELLER_ID',
+				"''",
+				'NULL',
+				"(SELECT " . DisplayNameSQL() . " FROM STAFF WHERE STAFF_ID=fst.SELLER_ID)",
+			) ) . " AS SELLER
 		FROM FOOD_SERVICE_STAFF_TRANSACTIONS fst
 		WHERE SYEAR='" . UserSyear() . "'
 		AND fst.TIMESTAMP BETWEEN '" . $date . "' AND date '" . $date . "' +1
 		AND SCHOOL_ID='" . UserSchool() . "'" . $where . "
 		ORDER BY " . ( $_REQUEST['by_name'] ? "FULL_NAME," : '' ) . "fst.TRANSACTION_ID DESC", array( 'DATE' => 'ProperDateTime', 'SHORT_NAME' => 'bump_count' ) );
 
-		foreach ( (array) $RET as $RET_key => $RET_val) {
-			$RET[ $RET_key ]=array_map('types_locale', $RET_val);
+		foreach ( (array) $RET as $RET_key => $RET_val )
+		{
+			$RET[$RET_key] = array_map( 'types_locale', $RET_val );
 		}
 
-		foreach ( (array) $RET as $key => $value)
+		foreach ( (array) $RET as $key => $value )
 		{
 			// get details of each transaction
-			$tmpRET = DBGet( "SELECT TRANSACTION_ID AS TRANS_ID,*,'".$value['SHORT_NAME']."' AS TRANSACTION_SHORT_NAME FROM FOOD_SERVICE_STAFF_TRANSACTION_ITEMS WHERE TRANSACTION_ID='".$value['TRANSACTION_ID']."'",array('SHORT_NAME' => 'bump_items_count'));
+			$tmpRET = DBGet( "SELECT TRANSACTION_ID AS TRANS_ID,*,'" . $value['SHORT_NAME'] . "' AS TRANSACTION_SHORT_NAME FROM FOOD_SERVICE_STAFF_TRANSACTION_ITEMS WHERE TRANSACTION_ID='" . $value['TRANSACTION_ID'] . "'", array( 'SHORT_NAME' => 'bump_items_count' ) );
 
-	//FJ add translation
-			foreach ( (array) $tmpRET as $RET_key => $RET_val) {
-				$tmpRET[ $RET_key ]=array_map('options_locale', $RET_val);
+			//FJ add translation
+
+			foreach ( (array) $tmpRET as $RET_key => $RET_val )
+			{
+				$tmpRET[$RET_key] = array_map( 'options_locale', $RET_val );
 			}
+
 			// merge transaction and detail records
-			$RET[ $key ] = array($value) + $tmpRET;
+			$RET[$key] = array( $value ) + $tmpRET;
 		}
+
 		//echo '<pre>'; var_dump($RET); echo '</pre>';
 
 		$columns = array(
@@ -88,15 +97,15 @@ if (UserStaffID() && ! $_REQUEST['modfunc'])
 	}
 	else
 	{
-	    $RET = DBGet( "SELECT fst.TRANSACTION_ID,fst.SHORT_NAME,fst.STAFF_ID,
+		$RET = DBGet( "SELECT fst.TRANSACTION_ID,fst.SHORT_NAME,fst.STAFF_ID,
 		(SELECT sum(AMOUNT) FROM FOOD_SERVICE_STAFF_TRANSACTION_ITEMS WHERE TRANSACTION_ID=fst.TRANSACTION_ID) AS AMOUNT,
 		fst.BALANCE,fst.TIMESTAMP AS DATE,fst.DESCRIPTION," .
-		db_case( array(
-			'fst.STAFF_ID',
-			"''",
-			'NULL',
-			"(SELECT " . DisplayNameSQL() . " FROM STAFF WHERE STAFF_ID=fst.STAFF_ID)"
-		) ) . " AS FULL_NAME
+			db_case( array(
+				'fst.STAFF_ID',
+				"''",
+				'NULL',
+				"(SELECT " . DisplayNameSQL() . " FROM STAFF WHERE STAFF_ID=fst.STAFF_ID)",
+			) ) . " AS FULL_NAME
 		FROM FOOD_SERVICE_STAFF_TRANSACTIONS fst
 		WHERE SYEAR='" . UserSyear() . "'
 		AND fst.TIMESTAMP BETWEEN '" . $date . "' AND date '" . $date . "' +1
@@ -112,14 +121,19 @@ if (UserStaffID() && ! $_REQUEST['modfunc'])
 			'AMOUNT' => _( 'Amount' ),
 		);
 
-		foreach ( (array) $RET as $RET_key => $RET_val) {
-			$RET[ $RET_key ]=array_map('types_locale', $RET_val);
+		foreach ( (array) $RET as $RET_key => $RET_val )
+		{
+			$RET[$RET_key] = array_map( 'types_locale', $RET_val );
 		}
 	}
 
-	$type_select = '<span class="nobr">'._('Type').' <select name="type_select"><option value="">'._('Not Specified').'</option>';
-	foreach ( (array) $types as $short_name => $type)
-		$type_select .= '<option value="'.$short_name.'"'.($_REQUEST['type_select']==$short_name ? ' selected' : '').'>'.$type['DESCRIPTION'].'</option>';
+	$type_select = '<span class="nobr">' . _( 'Type' ) . ' <select name="type_select"><option value="">' . _( 'Not Specified' ) . '</option>';
+
+	foreach ( (array) $types as $short_name => $type )
+	{
+		$type_select .= '<option value="' . $short_name . '"' . ( $_REQUEST['type_select'] == $short_name ? ' selected' : '' ) . '>' . $type['DESCRIPTION'] . '</option>';
+	}
+
 	$type_select .= '</select></span>';
 
 	$staff_RET = DBGet( "SELECT STAFF_ID," . DisplayNameSQL() . " AS FULL_NAME
@@ -129,58 +143,87 @@ if (UserStaffID() && ! $_REQUEST['modfunc'])
 		AND PROFILE='admin'
 		ORDER BY LAST_NAME" );
 
-	$staff_select = '<span class="nobr">'._('User').' <select name=staff_select><option value="">'._('Not Specified').'</option>';
-	foreach ( (array) $staff_RET as $staff)
-		$staff_select .= '<option value="'.$staff['STAFF_ID'].'"'.($_REQUEST['staff_select']==$staff['STAFF_ID'] ? ' selected' : '').'>'.$staff['FULL_NAME'].'</option>';
+	$staff_select = '<span class="nobr">' . _( 'User' ) . ' <select name=staff_select><option value="">' . _( 'Not Specified' ) . '</option>';
+
+	foreach ( (array) $staff_RET as $staff )
+	{
+		$staff_select .= '<option value="' . $staff['STAFF_ID'] . '"' . ( $_REQUEST['staff_select'] == $staff['STAFF_ID'] ? ' selected' : '' ) . '>' . $staff['FULL_NAME'] . '</option>';
+	}
+
 	$staff_select .= '</select></span>';
 
 	$PHP_tmp_SELF = PreparePHP_SELF();
-	echo '<form action="'.$PHP_tmp_SELF.'" method="POST">';
 
-	DrawHeader( PrepareDate( $date, '_date' ) . ' : ' . $type_select . ' : ' .
-		$staff_select . ' : ' . Buttons( _( 'Go' ) ) );
+	echo '<form action="' . $PHP_tmp_SELF . '" method="GET">';
+
+	DrawHeader(
+		PrepareDate( $date, '_date' ) . ' - ' . $type_select . ' - ' .
+		$staff_select . ' ' . SubmitButton( _( 'Go' ) ) );
 
 	DrawHeader( CheckBoxOnclick( 'by_name', _( 'Sort by Name' ) ) );
 
 	echo '</form>';
 
 	if ( ! empty( $_REQUEST['type_select'] ) )
-		$where = "AND fst.SHORT_NAME='".$_REQUEST['type_select']."' ";
+	{
+		$where = "AND fst.SHORT_NAME='" . $_REQUEST['type_select'] . "' ";
+	}
 
 	if ( ! empty( $_REQUEST['staff_select'] ) )
-		$where = "AND fst.SELLER_ID='".$_REQUEST['staff_select']."' ";
-
-	if ( $_REQUEST['detailed_view']!='true')
-		DrawHeader('<a href="'.PreparePHP_SELF($_REQUEST,array(),array('detailed_view' => 'true')).'">'._('Detailed View').'</a>');
-	else
-		DrawHeader('<a href="'.PreparePHP_SELF($_REQUEST,array(),array('detailed_view' => 'false')).'">'._('Original View').'</a>');
-
-	if ( $_REQUEST['detailed_view']=='true')
 	{
-		$LO_types = array(array(array()));
-		foreach ( (array) $types as $type)
-			if ( $type['COUNT'])
+		$where = "AND fst.SELLER_ID='" . $_REQUEST['staff_select'] . "' ";
+	}
+
+	if ( $_REQUEST['detailed_view'] != 'true' )
+	{
+		DrawHeader( '<a href="' . PreparePHP_SELF( $_REQUEST, array(), array( 'detailed_view' => 'true' ) ) . '">' . _( 'Detailed View' ) . '</a>' );
+	}
+	else
+	{
+		DrawHeader( '<a href="' . PreparePHP_SELF( $_REQUEST, array(), array( 'detailed_view' => 'false' ) ) . '">' . _( 'Original View' ) . '</a>' );
+	}
+
+	if ( $_REQUEST['detailed_view'] == 'true' )
+	{
+		$LO_types = array( array( array() ) );
+
+		foreach ( (array) $types as $type )
+		{
+			if ( $type['COUNT'] )
 			{
-				$LO_types[] = array(array('DESCRIPTION' => $type['DESCRIPTION'],'DETAIL' => '','COUNT' => $type['COUNT'],'AMOUNT'=>number_format($type['AMOUNT'],2)));
-				foreach ( (array) $type['ITEMS'] as $item)
-					if ( $item[1]['COUNT'])
-						$LO_types[last($LO_types)][] = array('DESCRIPTION' => $type['DESCRIPTION'],'DETAIL' => $item[1]['DESCRIPTION'],'COUNT' => $item[1]['COUNT'],'AMOUNT'=>number_format($item[1]['AMOUNT'],2));
+				$LO_types[] = array( array( 'DESCRIPTION' => $type['DESCRIPTION'], 'DETAIL' => '', 'COUNT' => $type['COUNT'], 'AMOUNT' => number_format( $type['AMOUNT'], 2 ) ) );
+
+				foreach ( (array) $type['ITEMS'] as $item )
+				{
+					if ( $item[1]['COUNT'] )
+					{
+						$LO_types[last( $LO_types )][] = array( 'DESCRIPTION' => $type['DESCRIPTION'], 'DETAIL' => $item[1]['DESCRIPTION'], 'COUNT' => $item[1]['COUNT'], 'AMOUNT' => number_format( $item[1]['AMOUNT'], 2 ) );
+					}
+				}
 			}
-		$types_columns = array('DESCRIPTION' => _('Description'),'DETAIL' => _('Detail'),'COUNT' => _('Count'),'AMOUNT' => _('Amount'));
-		$types_group = array('DESCRIPTION');
+		}
+
+		$types_columns = array( 'DESCRIPTION' => _( 'Description' ), 'DETAIL' => _( 'Detail' ), 'COUNT' => _( 'Count' ), 'AMOUNT' => _( 'Amount' ) );
+		$types_group = array( 'DESCRIPTION' );
 	}
 	else
 	{
-		$LO_types = array(array());
-		foreach ( (array) $types as $type)
-			if ( $type['COUNT'])
-				$LO_types[] = array('DESCRIPTION' => $type['DESCRIPTION'],'COUNT' => $type['COUNT'],'AMOUNT'=>number_format($type['AMOUNT'],2));
-		$types_columns = array('DESCRIPTION' => _('Description'),'COUNT' => _('Count'),'AMOUNT' => _('Amount'));
+		$LO_types = array( array() );
+
+		foreach ( (array) $types as $type )
+		{
+			if ( $type['COUNT'] )
+			{
+				$LO_types[] = array( 'DESCRIPTION' => $type['DESCRIPTION'], 'COUNT' => $type['COUNT'], 'AMOUNT' => number_format( $type['AMOUNT'], 2 ) );
+			}
+		}
+
+		$types_columns = array( 'DESCRIPTION' => _( 'Description' ), 'COUNT' => _( 'Count' ), 'AMOUNT' => _( 'Amount' ) );
 	}
-	unset($LO_types[0]);
 
+	unset( $LO_types[0] );
 
-	ListOutput($LO_types,$types_columns,'Transaction Type','Transaction Types',false,$types_group,array('save'=>false,'search'=>false,'print'=>false));
+	ListOutput( $LO_types, $types_columns, 'Transaction Type', 'Transaction Types', false, $types_group, array( 'save' => false, 'search' => false, 'print' => false ) );
 
-	ListOutput($RET,$columns,'Transaction','Transactions',$link,$group,array('save'=>false,'search'=>false,'print'=>false));
+	ListOutput( $RET, $columns, 'Transaction', 'Transactions', $link, $group, array( 'save' => false, 'search' => false, 'print' => false ) );
 }
