46ef9b132f02fa658889934c2b3a879a701ea2a9
François Jacquet <francoisjacquet@users.noreply.github.com>
2019-06-07 16:38:01 +0200

    JS Rollback to onclick event for Search icon

--- functions/ListOutput.fnc.php
+++ functions/ListOutput.fnc.php
@@ -472,7 +472,7 @@ function ListOutput( $result, $column_names, $singular = '.', $plural = '.', $li
 			'" placeholder="' . _( 'Search' ) . '" onkeypress="LOSearch(event, this.value, \'' .
 			$search_URL . '\');" autocomplete="off" />
 				<img src="assets/themes/' . Preferences( 'THEME' ) . '/btn/visualize.png"
-				onfocus="LOSearch(event, $(\'#LO_search\').val(), \'' . $search_URL . '\');"
+				onclick="LOSearch(event, $(\'#LO_search\').val(), \'' . $search_URL . '\');"
 				class="button" alt="" />
 				<label for="LO_search" class="a11y-hidden">' . _( 'Search' ) . '</label>';
 
