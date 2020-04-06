480a70bdbf1b3b3f0ed1580fca20d01b758b26ab
François Jacquet <francoisjacquet@users.noreply.github.com>
2020-04-06 16:40:30 +0200

    Add MoodleConfig() function

--- plugins/Moodle/getconfig.inc.php
+++ plugins/Moodle/getconfig.inc.php
@@ -1,36 +1,50 @@
 <?php
 
-// user logged in
-if ( UserSchool() > 0 )
+/**
+ * Set Moodle plugin configuration options
+ *
+ * @since 6.0
+ *
+ * @return bool False if ! UserSchool() or empty constants.
+ */
+function MoodleConfig()
 {
-	//define constants for plugin use
+	static $defined_constants = false;
 
-	//example: http://localhost/moodle
-	define( 'MOODLE_URL', ProgramConfig( 'moodle', 'MOODLE_URL' ) );
-
-	//example: d6c51ea6ffd9857578722831bcb070e1
-	define( 'MOODLE_TOKEN', ProgramConfig( 'moodle', 'MOODLE_TOKEN' ) );
-
-	//example: 10
-	define( 'MOODLE_PARENT_ROLE_ID', ProgramConfig( 'moodle', 'MOODLE_PARENT_ROLE_ID' ) );
-
-	$email_field = ProgramConfig( 'moodle', 'ROSARIO_STUDENTS_EMAIL_FIELD_ID' );
-
-	if ( $email_field !== 'USERNAME' )
+	if ( ! UserSchool() )
 	{
-		$email_field = 'CUSTOM_' . $email_field;
+		return false;
 	}
 
-	//example: 11 => CUSTOM_11
-	define( 'ROSARIO_STUDENTS_EMAIL_FIELD', $email_field );
+	if ( ! $defined_constants )
+	{
+		$defined_constants = true;
+
+		// Define constants for plugin use.
+
+		// Example: http://localhost/moodle
+		define( 'MOODLE_URL', ProgramConfig( 'moodle', 'MOODLE_URL' ) );
+
+		// Example: d6c51ea6ffd9857578722831bcb070e1
+		define( 'MOODLE_TOKEN', ProgramConfig( 'moodle', 'MOODLE_TOKEN' ) );
+
+		// Example: 10
+		define( 'MOODLE_PARENT_ROLE_ID', ProgramConfig( 'moodle', 'MOODLE_PARENT_ROLE_ID' ) );
+
+		$email_field = null;
+
+		if ( Config( 'STUDENTS_EMAIL_FIELD' ) )
+		{
+			$email_field = Config( 'STUDENTS_EMAIL_FIELD' ) === 'USERNAME' ?
+				Config( 'STUDENTS_EMAIL_FIELD' ) : 'CUSTOM_' . Config( 'STUDENTS_EMAIL_FIELD' );
+		}
+
+		// Example: 11 => CUSTOM_11.
+		define( 'ROSARIO_STUDENTS_EMAIL_FIELD', $email_field );
+	}
+
+	return MOODLE_URL
+		&& MOODLE_TOKEN
+		&& MOODLE_PARENT_ROLE_ID
+		&& ROSARIO_STUDENTS_EMAIL_FIELD;
 }
-// not logged in
-// @since 5.9 Automatic Moodle Student Account Creation.
-// Reload Moodle config constants.
-/*else
-{
-	define( 'MOODLE_URL', null );
-	define( 'MOODLE_TOKEN', null );
-	define( 'MOODLE_PARENT_ROLE_ID', null );
-	// define( 'ROSARIO_STUDENTS_EMAIL_FIELD_ID', null );
-}*/
