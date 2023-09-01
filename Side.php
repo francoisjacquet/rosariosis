359732bead5bfb62cded59baed853aacabec5d5d
François Jacquet <francoisjacquet@users.noreply.github.com>
2023-09-01 12:56:15 +0200

    Fix PHP warning undefined array key 1

--- Side.php
+++ Side.php
@@ -631,7 +631,7 @@ if ( ! isset( $_REQUEST['sidefunc'] )
 			if ( ! $current_cp_found )
 			{
 				// Do not use SetUserCoursePeriod() here as this is safe.
-				$_SESSION['UserCoursePeriod'] = $cp_RET[1]['COURSE_PERIOD_ID'];
+				$_SESSION['UserCoursePeriod'] = issetVal( $cp_RET[1]['COURSE_PERIOD_ID'] );
 
 				unset( $_SESSION['student_id'] );
 			}
