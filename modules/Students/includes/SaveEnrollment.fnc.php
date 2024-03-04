bd98210b338996041670bdc234f35b660e61020e
François Jacquet <francoisjacquet@users.noreply.github.com>
2024-03-04 17:35:59 +0100

    Fix case when START_DATE is set but empty

--- modules/Students/includes/SaveEnrollment.fnc.php
+++ modules/Students/includes/SaveEnrollment.fnc.php
@@ -16,37 +16,37 @@ function SaveEnrollment()
 		return;
 	}
 
-	foreach ( (array) $_REQUEST['values']['student_enrollment'] as $stu_enrol_id => $stu_enrol )
+	foreach ( (array) $_REQUEST['values']['student_enrollment'] as $id => $columns )
 	{
-		if ( $stu_enrol_id == 'new' && ! $stu_enrol['START_DATE'] )
+		if ( $id == 'new' && ! $columns['START_DATE'] )
 		{
-			unset( $_REQUEST['values']['student_enrollment'][$stu_enrol_id] );
+			unset( $_REQUEST['values']['student_enrollment'][$id] );
 
 			continue;
 		}
 
-		if ( $stu_enrol_id == 'new'
-			&& $stu_enrol['SCHOOL_ID'] )
+		if ( $id == 'new'
+			&& $columns['SCHOOL_ID'] )
 		{
 			// Check if student already enrolled on that date when inserting START_DATE.
 			$found = DBGetOne( "SELECT ID
 				FROM student_enrollment
 				WHERE STUDENT_ID='" . UserStudentID() . "'
 				AND SYEAR='" . UserSyear() . "'
-				AND '" . $stu_enrol['START_DATE'] . "' BETWEEN START_DATE AND END_DATE" );
+				AND '" . $columns['START_DATE'] . "' BETWEEN START_DATE AND END_DATE" );
 
 			if ( $found )
 			{
-				unset( $_REQUEST['values']['student_enrollment'][$stu_enrol_id] );
+				unset( $_REQUEST['values']['student_enrollment'][$id] );
 
 				$error[] = _( 'The student is already enrolled on that date, and cannot be enrolled a second time on the date you specified. Please fix, and try enrolling the student again.' );
 			}
-			elseif ( $stu_enrol['SCHOOL_ID'] != UserSchool() )
+			elseif ( $columns['SCHOOL_ID'] != UserSchool() )
 			{
 				// @since 5.4 Update current school to enrollment school.
 				$_SESSION['UserSchool'] = DBGetOne( "SELECT ID FROM schools
 					WHERE SYEAR='" . UserSyear() . "'
-					AND ID='" . (int) $stu_enrol['SCHOOL_ID'] . "'" );
+					AND ID='" . (int) $columns['SCHOOL_ID'] . "'" );
 			}
 
 			continue;
@@ -57,22 +57,27 @@ function SaveEnrollment()
 			continue;
 		}
 
-		if ( ! empty( $stu_enrol['START_DATE'] ) )
+		if ( ! empty( $columns['START_DATE'] ) )
 		{
 			// Check if student already enrolled on that date when updating START_DATE.
 			$found = DBGetOne( "SELECT ID
 				FROM student_enrollment
 				WHERE STUDENT_ID='" . UserStudentID() . "'
 				AND SYEAR='" . UserSyear() . "'
-				AND '" . $stu_enrol['START_DATE'] . "' BETWEEN START_DATE AND END_DATE" );
+				AND '" . $columns['START_DATE'] . "' BETWEEN START_DATE AND END_DATE" );
 
 			if ( $found )
 			{
-				unset( $_REQUEST['values']['student_enrollment'][$stu_enrol_id] );
+				unset( $_REQUEST['values']['student_enrollment'][$id] );
 
 				$error[] = _( 'The student is already enrolled on that date, and cannot be enrolled a second time on the date you specified. Please fix, and try enrolling the student again.' );
 			}
 
+			continue;
+		}
+
+		if ( ! isset( $columns['START_DATE'] ) )
+		{
 			continue;
 		}
 
@@ -82,10 +87,10 @@ function SaveEnrollment()
 			FROM student_enrollment
 			WHERE STUDENT_ID='" . UserStudentID() . "'
 			AND SYEAR='" . UserSyear() . "'
-			AND ID<>'" . (int) $stu_enrol_id . "'
+			AND ID<>'" . (int) $id . "'
 			AND START_DATE<(SELECT START_DATE
 				FROM student_enrollment
-				WHERE ID='" . (int) $stu_enrol_id . "')
+				WHERE ID='" . (int) $id . "')
 			ORDER BY START_DATE DESC;" );
 
 		if ( $previous_enrollment_school_id )
@@ -93,9 +98,9 @@ function SaveEnrollment()
 			DBQuery( "DELETE FROM student_enrollment
 				WHERE STUDENT_ID='" . UserStudentID() . "'
 				AND SYEAR='" . UserSyear() . "'
-				AND ID='" . (int) $stu_enrol_id . "';" );
+				AND ID='" . (int) $id . "';" );
 
-			unset( $_REQUEST['values']['student_enrollment'][$stu_enrol_id] );
+			unset( $_REQUEST['values']['student_enrollment'][$id] );
 
 			if ( $previous_enrollment_school_id != UserSchool() )
 			{
