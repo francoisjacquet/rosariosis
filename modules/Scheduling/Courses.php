02ca6be20bc7212f52af7c96036f14eb048c9a07
François Jacquet <francoisjacquet@users.noreply.github.com>
2024-08-31 14:34:15 +0200

    Fix PHP warning undefined array key "year_date"

--- modules/Scheduling/Courses.php
+++ modules/Scheduling/Courses.php
@@ -1693,11 +1693,14 @@ if (  ( ! $_REQUEST['modfunc']
 
 				$link['TITLE']['variables'] = [ 'course_period_id' => 'COURSE_PERIOD_ID', 'course_marking_period_id' => 'MARKING_PERIOD_ID' ];
 
-				if ( $_REQUEST['modfunc'] === 'choose_course' )
+				if ( $_REQUEST['modname'] === 'Scheduling/Schedule.php' )
 				{
 					$link['TITLE']['link'] .= '&year_date=' . $_REQUEST['year_date'] .
 						'&month_date=' . $_REQUEST['month_date'] . '&day_date=' . $_REQUEST['day_date'];
+				}
 
+				if ( $_REQUEST['modfunc'] === 'choose_course' )
+				{
 					$link['TITLE']['link'] .= '&modfunc=' . $_REQUEST['modfunc'] . '&student_id=' . issetVal( $_REQUEST['student_id'], '' ) . '&last_year=' . $_REQUEST['last_year'];
 				}
 				else
