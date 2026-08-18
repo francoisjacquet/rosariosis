2f28300493fc26766e41847b2bf68e71397ab26b
François Jacquet <francoisjacquet@users.noreply.github.com>
2026-08-18 16:10:34 +0200

    Only return 403 Forbidden code if not AJAX request

--- CHANGES.md
+++ CHANGES.md
@@ -4,6 +4,7 @@
 Changes in 12.9.3
 -----------------
 - Security fix #397 Unauthenticated stored XSS: remove HTML tags in SaveReport.php, thanks to @Bradsen
+- Only return 403 Forbidden code if not AJAX request in Warehouse.php
 
 Changes in 12.9.2
 -----------------
--- Warehouse.php
+++ Warehouse.php
@@ -330,8 +330,11 @@ if ( ! empty( $_REQUEST['modfunc'] )
 		// Security fix #371 check CSRF token for modfunc requests
 		if ( empty( $_SESSION['STAFF_ID'] ) && empty( $_SESSION['STUDENT_ID'] ) )
 		{
-			// No user in session yet, 403 Forbidden error (99% chances its a bot)
-			http_response_code( 403 );
+			if ( ! isAJAX() )
+			{
+				// No user in session yet, 403 Forbidden error (99% chances its a bot)
+				http_response_code( 403 );
+			}
 
 			$error[] = _( 'You\'re not allowed to use this program!' );
 
